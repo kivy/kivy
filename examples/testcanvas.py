3ea814b0452b2138d0af96013757a328fe2e0948
Mathieu Virbel <mathieu@pymt.eu>
2010-11-03 17:38:18 -0400

    add testcanvas

---
+++ examples/testcanvas.py
@@ -0,0 +1,18 @@
+from kivy.base import runTouchApp
+from kivy.core.window import Window
+from kivy.graphics import Canvas, Rectangle
+
+
+c = Canvas()
+'''
+with c:
+    Rectangle(size=(100, 100))
+'''
+
+def draw():
+    c.draw()
+
+Window.bind(on_draw=draw)
+
+runTouchApp()
+
