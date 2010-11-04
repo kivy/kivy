3b7560b2902afd8baa41ede6ab3cbf019420e67f
Mathieu Virbel <mathieu@pymt.eu>
2010-11-04 00:05:25 -0400

    textcanvas: remove uneeded transform_mat

--- examples/testcanvas.py
+++ examples/testcanvas.py
@@ -2,15 +2,13 @@ import kivy
 from kivy.base import runTouchApp
 from kivy.core.window import Window
 from kivy.core.image import Image
-from kivy.graphics import Canvas, Rectangle, BorderRectangle
-from kivy.lib.transformations import clip_matrix
+from kivy.graphics import Canvas, Rectangle, BorderRectangle, GraphicContext
 
 img = Image('examples/test.png')
 c = Canvas()
 
 with c:
     c.context.set('color', (1,1,1,1))
-    c.context.set('projection_mat', clip_matrix(0,Window.width,0,Window.height,-1,1))
     BorderRectangle(size=(100, 100), texture=img.texture)
 
 
