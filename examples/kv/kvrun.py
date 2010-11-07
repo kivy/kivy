da7486cafe8071982a2ff73097616476f1c8ec69
Mathieu Virbel <mathieu@pymt.eu>
2010-11-07 17:53:43 -0500

    kvrun: show FPS every one second

--- examples/kv/kvrun.py
+++ examples/kv/kvrun.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python
 
 from kivy.app import App
+from kivy.clock import Clock
 from kivy.lang import Builder
 
 class KvApp(App):
+    def _print_fps(self, *largs):
+        print 'FPS:', Clock.get_fps()
     def build(self):
+        Clock.schedule_interval(self._print_fps, 1)
         return Builder.load_file(self.options['filename'])
 
 if __name__ == '__main__':
