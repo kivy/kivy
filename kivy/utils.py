244984888a01cee4e8d283f03124ef166036465d
Mathieu Virbel <mathieu@pymt.eu>
2010-11-07 17:53:54 -0500

    utils: fix invalid import in __all__

--- kivy/utils.py
+++ kivy/utils.py
@@ -6,7 +6,7 @@ __all__ = ('intersection', 'difference', 'curry', 'strtotuple',
            'get_color_from_hex', 'get_color_for_pyglet', 'get_random_color',
            'is_color_transparent', 'boundary',
            'deprecated', 'SafeList',
-           'interpolate', 'UserDict')
+           'interpolate', 'OrderedDict')
 
 import inspect
 import re
