65274b1a8b12ea66c6ef3d6737771f675016b58e
Mathieu Virbel <mathieu@pymt.eu>
2010-11-07 21:36:17 -0500

    add some kv for example

---
+++ examples/kv/app_layout.py
@@ -0,0 +1,23 @@
+BoxLayout:
+    Button:
+        id: bswitch
+
+    BoxLayout:
+
+        orientation: 'vertical' if bswitch.state == 'down' else 'horizontal'
+
+        Button:
+            id: btn1
+            text: 'Button 1'
+
+        Button:
+            text: btn1.state
+
+        Button:
+            text: 'Button 3'
+
+        Button:
+            text: 'Button 4'
+
+        Button:
+            text: 'Button 5'
