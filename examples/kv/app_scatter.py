feb10af8c7d41845a8753bd38998ccdda84e7580
Mathieu Virbel <mathieu@pymt.eu>
2010-12-22 20:41:01 +0100

    update scatter application

--- examples/kv/app_scatter.py
+++ examples/kv/app_scatter.py
@@ -1,10 +1,6 @@
 Widget:
 	Scatter:
-		pos: 50, 50
-		rotation: 45
-		scale: 2
-		canvas:
-			Color:
-				rgb: 0, 1, 0
-			Rectangle:
-				size: self.size
+        size: image.texture_size
+        Image:
+            id: image
+            source: '../kivy.jpg'
