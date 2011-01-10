a2104f1398e918964e834e536891fadc67225345
Mathieu Virbel <mathieu@pymt.eu>
2011-01-10 01:11:37 +0100

    don't include opengl if documentation is generated

--- kivy/core/gl/__init__.py
+++ kivy/core/gl/__init__.py
@@ -9,15 +9,16 @@ core provider can select an OpenGL ES or a 'classic' desktop OpenGL library.
 # Right now, only PyOpenGL
 from os import environ
 
-from kivy.config import Config
-from kivy.logger import Logger
-from kivy.graphics.opengl import *
-
-def print_gl_version():
-    version = glGetString(GL_VERSION)
-    Logger.info('GL: OpenGL version <%s>' % str(version))
-
 if 'KIVY_DOC_INCLUDE' not in environ:
+
+    from kivy.config import Config
+    from kivy.logger import Logger
+    from kivy.graphics.opengl import *
+
+    def print_gl_version():
+        version = glGetString(GL_VERSION)
+        Logger.info('GL: OpenGL version <%s>' % str(version))
+
     # Disable pyOpenGL auto GL Error Check?
     gl_check = Config.get('kivy', 'gl_error_check')
     if gl_check.lower() in ['0', 'false', 'no']:
