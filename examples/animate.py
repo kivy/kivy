c5e040ce50be0c2f327d0b510be893f6e0bbd334
Mathieu Virbel <mathieu@pymt.eu>
2011-01-11 02:36:46 +0100

    little animation test

---
+++ examples/animate.py
@@ -0,0 +1,19 @@
+from kivy.uix.button import Button
+from kivy.app import App
+from kivy.animation import Animation
+
+class TestApp(App):
+    def animate(self, instance):
+        animation = Animation(pos=(100, 100), t='out_bounce')
+        animation += Animation(pos=(200, 100), t='out_bounce')
+        animation &= Animation(size=(500, 500))
+        animation += Animation(size=(100, 50))
+        animation.start(instance)
+
+    def build(self):
+        button = Button(size_hint=(None, None), label='plop')
+        button.bind(on_press=self.animate)
+        return button
+
+TestApp().run()
+
