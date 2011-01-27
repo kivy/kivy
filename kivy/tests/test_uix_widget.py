3afa16e82ad36c3eae822e5db217d0cded521903
Mathieu Virbel <mathieu@pymt.eu>
2011-01-27 18:09:56 +0100

    Slider widget fixes - add documentation - add orientation property (horizontal/vertical) - add padding (used for internal cursor) - better usage of properties - unit tests

--- kivy/tests/test_uix_widget.py
+++ kivy/tests/test_uix_widget.py
@@ -22,10 +22,14 @@ class UIXWidgetTestCase(GraphicUnitTest):
     def test_slider_properties(self):
         from kivy.uix.slider import Slider
         r = self.render
-        r(Slider(value=25), 2)
-        r(Slider(value=50), 2)
-        r(Slider(value=100), 2)
-        r(Slider(min=-100, max=100, value=0), 2)
+        r(Slider(value=25))
+        r(Slider(value=50))
+        r(Slider(value=100))
+        r(Slider(min=-100, max=100, value=0))
+        r(Slider(orientation='vertical', value=25))
+        r(Slider(orientation='vertical', value=50))
+        r(Slider(orientation='vertical', value=100))
+        r(Slider(orientation='vertical', min=-100, max=100, value=0))
 
     def test_image_properties(self):
         from kivy.uix.image import Image
