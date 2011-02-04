068b6700268a5680bb046c7583c8b2f85c9f1a18
Mathieu Virbel <mat@pymt.eu>
2011-02-04 03:28:30 +0100

    introduce FloatLayout, a layout that just respect the size_hint without touching the position.

---
+++ kivy/uix/floatlayout.py
@@ -0,0 +1,81 @@
+'''
+Float Layout
+============
+
+This layout don't arrange widgets, but will respect the size_hint attribute.
+Position of children will be not touched at all.
+
+For example, if you create a :class:`FloatLayout` with size of (300, 300)::
+
+    layout = FloatLayout(size=(300, 300))
+
+    # by default, all widgets have size_hint=(1, 1)
+    # So this button will have the same size as layout
+    button = Button(text='Hello world')
+    layout.add_widget(button)
+
+    # if you want to create a button to be the 50% of the layout width, and 25%
+    # of the layout height, and set position to 20, 20, you can do
+    button = Button(text='Hello world', size_hint=(.5, .25), pos=(20, 20))
+
+.. note::
+
+    This layout can be used to start an application. Most of time, you need to
+    want which size is your Window.
+
+'''
+
+__all__ = ('FloatLayout', )
+
+from kivy.clock import Clock
+from kivy.uix.layout import Layout
+from kivy.properties import NumericProperty, OptionProperty
+
+
+class FloatLayout(Layout):
+    '''Float layout class. See module documentation for more informations.
+    '''
+
+    def __init__(self, **kwargs):
+        kwargs.setdefault('size', (1, 1))
+        self._minimum_size = (0, 0)
+        super(FloatLayout, self).__init__(**kwargs)
+        self.bind(
+            children = self._trigger_layout,
+            size = self._trigger_layout)
+
+    def _trigger_layout(self, *largs):
+        Clock.unschedule(self._do_layout)
+        Clock.schedule_once(self._do_layout)
+
+    def update_minimum_size(self, *largs):
+        '''Calculates the minimum size of the layout.
+        '''
+        width = height = 0
+
+        for w in self.children:
+            shw, shh = w.size_hint
+            if isinstance(w, Layout):
+                _w, _h = w.minimum_size
+                if shw is None:
+                    width = max(_w, width)
+                if shh is not None:
+                    height = max(_h, height)
+            else:
+                if shw is None:
+                    width = max(w.width, width)
+                if shh is None:
+                    height = max(w.height, height)
+
+        self.minimum_size = (width, height)
+
+    def _do_layout(self, *largs):
+        # optimize layout by preventing looking at the same attribute in a loop
+        selfw, selfh = self.size
+        for c in self.children:
+            shw, shh = c.size_hint
+            if shw:
+                c.width = selfw * shw
+            if shh:
+                c.height = selfh * shh
+
