48d0267c3b513566ac4fd46894921dfc6a002158
Mathieu Virbel <mat@kivy.org>
2011-02-10 21:49:03 +0100

    shader: remove unused varying, attribute and uniform

--- kivy/data/glsl/default.fs
+++ kivy/data/glsl/default.fs
@@ -1,4 +1,3 @@
-
 #ifdef GL_ES
     precision highp float;
 #endif
@@ -6,13 +5,11 @@
 /* Outputs from the vertex shader */
 varying vec4 frag_color;
 varying vec2 tex_coord0;
-varying vec2 tex_coord1;
-varying vec2 tex_coord2;
 
 /* uniform texture samplers */
 uniform sampler2D texture0;
 
 void main (void){
-    //gl_FragColor = vec4(tex_coord0.xy, 1.0,1.0);
-    gl_FragColor = frag_color * texture2D( texture0, tex_coord0);
+    gl_FragColor = frag_color * texture2D(texture0, tex_coord0);
 }
+
