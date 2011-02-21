ecfaa94054d3c442880be290b615a0631b706c39
Christopher Denter <christopher@kivy.org>
2011-02-21 19:57:15 +0100

    Fix keybinding keyboard handler

--- kivy/modules/keybinding.py
+++ kivy/modules/keybinding.py
@@ -10,12 +10,15 @@ Activate this module to map some keys to useful functions
 
 __all__ = ('start', 'stop')
 
-def _on_keyboard_handler(instance, key, scancode, unicode):
+
+def _on_keyboard_handler(instance, key, scancode, unicode, modifier):
     if key == 293: # F12
         instance.screenshot()
 
+
 def start(win, ctx):
     win.bind(on_keyboard=_on_keyboard_handler)
+
 
 def stop(win, ctx):
     win.unbind(on_keyboard=_on_keyboard_handler)
