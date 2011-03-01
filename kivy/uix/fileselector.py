478effe238a964ecb00fc36d461b6f07cb24b270
Gabriel Pettier <gabriel.pettier@gmail.com>
2011-03-01 01:40:48 +0100

    first fileselector implementation

--- kivy/uix/fileselector.py
+++ kivy/uix/fileselector.py
@@ -7,13 +7,31 @@ from kivy.uix.gridlayout import GridLayout
 from os import listdir
 from os.path import isdir
 
+
 class FileSelectorException(Exception):
     pass
+
 
 def alpha_sort(stringlist):
     return sorted(stringlist)
 
+
+def filetype_sort(stringlist):
+    #TODO
+    raise NotImplementedError
+
+
+class file(object):
+
+    def __init__(self, name):
+        self.name = name
+
+    def call(self):
+        pass
+
+
 class FileSelector(Widget):
+
     def __init__(self, *largs):
         Widget.__init__(self)
         self.cd = '.'
@@ -24,9 +42,15 @@ class FileSelector(Widget):
         def cd(self, path):
             if isdir(path):
                 self.path = path
+                self.update_display()
             else:
                 raise FileSelectorException("invalid path "+path)
 
+        def update_display(self):
+            self.grid.clear_widgets()
+            for f in self.ls():
+                button = Button(text=f, on_press=)
+                self.grid.add_widget(button)
 
         def ls(self):
             return self.sort(listdir(self.path))
