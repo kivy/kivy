6f0bae758f94f26c841c7559a6bad067c0c4f48b
Christopher Denter <christopher@kivy.org>
2011-03-19 18:27:42 +0100

    docs: Reword kivy.app

--- kivy/app.py
+++ kivy/app.py
@@ -3,14 +3,14 @@ Application
 ===========
 
 The :class:`App` class is the base for creating Kivy applications.
-Think of it as your main entry point into the Kivy runloop.  In most cases, you
+Think of it as your main entry point into the Kivy run loop.  In most cases, you
 subclass this class and make your own app. You create an instance of your
 specific app class and then, when you are ready to start the application's life
 cycle, you call your instance's :func:`App.run` method.
 
 
-Create an application by overidding build()
--------------------------------------------
+Creating an Application by Overriding build()
+---------------------------------------------
 
 To initialize your app with a widget tree, override the build() method in
 your app class and return the widget tree you constructed.
@@ -20,40 +20,33 @@ Here's an example of very simple application that just shows a button:
 .. include:: ../examples/application/app_with_build.py
    :literal:
 
-
 The file is also available in the examples folder at
 :file:`kivy/examples/application/app_with_build.py`.
 
+Here, no widget tree was constructed (or if you will, a tree with only the root
+node).
 
-Create an application with kv file
-----------------------------------
+
+Creating an Application via kv File
+------------------------------------
 
 You can also use the :doc:`api-kivy.lang` for creating application. The .kv can
 contain rules and root widget definitions at the same time. Here is the same
 example as the Button one in a kv file.
 
-Content of 'test.kv'::
+Contents of 'test.kv':
 
-    #:kivy 1.0
+.. include:: ../examples/application/test.kv
+   :literal:
 
-    Button:
-        text: 'Hello world'
+Contents of 'main.py':
 
+.. include:: ../examples/application/app_with_kv.py
+   :literal:
 
-Content of 'main.py'::
-
-    from kivy.app import App
-
-    class TestApp(App):
-        pass
-
-    if __name__ == '__main__':
-        TestApp().run()
-
-Check :file:`kivy/examples/application/app_with_kv.py`.
+See :file:`kivy/examples/application/app_with_kv.py`.
 
 The relation between main.py and test.kv is explained in :func:`App.load_kv`.
-
 '''
 
 from inspect import getfile
@@ -64,14 +57,14 @@ from kivy.lang import Builder
 
 
 class App(EventDispatcher):
-    ''' Application class, see module documentation for more informations.
+    ''' Application class, see module documentation for more information.
 
     :Events:
         `on_start`:
-            Fired when the application is beeing started (before the
+            Fired when the application is being started (before the
             :func:`~kivy.base.runTouchApp` call.
         `on_stop`:
-            Fired when the application stop.
+            Fired when the application stops.
     '''
 
     def __init__(self, **kwargs):
@@ -82,31 +75,34 @@ class App(EventDispatcher):
         self.use_default_uxl = kwargs.get('use_default_uxl', True)
         self.built = False
 
-        #: Root widget setted by the :func:`build` method or by the
-        #: :func:`load_kv` method if the kv file return a root widget.
+        #: Root widget set by the :func:`build` method or by the
+        #: :func:`load_kv` method if the kv file contains a root widget.
         self.root = None
 
     def build(self):
-        '''Initializes the application, will be called only once.
+        '''Initializes the application; will be called only once.
         If this method returns a widget (tree), it will be used as the root
         widget and added to the window.
         '''
         pass
 
     def load_kv(self):
-        '''If the application have never been built, try to found the kv of the
-        application in the same directory as the application class.
+        '''This method is invoked the first time the app is being run if no
+        widget tree has been constructed before for this app.
+        This method then looks for a matching kv file in the same directory as
+        the file that contains the application class.
 
         For example, if you have a file named main.py that contains::
 
             class ShowcaseApp(App):
                 pass
 
-        The :func:`load_kv` will search for a filename named `showcase.kv` in
-        the directory of the main.py. The name of the kv file is the lower name
-        of the class, without the App at the end if exist.
+        This method will search for a file named `showcase.kv` in
+        the directory that contains main.py. The name of the kv file has to be
+        the lowercase name of the class, without the 'App' postfix at the end
+        if it exists.
 
-        You can define rules and root widget in your kv file::
+        You can define rules and a root widget in your kv file::
 
             <ClassName>: # this is a rule
                 ...
@@ -114,9 +110,10 @@ class App(EventDispatcher):
             ClassName: # this is a root widget
                 ...
 
-        You cannot declare a root widget twice. Check :doc:`api-kivy.lang`
-        documentation for more information about how to create kv files. If your
-        kv file return a root widget, it will be set in self.root
+        There must be only one root widget. See the :doc:`api-kivy.lang`
+        documentation for more information on how to create kv files. If your
+        kv file contains a root widget, it will be used as self.root, the root
+        widget for the application.
         '''
         directory = dirname(getfile(self.__class__))
         clsname = self.__class__.__name__
@@ -147,8 +144,8 @@ class App(EventDispatcher):
     def stop(self, *largs):
         '''Stop the application.
 
-        If you use this method, the whole application will stop by using
-        :func:`~kivy.base.stopTouchApp` call.
+        If you use this method, the whole application will stop by issuing
+        a call to :func:`~kivy.base.stopTouchApp`.
         '''
         stopTouchApp()
 
