ac71d6634084ec72cbf7fe40c67482df4b336e5d
Mathieu Virbel <mat@kivy.org>
2011-03-23 00:50:20 +0100

    graphics: add stencil instructions (StencilPush, StencilPop, StencilUse), and support stacking up to 8 stencil layers.

--- kivy/graphics/__init__.py
+++ kivy/graphics/__init__.py
@@ -41,4 +41,5 @@ Compilation
 from kivy.graphics.instructions import *
 from kivy.graphics.context_instructions import *
 from kivy.graphics.vertex_instructions import *
+from kivy.graphics.stencil_instructions import *
 from kivy.graphics.fbo import Fbo
