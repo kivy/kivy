de4f4bdf021dda0c72377d86f2b43517848dbec3
Mathieu Virbel <mat@kivy.org>
2011-03-24 18:19:35 +0100

    scrollview: rework scrollview example to also show some debug about viewport and content

--- examples/widgets/scrollview.py
+++ examples/widgets/scrollview.py
@@ -4,19 +4,22 @@ from kivy.uix.button import Button
 from kivy.uix.scrollview import ScrollView
 from kivy.uix.gridlayout import GridLayout
 
-class TestApp(App):
+
+class ScrollViewApp(App):
+
     def build(self):
-        layout = GridLayout(rows=10,cols=10,spacing=20)
-        for i in range(100):
-            btn = Button(text=str(i))
+        layout = GridLayout(cols=1, spacing=10, size_hint=(None, None),
+                            width=500)
+        for i in range(30):
+            btn = Button(text=str(i), height=40, width=500)
             layout.add_widget(btn)
 
         root = ScrollView(size_hint=(None, None))
-        root.size = (480,320)
+        root.size = (480, 320)
         root.center = Window.center
         root.add_widget(layout)
 
         return root
 
 if __name__ == '__main__':
-    TestApp().run()
+    ScrollViewApp().run()
