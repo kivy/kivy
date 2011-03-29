e63a5f047bd713eaa5f83ebfc027baf3f9d55325
Mathieu Virbel <mat@kivy.org>
2011-03-29 18:23:12 +0200

    examples: add a very short example with scatter + video, and include trailer of big buck bunny. (sorry about that 3mo file, but that's the smallest and open source interesting video i've known ^^)

---
+++ examples/widgets/videoplayer.py
@@ -0,0 +1,23 @@
+import kivy
+kivy.require('1.0.4')
+
+from os.path import dirname, join
+from kivy.app import App
+from kivy.uix.video import Video
+from kivy.uix.scatter import Scatter
+
+
+class VideoPlayer(App):
+
+    def build(self):
+        curdir = dirname(__file__)
+        filename = join(curdir, 'bigbuckbunny_trailer.m4v')
+        video = Video(source=filename, play=True)
+        scatter = Scatter()
+        video.bind(texture_size=scatter.setter('size'))
+        scatter.add_widget(video)
+        return scatter
+
+
+if __name__ in ('__main__', '__android__'):
+    VideoPlayer().run()
