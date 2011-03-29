ee15a6cfc7f361d900c491a60a2e7a163998de23
Mathieu Virbel <mat@kivy.org>
2011-03-29 19:41:25 +0200

    videoplayer: add the possibility to test other video from commandline

--- examples/widgets/videoplayer.py
+++ examples/widgets/videoplayer.py
@@ -1,6 +1,7 @@
 import kivy
 kivy.require('1.0.4')
 
+from sys import argv
 from os.path import dirname, join
 from kivy.app import App
 from kivy.uix.video import Video
@@ -10,8 +11,11 @@ from kivy.uix.scatter import Scatter
 class VideoPlayer(App):
 
     def build(self):
-        curdir = dirname(__file__)
-        filename = join(curdir, 'bigbuckbunny_trailer.m4v')
+        if len(argv) > 1:
+            filename = argv[1]
+        else:
+            curdir = dirname(__file__)
+            filename = join(curdir, 'bigbuckbunny_trailer.m4v')
         video = Video(source=filename, play=True)
         scatter = Scatter()
         video.bind(texture_size=scatter.setter('size'))
