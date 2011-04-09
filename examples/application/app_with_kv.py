1db648bcd22de561d449e202f85b4b41abc2357f
George Sebastian <george.sebastian@ieee.org>
2011-04-09 11:49:35 +0530

    Fixes on the example

--- examples/application/app_with_kv.py
+++ examples/application/app_with_kv.py
@@ -1,13 +1,14 @@
 from kivy.app import App
 import sys
+
+
 class TestApp(App):
     pass
 
 if __name__ == '__main__':
-  ' Add template1/ or template2/ to the command line, to load different kv files'
+    ''' Add template1/ or template2/
+    to the command line to load different kv files'''
     if sys.argv.__len__() is 2:
         TestApp().run(sys.argv[1])
     else:
         print "Usage: "+sys.argv[0]+" [directory_of_kv_file]"
-        
-        
