7e41fb5274229cd584251d827f7ea51495671d8b
George Sebastian <george.sebastian@ieee.org>
2011-04-09 12:06:20 +0530

    Fixes on the example

--- examples/application/app_with_kv.py
+++ examples/application/app_with_kv.py
@@ -10,5 +10,7 @@ if __name__ == '__main__':
     to the command line to load different kv files'''
     if sys.argv.__len__() is 2:
         TestApp().run(sys.argv[1])
+    else if sys.argv.__len__() is 1:
+        TestApp().run()
     else:
         print "Usage: "+sys.argv[0]+" [directory_of_kv_file]"
