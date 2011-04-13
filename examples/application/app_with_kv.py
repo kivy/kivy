78090b3978074da939d09babef8064ead1771068
George Sebastian <george.sebastian@ieee.org>
2011-04-14 00:54:31 +0530

    Added an example

--- examples/application/app_with_kv.py
+++ examples/application/app_with_kv.py
@@ -1,7 +1,14 @@
 from kivy.app import App
+import sys
 
 class TestApp(App):
     pass
 
 if __name__ == '__main__':
-    TestApp().run()
+    ''' Give commandline argument of "template1" or "template2" 
+    to use the templates in respective directories
+    '''
+    if sys.argv.__len__() is 2:
+        TestApp().run(sys.argv[1])
+    else:
+        TestApp().run()
