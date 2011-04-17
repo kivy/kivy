cd47a83d1725ca7db86925473dd50210d6f25edd
George Sebastian <george.sebastian@ieee.org>
2011-04-17 19:16:25 +0530

    Style fixes

--- examples/application/app_with_kv.py
+++ examples/application/app_with_kv.py
@@ -1,14 +1,17 @@
 from kivy.app import App
 import sys
+from kivy.lang import Builder
+
 
 class TestApp(App):
     pass
+#    def load_kv(self):
+#        root=Builder.load_file('template1/test.kv')
+#        if root:
+#          self.root = root
 
 if __name__ == '__main__':
-    ''' Give commandline argument of "template1" or "template2" 
+    ''' Give commandline argument of "template1" or "template2"
     to use the templates in respective directories
     '''
-    if sys.argv.__len__() is 2:
-        TestApp().run(sys.argv[1])
-    else:
-        TestApp().run()
+    TestApp().run()
