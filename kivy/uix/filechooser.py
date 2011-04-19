89e11c3073353c5a67fd60d6a884bedecc49c0e1
Christopher Denter <christopher@kivy.org>
2011-04-19 16:47:43 +0200

    filechooser: fix imports

--- kivy/uix/filechooser.py
+++ kivy/uix/filechooser.py
@@ -26,7 +26,7 @@ from kivy.properties import StringProperty, ListProperty, BooleanProperty, \
 from sys import platform
 from os import getcwdu, listdir
 from os.path import basename, getsize, isdir, join, sep, normpath, dirname, \
-                    samefile, expanduser, altsep
+                    expanduser, altsep, splitdrive
 from fnmatch import fnmatch
 
 
@@ -218,7 +218,7 @@ class FileChooserController(FloatLayout):
         if platform == 'win32':
             is_root = splitdrive()[1] in (sep, altsep)
         elif platform in ('darwin', 'linux2'):
-            is_root = samefile(expanduser(self.path), sep)
+            is_root = normpath(expanduser(self.path)) == sep
         else:
             # Unknown file system; Just always add the .. entry but also log
             Logger.warning('Filechooser: Unsupported OS: %r' % platform)
