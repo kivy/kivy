fc2db98d335996afdf728a038d784afdedbdfeda
Christopher Denter <christopher@kivy.org>
2011-07-03 17:03:15 +0200

    osx-support: sdl window: handle modifiers; remove cruft; wip

--- kivy/core/window/window_sdl.py
+++ kivy/core/window/window_sdl.py
@@ -14,8 +14,18 @@ import sys
 try:
     from kivy.core import sdl
 except:
-    Logger.warning('WinPygame: SDL wrapper failed to import!')
+    Logger.warning('WindowSDL: SDL wrapper failed to import!')
     raise
+
+
+L_SHIFT = 1
+R_SHIFT = 2
+L_CTRL = 64
+R_CTRL = 128
+L_ALT = 256
+R_ALT = 512
+L_META = 1024
+R_META = 2048
 
 
 class WindowSDL(WindowBase):
@@ -43,7 +53,7 @@ class WindowSDL(WindowBase):
         use_fullscreen = False
         self._fullscreenmode = params['fullscreen']
         if self._fullscreenmode == 'fake':
-            Logger.debug('WinPygame: Set window to fake fullscreen mode')
+            Logger.debug('WindowSDL: Set window to fake fullscreen mode')
             use_fake = True
             # if no position set, in fake mode, we always need to set the
             # position. so replace 0, 0.
@@ -52,7 +62,7 @@ class WindowSDL(WindowBase):
             os.environ['SDL_VIDEO_WINDOW_POS'] = '%d,%d' % self._pos
 
         elif self._fullscreenmode:
-            Logger.debug('WinPygame: Set window to fullscreen mode')
+            Logger.debug('WindowSDL: Set window to fullscreen mode')
             use_fullscreen = True
 
         elif self._pos is not None:
@@ -95,36 +105,7 @@ class WindowSDL(WindowBase):
         sdl.teardown_window()
         self.dispatch('on_close')
 
-    def set_title(self, title):
-        return
-        pygame.display.set_caption(title)
-
-    def set_icon(self, filename):
-        return
-        try:
-            if not exists(filename):
-                return False
-            icon = pygame.image.load(filename)
-            pygame.display.set_icon(icon)
-        except:
-            Logger.exception('WinPygame: unable to set icon')
-
-    def screenshot(self, *largs, **kwargs):
-        return
-        filename = super(WindowPygame, self).screenshot(*largs, **kwargs)
-        if filename is None:
-            return None
-        from kivy.core.gl import glReadPixels, GL_RGB, GL_UNSIGNED_BYTE
-        width, height = self.size
-        data = glReadPixels(0, 0, width, height, GL_RGB, GL_UNSIGNED_BYTE)
-        data = str(buffer(data))
-        surface = pygame.image.fromstring(data, self.size, 'RGB', True)
-        pygame.image.save(surface, filename)
-        Logger.debug('Window: Screenshot saved at <%s>' % filename)
-        return filename
-
     def on_keyboard(self, key, scancode=None, unicode=None, modifier=None):
-        return
         # Quit if user presses ESC or the typical OSX shortcuts CMD+q or CMD+w
         # TODO If just CMD+w is pressed, only the window should be closed.
         is_osx = sys.platform == 'darwin'
@@ -132,7 +113,7 @@ class WindowSDL(WindowBase):
             stopTouchApp()
             self.close()  #not sure what to do here
             return True
-        super(WindowPygame, self).on_keyboard(key, scancode, unicode, modifier)
+        super(WindowSDL, self).on_keyboard(key, scancode, unicode, modifier)
 
     def flip(self):
         sdl.flip()
@@ -148,7 +129,6 @@ class WindowSDL(WindowBase):
             if event is None:
                 continue
 
-            #print 'sdl receive', event
             action, args = event[0], event[1:]
             if action == 'quit':
                 EventLoop.quit = True
@@ -185,41 +165,17 @@ class WindowSDL(WindowBase):
 
             elif action in ('keydown', 'keyup'):
                 mod, key, scancode, unicode = args
-                self._pygame_update_modifiers(mod)
+                self._update_modifiers(mod)
                 if action == 'keyup':
                     self.dispatch('on_key_up', key, scancode)
                     continue
 
                 # don't dispatch more key if down event is accepted
-                if self.dispatch('on_key_down', key,
-                                 scancode, unicode,
+                if self.dispatch('on_key_down', key, scancode, unichr(unicode),
                                  self.modifiers):
                     continue
-                self.dispatch('on_keyboard', key,
-                              scancode, unicode,
+                self.dispatch('on_keyboard', key, scancode, unichr(unicode),
                               self.modifiers)
-
-            elif action == 'textinput':
-                self.dispatch('on_keyboard', None, None, args[0],
-                              self.modifiers)
-        #    # video resize
-        #    elif event.type == pygame.VIDEORESIZE:
-        #        self._size = event.size
-        #        # don't use trigger here, we want to delay the resize event
-        #        cb = self._do_resize
-        #        Clock.unschedule(cb)
-        #        Clock.schedule_once(cb, .1)
-
-        #    elif event.type == pygame.VIDEOEXPOSE:
-        #        self.canvas.ask_update()
-
-        #    # ignored event
-        #    elif event.type == pygame.ACTIVEEVENT:
-        #        pass
-
-        #    # unhandled event !
-        #    else:
-        #        Logger.debug('WinPygame: Unhandled event %s' % str(event))
 
     def _do_resize(self, dt):
         Logger.debug('Window: Resize window to %s' % str(self._size))
@@ -246,33 +202,13 @@ class WindowSDL(WindowBase):
         # force deletion of window
         sdl.teardown_window()
 
-    #
-    # Pygame wrapper
-    #
-    def _pygame_update_modifiers(self, mods=None):
-        return
-        # Available mod, from dir(pygame)
-        # 'KMOD_ALT', 'KMOD_CAPS', 'KMOD_CTRL', 'KMOD_LALT',
-        # 'KMOD_LCTRL', 'KMOD_LMETA', 'KMOD_LSHIFT', 'KMOD_META',
-        # 'KMOD_MODE', 'KMOD_NONE'
-        if mods is None:
-            mods = pygame.key.get_mods()
+    def _update_modifiers(self, mods=None):
         self._modifiers = []
-        if mods & (pygame.KMOD_SHIFT | pygame.KMOD_LSHIFT):
+        if mods & (L_SHIFT | R_SHIFT):
             self._modifiers.append('shift')
-        if mods & (pygame.KMOD_ALT | pygame.KMOD_LALT):
+        if mods & (L_ALT | R_ALT):
             self._modifiers.append('alt')
-        if mods & (pygame.KMOD_CTRL | pygame.KMOD_LCTRL):
+        if mods & (L_CTRL | R_CTRL):
             self._modifiers.append('ctrl')
-        if mods & (pygame.KMOD_META | pygame.KMOD_LMETA):
+        if mods & (L_META | R_META):
             self._modifiers.append('meta')
-
-    def on_keyboard(self, key, scancode=None, unicode=None, modifier=None):
-        # Quit if user presses ESC or the typical OSX shortcuts CMD+q or CMD+w
-        # TODO If just CMD+w is pressed, only the window should be closed.
-        is_osx = sys.platform == 'darwin'
-        if key == 27 or (is_osx and key in (113, 119) and modifier == 1024):
-            stopTouchApp()
-            self.close()  #not sure what to do here
-            return True
-        super(WindowSDL, self).on_keyboard(key, scancode, unicode, modifier)
