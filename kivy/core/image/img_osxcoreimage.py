f046b3e27a45ed8592bf604482b507d9b75390c2
Christopher Denter <christopher@kivy.org>
2011-07-14 15:40:42 +0200

    osx-support: image support: properly return list of supported formats, remove cruft

--- kivy/core/image/img_osxcoreimage.py
+++ kivy/core/image/img_osxcoreimage.py
@@ -19,25 +19,19 @@ class ImageLoaderOSXCoreImage(ImageLoaderBase):
     @staticmethod
     def extensions():
         '''Return accepted extension for this loader'''
-        # See http://www.pythonware.com/library/pil/handbook/index.htm
-        # XXX
-        return ('bmp', 'bufr', 'cur', 'dcx', 'fits', 'fl', 'fpx', 'gbr',
-                'gd', 'gif', 'grib', 'hdf5', 'ico', 'im', 'imt', 'iptc',
-                'jpeg', 'jpg', 'mcidas', 'mic', 'mpeg', 'msp', 'pcd',
-                'pcx', 'pixar', 'png', 'ppm', 'psd', 'sgi', 'spider',
-                'tga', 'tiff', 'wal', 'wmf', 'xbm', 'xpm', 'xv')
+        # See "Supported Image File Formats" in the Cocoa drawing guide
+        sup = ['eps', 'epi', 'epsf', 'epsi', 'ps', 'tiff', 'tif', 'jpg',
+               'jpeg', 'jpe', 'gif', 'png', 'pict', 'pct', 'pic', 'bmp',
+               'BMPf', 'ico', 'icns']
+        sup_up = [format.upper() for format in sup]
+        return sup + sup_up
 
     def load(self, filename):
-        #ret = osxcoreimage.load_raw_image_data(filename)
         ret = osxcoreimage.load_image_data(filename)
         if ret is None:
             Logger.warning('Image: Unable to load image <%s>' % filename)
             raise Exception('Unable to load image')
         w, h, imgtype, data = ret
-        print 'loading image:', filename, w, h, imgtype, 'size:', len(data)
-        if 'testpattern' in filename:
-            colors = map(lambda c: ord(c) / 255.0, data)
-            print colors, '================================' * 5, repr(data)
         return ImageData(w, h, imgtype, data)
 
 
