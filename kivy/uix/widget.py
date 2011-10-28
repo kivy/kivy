219474c5153e820fe8ab4a787bb623796f7386fb
Mathieu Virbel <mat@kivy.org>
2011-10-28 10:29:22 +0200

    uix: ensure that event are registered before calling the super()

--- kivy/uix/widget.py
+++ kivy/uix/widget.py
@@ -130,12 +130,12 @@ class Widget(EventDispatcher):
         return self
 
     def __init__(self, **kwargs):
-        super(Widget, self).__init__()
-
         # Register touch events
         self.register_event_type('on_touch_down')
         self.register_event_type('on_touch_move')
         self.register_event_type('on_touch_up')
+
+        super(Widget, self).__init__()
 
         # Before doing anything, ensure the windows exist.
         EventLoop.ensure_window()
