34a33458725a96e043e7af6422e63216baf6cb6f
Mathieu Virbel <mat@kivy.org>
2012-01-25 19:06:10 +0100

    doc: rework widget page

--- kivy/uix/__init__.py
+++ kivy/uix/__init__.py
@@ -3,12 +3,37 @@ Widgets
 =======
 
 A widget is an element of a graphical user interface.
-
 The `kivy.uix` module contains classes for creating and arranging Widgets. 
 
-If you don't know anything about widgets, you should start by reading the
-:class:`~kivy.uix.widget.Widget` documentation.
+Read first: :doc:`api-kivy.uix.widget`
 
-If you need to arrange widgets on the screen, start by reading the
-:class:`~kivy.uix.layout.Layout` documentation.
+- **UX widgets**: Classical user interface widgets, perfect and ready to be
+  assembled for complex widgets.
+
+    :doc:`api-kivy.uix.label`, :doc:`api-kivy.uix.button`,
+    :doc:`api-kivy.uix.image`, :doc:`api-kivy.uix.slider`,
+    :doc:`api-kivy.uix.progressbar`, :doc:`api-kivy.uix.textinput`,
+    :doc:`api-kivy.uix.togglebutton`, :doc:`api-kivy.uix.switch`
+
+- **Layouts**: A layout widget have no rendering, just a trigger
+  that will arrange its children in a specific way. Read more on
+  :doc:`api-kivy.uix.layout`
+
+    :doc:`api-kivy.uix.gridlayout`, :doc:`api-kivy.uix.boxlayout`,
+    :doc:`api-kivy.uix.anchorlayout`, :doc:`api-kivy.uix.stacklayout`,
+
+- **Complex UX widgets**: Non-atomic widgets, result of classic widget
+  assembly. We call them complex because the assembly and usage are not as
+  generic as the classicals widgets.
+
+    :doc:`api-kivy.uix.bubble`,
+    :doc:`api-kivy.uix.filechooser`, :doc:`api-kivy.uix.popup`,
+    :doc:`api-kivy.uix.vkeyboard`
+
+- **Behaviors widgets**: Theses widgets have no rendering, but act the graphics
+  part, or even on the touch part.
+
+    :doc:`api-kivy.uix.scatter`, :doc:`api-kivy.uix.stencilview`
+
+----
 '''
