a87b69af0b28c6ddffee7976255349790cff6fe3
gabriel.pettier <gabriel.pettier@gmail.com>
2012-03-19 20:14:54 +0100

    combobox: added example in module docstringe, pep8 fixes

--- examples/widgets/combobox.py
+++ examples/widgets/combobox.py
@@ -1,8 +1,6 @@
 from kivy.app import App
 from kivy.uix.combobox import ComboBox
-from kivy.uix.label import Label
 from kivy.uix.floatlayout import FloatLayout
-
 
 class ComboBoxTest(App):
 
@@ -13,15 +11,14 @@ class ComboBoxTest(App):
                 pos_hint={'center_x': .5, 'center_y': .7},
                 size_hint=(None, None),
                 size=(200, 40),
-                selected_color=(1, .2, .2, 1)
-        )
+                selected_color=(1, .2, .2, 1))
+
         c.bind(text=self.text_changed,
                current_index=self.index_changed,
-               highlight_index=self.on_highlight
-        )
+               highlight_index=self.on_highlight)
 
         f.add_widget(c)
-        return f 
+        return f
 
     def text_changed(self, instance, value):
         print 'Text changed: %s' % value
