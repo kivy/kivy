08765bf38b7b522d3cbc4da35b7311f822c8e714
Michele Lacchia <michelelacchia@gmail.com>
2012-04-08 09:31:36 +0200

    PEP8 fixes for test_multistroke.py

--- kivy/tests/test_multistroke.py
+++ kivy/tests/test_multistroke.py
@@ -3,40 +3,44 @@ import kivy.multistroke
 from kivy.multistroke import GPoint, Recognizer, Multistroke
 
 # Not exactly sure how to switch between them and run tests twice?
-PERFECT_INVAR   = 67108864 # depends on /.0001**2 on perfect match
-PERFECT_BOUND   = 1
+PERFECT_INVAR = 67108864  # depends on /.0001**2 on perfect match
+PERFECT_BOUND = 1
 CANDIDATE_SCORE = 6.175
-T_MISS_SCORE    = 1.25
+T_MISS_SCORE = 1.25
 
 best_score = 0.0
+counter = 0
+
+
 def best_score_cb(result):
     global best_score
     best_score = result.best['score']
 
-counter = 0
+
 def counter_cb(result):
     global counter
     counter += 1
 
 # These are taken from the examples in JavaScript code (but made unistrokes)
-TGesture = [ GPoint(30, 7),   GPoint(103, 7), GPoint(66, 7),  GPoint(66, 87) ]
-NGesture = [ GPoint(177, 92), GPoint(177, 2), GPoint(182, 1), GPoint(246, 95),
-             GPoint(247, 87), GPoint(247, 1) ]
+TGesture = [GPoint(30, 7), GPoint(103, 7), GPoint(66, 7), GPoint(66, 87)]
+NGesture = [GPoint(177, 92), GPoint(177, 2), GPoint(182, 1), GPoint(246, 95),
+             GPoint(247, 87), GPoint(247, 1)]
 
 # dataset that matches N pretty well
 Ncandidate = [
-    GPoint(160,271), GPoint(160,263), GPoint(158,257), GPoint(156,249), 
-    GPoint(146,187), GPoint(144,181), GPoint(144,175), GPoint(142,167),
-    GPoint(140,113), GPoint(140,107), GPoint(140,103), GPoint(140,99),  
-    GPoint(140,85),  GPoint(138,85),  GPoint(138,87),  GPoint(138,89),  
-    GPoint(166,151), GPoint(176,171), GPoint(188,189), GPoint(200,205),
-    GPoint(238,263), GPoint(242,269), GPoint(244,273), GPoint(246,277), 
-    GPoint(252,289), GPoint(254,291), GPoint(256,291), GPoint(258,291), 
-    GPoint(260,281), GPoint(260,275), GPoint(260,267), GPoint(260,255),
-    GPoint(254,189), GPoint(254,175), GPoint(254,161), GPoint(254,147), 
-    GPoint(260,103), GPoint(260,101), GPoint(260,99),  GPoint(260,95),  
-    GPoint(260,93),  GPoint(260,91),  GPoint(260,89)
+    GPoint(160, 271), GPoint(160, 263), GPoint(158, 257), GPoint(156, 249),
+    GPoint(146, 187), GPoint(144, 181), GPoint(144, 175), GPoint(142, 167),
+    GPoint(140, 113), GPoint(140, 107), GPoint(140, 103), GPoint(140, 99),
+    GPoint(140, 85),  GPoint(138, 85),  GPoint(138, 87),  GPoint(138, 89),
+    GPoint(166, 151), GPoint(176, 171), GPoint(188, 189), GPoint(200, 205),
+    GPoint(238, 263), GPoint(242, 269), GPoint(244, 273), GPoint(246, 277),
+    GPoint(252, 289), GPoint(254, 291), GPoint(256, 291), GPoint(258, 291),
+    GPoint(260, 281), GPoint(260, 275), GPoint(260, 267), GPoint(260, 255),
+    GPoint(254, 189), GPoint(254, 175), GPoint(254, 161), GPoint(254, 147),
+    GPoint(260, 103), GPoint(260, 101), GPoint(260, 99),  GPoint(260, 95),
+    GPoint(260, 93),  GPoint(260, 91),  GPoint(260, 89)
 ]
+
 
 class MultistrokeTestCase(unittest.TestCase):
 
@@ -44,12 +48,12 @@ class MultistrokeTestCase(unittest.TestCase):
         global best_score
         best_score = 0
         counter = 0
-        
+
         self.Tinvar = Multistroke('T', [TGesture], orientation_dep=False)
         self.Tbound = Multistroke('T', [TGesture], orientation_dep=True)
         self.Ninvar = Multistroke('N', [NGesture], orientation_dep=False)
         self.Nbound = Multistroke('N', [NGesture], orientation_dep=True)
-        
+
 # -----------------------------------------------------------------------------
 # Recognizer scheduling
 # -----------------------------------------------------------------------------
@@ -60,7 +64,7 @@ class MultistrokeTestCase(unittest.TestCase):
         self.assertEqual(r._completed, 2)
         self.assertEqual(r.progress, 1)
         self.assertTrue(r.best['score'] > 0.91 and r.best['score'] < 0.92)
-        
+
     def test_scheduling(self):
         global best_score
         from kivy.clock import Clock
@@ -87,7 +91,7 @@ class MultistrokeTestCase(unittest.TestCase):
         best_score = 0
         gdb.db.append(self.Ninvar)
         r = gdb.recognize([Ncandidate], max_gpf=1, use_protractor=False)
-        r.bind(on_complete = best_score_cb)
+        r.bind(on_complete=best_score_cb)
         self.assertEqual(r.progress, 0)
         Clock.tick()
         self.assertEqual(r.progress, 0.5)
@@ -99,13 +103,13 @@ class MultistrokeTestCase(unittest.TestCase):
         best_score = 0
         gdb.db.append(self.Ninvar)
         r = gdb.recognize([Ncandidate], max_gpf=1, use_protractor=False)
-        r.bind(on_complete = best_score_cb)
+        r.bind(on_complete=best_score_cb)
         self.assertEqual(r.progress, 0)
         Clock.tick()
-        self.assertEqual(r.progress, 1/3.)
+        self.assertEqual(r.progress, 1 / 3.)
 
         Clock.tick()
-        self.assertEqual(r.progress, 2/3.)
+        self.assertEqual(r.progress, 2 / 3.)
         self.assertEqual(best_score, 0)
 
         Clock.tick()
@@ -121,18 +125,18 @@ class MultistrokeTestCase(unittest.TestCase):
         for i in range(9):
             gdb.add_gesture('T', [TGesture], priority=50)
         gdb.add_gesture('N', [NGesture])
-        
+
         r1 = gdb.recognize([Ncandidate], max_gpf=1, use_protractor=False)
-        r1.bind(on_complete = counter_cb)
-        Clock.tick() # first run scheduled here; 9 left
+        r1.bind(on_complete=counter_cb)
+        Clock.tick()  # first run scheduled here; 9 left
 
         r2 = gdb.recognize([Ncandidate], max_gpf=1, use_protractor=False)
-        r2.bind(on_complete = counter_cb)
-        Clock.tick() # 8 left
+        r2.bind(on_complete=counter_cb)
+        Clock.tick()  # 8 left
 
         r3 = gdb.recognize([Ncandidate], max_gpf=1, use_protractor=False)
-        r3.bind(on_complete = counter_cb)
-        Clock.tick() # 7 left
+        r3.bind(on_complete=counter_cb)
+        Clock.tick()  # 7 left
 
         # run some immediate searches, should not interfere.
         for i in range(5):
@@ -159,10 +163,10 @@ class MultistrokeTestCase(unittest.TestCase):
         gdb = Recognizer([self.Tbound, self.Ninvar])
         r = gdb.recognize([Ncandidate], max_gpf=1, timeout=0.1,
                           use_protractor=False)
-        Clock.tick() # matches Tbound in this tick
+        Clock.tick()  # matches Tbound in this tick
         self.assertEqual(best_score, 0)
         sleep(0.11)
-        Clock.tick() # should match Ninv, but times out (got T)
+        Clock.tick()  # should match Ninv, but times out (got T)
         self.assertEqual(r.status, 'timeout')
         self.assertEqual(r.progress, .5)
         self.assertTrue(r.best['score'] < 0.5)
@@ -177,14 +181,14 @@ class MultistrokeTestCase(unittest.TestCase):
         r = gdb.recognize([Ncandidate], max_gpf=1, timeout=0.2,
                           use_protractor=False)
 
-        Clock.tick() # matches Tbound in this tick
+        Clock.tick()  # matches Tbound in this tick
         self.assertEqual(best_score, 0)
         sleep(0.1)
-        Clock.tick() # matches Ninvar in this tick
+        Clock.tick()  # matches Ninvar in this tick
         sleep(0.1)
-        Clock.tick() # should match Tinvar, but times out
+        Clock.tick()  # should match Tinvar, but times out
         self.assertEqual(r.status, 'timeout')
-        self.assertEqual(r.progress, 2/3.)
+        self.assertEqual(r.progress, 2 / 3.)
         self.assertTrue(r.best['score'] >= .91 and r.best['score'] <= .92)
 
     def test_priority_sorting(self):
@@ -217,7 +221,7 @@ class MultistrokeTestCase(unittest.TestCase):
         gdb.add_gesture('T', [TGesture], numpoints=100)
         n = gdb.filter(numpoints=100)
         self.assertEqual(len(n), 1)
-        n = gdb.filter(numpoints=[100,16])
+        n = gdb.filter(numpoints=[100, 16])
         self.assertEqual(len(n), 3)
 
     def test_numstrokes_filter(self):
@@ -228,7 +232,7 @@ class MultistrokeTestCase(unittest.TestCase):
         gdb.add_gesture('T', [TGesture, TGesture])
         n = gdb.filter(numstrokes=2)
         self.assertEqual(len(n), 1)
-        n = gdb.filter(numstrokes=[1,2])
+        n = gdb.filter(numstrokes=[1, 2])
         self.assertEqual(len(n), 3)
 
     def test_priority_filter(self):
@@ -275,11 +279,11 @@ class MultistrokeTestCase(unittest.TestCase):
 # misc tests
 # -----------------------------------------------------------------------------
     def test_resample(self):
-        r = kivy.multistroke.Resample([GPoint(0,0), GPoint(1,1)], 11)
+        r = kivy.multistroke.Resample([GPoint(0, 0), GPoint(1, 1)], 11)
         self.assertEqual(len(r), 11)
         self.assertEqual(round(r[9].x, 1), 0.9)
 
-        r = kivy.multistroke.Resample(TGesture, 25);
+        r = kivy.multistroke.Resample(TGesture, 25)
         self.assertEqual(len(r), 25)
         self.assertEqual(round(r[12].x), 81)
         self.assertEqual(r[12].y, 7)
@@ -398,7 +402,7 @@ class MultistrokeTestCase(unittest.TestCase):
         r = gdb.recognize([Ncandidate], orientation_dep=False, max_gpf=0,
                           use_protractor=False)
         self.assertTrue(r.best['score'] < 0.6)
-        
+
 # ------------------------------------------------------------------------
 # Test protractor search - .99/1 is not safe
 # ------------------------------------------------------------------------
@@ -418,7 +422,7 @@ class MultistrokeTestCase(unittest.TestCase):
                      use_protractor=True)
         self.assertEqual(r.best['name'], 'N')
         self.assertTrue(r.best['score'] >= CANDIDATE_SCORE)
-        self.assertTrue(r.best['score'] <= CANDIDATE_SCORE+0.1)
+        self.assertTrue(r.best['score'] <= CANDIDATE_SCORE + 0.1)
 
     def test_protractor_bound(self):
         gdb = Recognizer([self.Tbound, self.Nbound])
@@ -436,7 +440,7 @@ class MultistrokeTestCase(unittest.TestCase):
                      use_protractor=True)
         self.assertEqual(r.best['name'], 'N')
         self.assertTrue(r.best['score'] >= CANDIDATE_SCORE)
-        self.assertTrue(r.best['score'] <= CANDIDATE_SCORE+0.1)
+        self.assertTrue(r.best['score'] <= CANDIDATE_SCORE + 0.1)
 
     def test_phi_orientation_filter(self):
         gdb = Recognizer([self.Tbound])
