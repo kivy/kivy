5341e4f4b926cfc842cf823ab962404df5879d27
tshirtman <gabriel.pettier@gmail.com>
2012-06-07 16:28:02 +0200

    fix file path in test_image

--- kivy/tests/test_image.py
+++ kivy/tests/test_image.py
@@ -5,12 +5,15 @@ class ImageTestCase(unittest.TestCase):
 
     def setUp(self):
         from kivy.core.image import Image
+        import os
         self.cls = Image
-        self.root = Image('test_button.png')
+        self.image = os.path.join(os.path.dirname(__file__), 'test_button.png')
+        print self.image
+        self.root = Image(self.image)
 
     def test_keep_data(self):
         root = self.root
         self.assertEqual(root._image._data[0].data, None)
-        i1 = self.cls('test_button.png', keep_data = True)
+        i1 = self.cls(self.image, keep_data = True)
         if not i1._image._data[0].data:
             self.fail('Image has no data even with keep_data = True')
