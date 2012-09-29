297dad858cd01fedfccb51c4598467226e44ceb4
Mathieu Virbel <mat@kivy.org>
2012-09-29 13:37:40 +0200

    boxlayout: ensure that any change of the size/size_hint/pos_hint will update the boxlayout.

--- kivy/uix/boxlayout.py
+++ kivy/uix/boxlayout.py
@@ -190,3 +190,17 @@ class BoxLayout(Layout):
                 c.width = w
                 c.height = h
                 y += h + spacing
+
+    def add_widget(self, widget, index=0):
+        widget.bind(
+            size=self._trigger_layout,
+            size_hint=self._trigger_layout,
+            pos_hint=self._trigger_layout)
+        return super(Layout, self).add_widget(widget, index)
+
+    def remove_widget(self, widget):
+        widget.unbind(
+            size=self._trigger_layout,
+            size_hint=self._trigger_layout,
+            pos_hint=self._trigger_layout)
+        return super(Layout, self).remove_widget(widget)
