70891fce3e04a20191ef0d6028530ef3e2bf909f
Mathieu Virbel <mat@kivy.org>
2012-10-25 12:01:43 +0200

    firstwidget: update the firstwidget introduction to remove userdata, and use only touch.ud + enhance docs. #closes 726

--- examples/guide/firstwidget/3_draw_ellipse.py
+++ examples/guide/firstwidget/3_draw_ellipse.py
@@ -4,14 +4,16 @@ from kivy.graphics import Color, Ellipse
 
 
 class MyPaintWidget(Widget):
+
     def on_touch_down(self, touch):
         with self.canvas:
             Color(1, 1, 0)
             d = 30.
-            Ellipse(pos=(touch.x - d/2, touch.y - d/2), size=(d, d))
+            Ellipse(pos=(touch.x - d / 2, touch.y - d / 2), size=(d, d))
 
 
 class MyPaintApp(App):
+
     def build(self):
         return MyPaintWidget()
 
