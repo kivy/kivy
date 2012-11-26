450c8664f299412de7daaef5749fb427acdb0a1c
qua-non <akshayaurora@gmail.com>
2012-11-26 05:55:31 +0530

    UIX: fix loading with non default `arrow_pos`, closes #820

--- kivy/uix/bubble.py
+++ kivy/uix/bubble.py
@@ -223,6 +223,8 @@ class Bubble(GridLayout):
         self_arrow_img.pos = (0, 0)
 
         self.clear_widgets(do_super=True)
+        self_content.parent = None
+
         self_arrow_img.size_hint = (1, None)
         self_arrow_img.height = self_arrow_img.texture_size[1]
         widget_list = []
