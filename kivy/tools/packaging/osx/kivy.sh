9b402de1bd36133b648a3a56433c31af73870d10
Mathieu Virbel <mat@kivy.org>
2012-12-04 02:02:01 +0100

    osx: move to 2.7 by default.

--- kivy/tools/packaging/osx/kivy.sh
+++ kivy/tools/packaging/osx/kivy.sh
@@ -14,9 +14,9 @@ export GST_PLUGIN_PATH=${SCRIPT_PATH}/lib/gst-plugins:$GST_PLUGIN_PATH
 # That'd mean however that other libraries can't be found anymore. So if the user has
 # a broken pygame installation, it's not our fault...
 #
-# Also, hardcode the link to python 2.6 for now. If you don't want that, just adjust it.
+# Also, hardcode the link to python 2.7 for now. If you don't want that, just adjust it.
 # We're doing that because the situation of Python on OSX is less than ideal and users
 # tend to install Python from arbitrary resources. To ensure smooth execution, we're
 # enforcing the usage of the Python version that we actually built against.
 # If you know what you're doing you can easily change this.
-exec /System/Library/Frameworks/Python.framework/Versions/2.6/bin/python2.6 "$@"
+exec /System/Library/Frameworks/Python.framework/Versions/2.7/bin/python2.7 "$@"
