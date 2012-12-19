e4565fe64264b8618b27144f15fb8d43fa4b6509
Thomas Hansen <thomas.hansen@gmail.com>
2012-12-19 14:01:08 -0600

    - load texture coordinates and normals correctly even if missing (default vectors with 0.0) - added calculate_normals to MeshData class for calculating surface normals of mesh faces (not sure it works correctly)

--- examples/3Drendering/objloader.py
+++ examples/3Drendering/objloader.py
@@ -1,6 +1,3 @@
-
-
-
 class MeshData(object):
     def __init__(self, **kwargs):
         self.name = kwargs.get("name")
@@ -11,9 +8,35 @@ class MeshData(object):
         self.vertices = []
         self.indices = []
 
+    def calculate_normals(self):
+        for i in range(len(self.indices) / (3)):
+            fi = i * 3
+            v1i = self.indices[fi]
+            v2i = self.indices[fi + 1]
+            v3i = self.indices[fi + 2]
+
+            vs = self.vertices
+            p1 = [vs[v1i + c] for c in range(3)]
+            p2 = [vs[v2i + c] for c in range(3)]
+            p3 = [vs[v3i + c] for c in range(3)]
+
+            u,v  = [0,0,0], [0,0,0]
+            for j in range(3):
+                v[j] = p2[j] - p1[j]
+                u[j] = p3[j] - p1[j]
+
+            n = [0,0,0]
+            n[0] = u[1] * v[2] - u[2] * v[1]
+            n[1] = u[2] * v[0] - u[0] * v[2]
+            n[2] = u[0] * v[1] - u[1] * v[0]
+
+            for k in range(3):
+                self.vertices[v1i + 3 + k] = n[k]
+                self.vertices[v2i + 3 + k] = n[k]
+                self.vertices[v3i + 3 + k] = n[k]
+
 
 class ObjFile:
-
     def finish_object(self):
         if self._current_object == None:
             return
@@ -23,16 +46,30 @@ class ObjFile:
         for f in self.faces:
             verts =  f[0]
             norms = f[1]
+            tcs = f[2]
             for i in range(3):
+                #get normal components
+                n = (0.0, 0.0, 0.0)
+                if norms[i] != -1:
+                    n = self.normals[norms[i]-1]
+
+                #get texture coordinate components
+                t = (0.0, 0.0)
+                if tcs[i] != -1:
+                    t = self.texcoords[tcs[i]-1]
+
+                #get vertex components
                 v = self.vertices[verts[i]-1]
-                n = self.normals[norms[i]-1]
-                data = [v[0], v[1], v[2], n[0], n[1], n[2], 0.0,0.0]
+
+                data = [v[0], v[1], v[2], n[0], n[1], n[2], t[0], t[1]]
                 mesh.vertices.extend(data)
+
             tri = [idx, idx+1, idx+2]
             mesh.indices.extend(tri)
             idx += 3
 
         self.objects[self._current_object] = mesh
+        #mesh.calculate_normals()
         self.faces = []
 
     def __init__(self, filename, swapyz=False):
@@ -80,11 +117,11 @@ class ObjFile:
                     if len(w) >= 2 and len(w[1]) > 0:
                         texcoords.append(int(w[1]))
                     else:
-                        texcoords.append(0)
+                        texcoords.append(-1)
                     if len(w) >= 3 and len(w[2]) > 0:
                         norms.append(int(w[2]))
                     else:
-                        norms.append(0)
+                        norms.append(-1)
                 self.faces.append((face, norms, texcoords, material))
         self.finish_object()
 
@@ -103,6 +140,3 @@ def MTL(filename):
             raise ValueError, "mtl file doesn't start with newmtl stmt"
         mtl[values[0]] = values[1:]
     return contents
-
-
-
--- examples/3Drendering/simple.glsl
+++ examples/3Drendering/simple.glsl
@@ -4,7 +4,6 @@ simple diffuse lighting based on laberts cosine law; see e.g.:
     http://en.wikipedia.org/wiki/Lambertian_reflectance
     http://en.wikipedia.org/wiki/Lambert%27s_cosine_law
 */
-
 ---VERTEX SHADER-------------------------------------------------------
 
 attribute vec3  v_pos;
@@ -25,21 +24,18 @@ void main (void) {
 }
 
 
-
 ---FRAGMENT SHADER-----------------------------------------------------
-
-uniform mat4 normal_mat;
 
 varying vec4 normal_vec;
 varying vec4 vertex_pos;
 
+uniform mat4 normal_mat;
+
 void main (void){
     //correct normal, and compute light vector (assume light at the eye)
-    vec4 v_normal = normalize( vec4(mat3(normal_mat) * vec3(normal_vec), 0.0) ) ;
+    vec4 v_normal = normalize( normal_mat * normal_vec ) ;
     vec4 v_light = normalize( vec4(0,0,0,1) - vertex_pos );
-
     //reflectance based on lamberts law of cosine
     float theta = clamp(dot(v_normal, v_light), 0.0, 1.0);
     gl_FragColor = vec4(theta, theta, theta, 1.0);
 }
-
