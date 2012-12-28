32f29eac0582e73bc17b774a98891d11de760458
Mathieu Virbel <mat@kivy.org>
2012-12-28 18:53:22 +0100

    py3: use different implementation for weakmethod, much easier to understand.

--- kivy/weakmethod.py
+++ kivy/weakmethod.py
@@ -12,56 +12,91 @@ http://code.activestate.com/recipes/81253/, based on the nicodemus version.
 '''
 
 import weakref
+import sys
 
+if sys.version > '3':
 
-class WeakMethod(object):
-    '''Implementation of weakref for function and bounded method.
-    '''
+    class WeakMethod:
+        '''Implementation of weakref for function and bounded method.
+        '''
+        def __init__(self, method):
+            try:
+                if method.__self__ is not None:
+                    self.method_name = method.__func__.__name__
+                    self.proxy = weakref.proxy(method.__self__)
+                else:
+                    self.method = method
+                    self.proxy = None
+            except AttributeError:
+                self.proxy = None
+                self.method = method
 
-    def __init__(self, method):
-        try:
-            if method.__self__ is not None:
-                # bound method
-                self._obj = weakref.ref(method.__self__)
-            else:
-                # unbound method
+        def __call__(self, *args):
+            '''Return a new bound-method like the original, or the
+            original function if refers just to a function or unbound
+            method.
+            Returns None if the original object doesn't exist
+            '''
+            if self.proxy:
+                return getattr(self.proxy, self.method_name)(*args)
+            return self.method
+
+        def is_dead(self):
+            '''Returns True if the referenced callable was a bound method and
+            the instance no longer exists. Otherwise, return False.
+            '''
+            return self.proxy is not None and bool(dir(self.proxy))
+
+else:
+
+    import new
+
+    class WeakMethod(object):
+        '''Implementation of weakref for function and bounded method.
+        '''
+
+        def __init__(self, method):
+            try:
+                if method.__self__ is not None:
+                    # bound method
+                    self._obj = weakref.ref(method.im_self)
+                else:
+                    # unbound method
+                    self._obj = None
+                self._func = method.im_func
+                self._class = method.im_class
+            except AttributeError:
+                # not a method
                 self._obj = None
-            self._func = method.__func__
-            self._class = method.__self__.__class__
-        except AttributeError:
-            # not a method
-            self._obj = None
-            self._func = method
-            self._class = None
+                self._func = method
+                self._class = None
 
-    def __call__(self):
-        '''Return a new bound-method like the original, or the
-        original function if refers just to a function or unbound
-        method.
-        Returns None if the original object doesn't exist
-        '''
-        if self.is_dead():
-            return None
-        if self._obj is not None:
-            # we have an instance: return a bound method
-            o = self._obj()
-            return self.func.__get__(o, o.__class__)
-        else:
-            # we don't have an instance: return just the function
-            return self._func
+        def __call__(self):
+            '''Return a new bound-method like the original, or the
+            original function if refers just to a function or unbound
+            method.
+            Returns None if the original object doesn't exist
+            '''
+            if self.is_dead():
+                return None
+            if self._obj is not None:
+                return new.instancemethod(self._func, self._obj(), self._class)
+            else:
+                # we don't have an instance: return just the function
+                return self._func
 
-    def is_dead(self):
-        '''Returns True if the referenced callable was a bound method and
-        the instance no longer exists. Otherwise, return False.
-        '''
-        return self._obj is not None and self._obj() is None
+        def is_dead(self):
+            '''Returns True if the referenced callable was a bound method and
+            the instance no longer exists. Otherwise, return False.
+            '''
+            return self._obj is not None and self._obj() is None
 
-    def __eq__(self, other):
-        try:
-            return type(self) is type(other) and self() == other()
-        except:
-            return False
+        def __eq__(self, other):
+            try:
+                return type(self) is type(other) and self() == other()
+            except:
+                return False
 
-    def __ne__(self, other):
-        return not self == other
+        def __ne__(self, other):
+            return not self == other
 
