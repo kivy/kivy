9412685dc27b7dd0fe40fcd6c26dc516daf7d5a6
Mathieu Virbel <mat@kivy.org>
2013-01-24 17:28:08 +0100

    clean old vertexformat code, and removed glGetAttribLocation in favor to glBindAttribLocation approach (less GL instructions generated, as before.)

---
+++ kivy/graphics/vertex.pyx
@@ -0,0 +1,71 @@
+
+__all__ = ('VertexFormat', 'VertexFormatException')
+
+include "config.pxi"
+include "common.pxi"
+
+from kivy.graphics.c_opengl cimport GL_FLOAT, GLfloat
+
+cdef list attrib_location_indices = []
+
+class VertexFormatException(Exception):
+    pass
+
+
+cdef class VertexFormat:
+    '''VertexFormat is used to describe the layout of the vertex data stored 
+    in vertex arrays/vbo.
+
+    .. versionadded:: 1.5.2
+    '''
+    def __cinit__(self, *fmt):
+        self.vattr = NULL
+        self.vattr_count = 0
+        self.vsize = 0
+        self.vbytesize = 0
+
+    def __dealloc__(self):
+        if self.vattr != NULL:
+            free(self.vattr)
+            self.vattr = NULL
+
+    def __init__(self, *fmt):
+        cdef vertex_attr_t *attr
+        cdef int index, size
+
+        if not fmt:
+            raise VertexFormatException('No format specified')
+
+        self.vattr_count = len(fmt)
+        self.vattr = <vertex_attr_t *>malloc(sizeof(vertex_attr_t) * self.vattr_count)
+
+        if self.vattr == NULL:
+            raise MemoryError()
+
+        index = 0
+        for name, size, tp in fmt:
+
+            # ensure the name is in the location indices
+            if name not in attrib_location_indices:
+                attrib_location_indices.append(name)
+
+            attr = &self.vattr[index]
+
+            # fill the vertex format
+            attr.per_vertex = 1
+            attr.name = <bytes>name
+            attr.index = index#attrib_location_indices.index(name)
+            attr.size = size
+
+            # only float is accepted as attribute format
+            if tp == 'float':
+                attr.type = GL_FLOAT
+                attr.bytesize = sizeof(GLfloat) * size
+            else:
+                raise VertexFormatException('Unknow format type %r' % tp)
+
+            # adjust the size, and prepare for the next iteration.
+            index += 1
+            self.vsize += attr.size
+            self.vbytesize += attr.bytesize
+
