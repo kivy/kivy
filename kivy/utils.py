977f296f02bc35c1710ac099b5086de3b0cfc0fa
Dusty Phillips <dusty@linux.ca>
2013-03-18 14:39:46 -0600

    Do not mask AttributeError as KeyError.
    hasattr in python2 catches KeyError, but this is considered 'broken by design'
    and in Python3 it only works with AttributeError.
    
    See http://mail.python.org/pipermail/python-dev/2010-August/103184.html for more
    information.
    
    I am not certain if any code depends on hasattr raising a keyerror when parsing
    .kv files, but this is my best guess for a solution.

--- kivy/utils.py
+++ kivy/utils.py
@@ -207,10 +207,7 @@ class QueryDict(dict):
         try:
             return self.__getitem__(attr)
         except KeyError:
-            try:
-                return super(QueryDict, self).__getattr__(attr)
-            except AttributeError:
-                raise KeyError(attr)
+            return super(QueryDict, self).__getattr__(attr)
 
     def __setattr__(self, attr, value):
         self.__setitem__(attr, value)
