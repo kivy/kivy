cc7036adbe20294126d82d0cf254805c9e2df835
Thomas Hansen <thomas.hansen@gmail.com>
2013-03-19 16:33:52 -0700

    Update kivy-mode.el
    fix typo

--- kivy/tools/highlight/kivy-mode.el
+++ kivy/tools/highlight/kivy-mode.el
@@ -1,4 +1,4 @@
-nnn;;; kivy-mode.el --- Emacs major mode for editing Kivy files
+;;; kivy-mode.el --- Emacs major mode for editing Kivy files
 ;;
 ;; Author: Dean Serenevy <dean@serenevy.net>
 ;; Version: 0.1.0
