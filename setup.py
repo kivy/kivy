0e4946c0602d3598e98ee4310cb69a4335f56aa6
prost87 <prost87@gmail.com>
2013-03-31 11:36:38 +0200

    setup: refactored usage of environment variables

--- setup.py
+++ setup.py
@@ -17,9 +17,11 @@ c_options = {
     'use_opengl_es2': True,
     'use_opengl_debug': False,
     'use_glew': False,
-    'use_sdl': False,
+    'use_sdl': 'KIVY_USE_SDL' in environ,
     'use_ios': False,
-    'use_mesagl': 'USE_MESAGL' in environ}
+    'use_mesagl': 'KIVY_USE_MESAGL' in environ or 'USE_MESAGL' in environ,
+    'use_x11': 'KIVY_USE_X11' in environ or 'WITH_X11' in environ,
+}
 
 # now check if environ is changing the default values
 for key in c_options.keys():
@@ -360,7 +362,7 @@ if platform in ('darwin', 'ios'):
     sources['core/image/img_imageio.pyx'] = merge(
         base_flags, osx_flags)
 
-if 'WITH_X11' in environ:
+if c_options['use_x11']:
     sources['core/window/window_x11.pyx'] = merge(
         base_flags, gl_flags, graphics_flags, {
             'depends': [join(dirname(__file__),
