920c6c72243f6c6fec013cf8923468bf29c884e3
Mathieu Virbel <mat@kivy.org>
2013-05-02 15:51:17 +0200

    garden: introduce kivy.garden module, and kivy/tools/garden commandline tool.

--- setup.py
+++ setup.py
@@ -427,6 +427,7 @@ setup(
         'hardware-accelerated multitouch applications.'),
     ext_modules=ext_modules,
     cmdclass=cmdclass,
+    scripts=['kivy/tools/garden'],
     packages=[
         'kivy',
         'kivy.adapters',
