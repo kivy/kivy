5db4437eda0f3d3295756bea1274a8408e715c72
Mathieu Virbel <mat@kivy.org>
2013-05-25 19:03:49 +0200

    postproc: fix doubletap/tripletap when the devices doesnt have "button" profile. closes #1204

--- kivy/input/postproc/doubletap.py
+++ kivy/input/postproc/doubletap.py
@@ -38,6 +38,10 @@ class InputPostprocDoubleTap(object):
         must be ok, also, the touch profile must be compared so the kind
         of touch is the same
         '''
+        ref_button = None
+        if 'button' in ref.profile:
+            ref_button = ref.button
+
         for touchid in self.touches:
             if ref.uid == touchid:
                 continue
@@ -53,9 +57,11 @@ class InputPostprocDoubleTap(object):
                 continue
             if touch.is_mouse_scrolling or ref.is_mouse_scrolling:
                 continue
-            if 'button' in touch.profile or 'button' in ref.profile:
-                if 'button' not in ref.profile or ref.button != touch.button:
-                    continue
+            touch_button = None
+            if 'button' in touch.profile:
+                touch_button = touch.button
+            if touch_button != ref_button:
+                continue
             touch.double_tap_distance = distance
             return touch
         return None
