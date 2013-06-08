5bd6da5f20bfff2a27fe6f83614a5867b88bbb56
Mathieu Virbel <mat@kivy.org>
2013-06-08 10:28:28 +0200

    graphics: use destructor callback in ref() to avoid self.gc. ref #1270

--- kivy/graphics/context.pyx
+++ kivy/graphics/context.pyx
@@ -45,6 +45,7 @@ cdef class Context:
         self.trigger_gl_dealloc = Clock.create_trigger(self.gl_dealloc, 0)
 
     cdef void flush(self):
+        gc.collect()
         self.lr_texture = array('i')
         self.lr_canvas = []
         self.lr_vbo = array('i')
@@ -52,22 +53,22 @@ cdef class Context:
         self.lr_fbo_fb = array('i')
 
     cdef void register_texture(self, Texture texture):
-        self.l_texture.append(ref(texture))
+        self.l_texture.append(ref(texture, self.l_texture.remove))
 
     cdef void register_canvas(self, Canvas canvas):
-        self.l_canvas.append(ref(canvas))
+        self.l_canvas.append(ref(canvas, self.l_canvas.remove))
 
     cdef void register_vbo(self, VBO vbo):
-        self.l_vbo.append(ref(vbo))
+        self.l_vbo.append(ref(vbo, self.l_vbo.remove))
 
     cdef void register_vertexbatch(self, VertexBatch vb):
-        self.l_vertexbatch.append(ref(vb))
+        self.l_vertexbatch.append(ref(vb, self.l_vertexbatch.remove))
 
     cdef void register_shader(self, Shader shader):
-        self.l_shader.append(ref(shader))
+        self.l_shader.append(ref(shader, self.l_shader.remove))
 
     cdef void register_fbo(self, Fbo fbo):
-        self.l_fbo.append(ref(fbo))
+        self.l_fbo.append(ref(fbo, self.l_fbo.remove))
 
     cdef void dealloc_texture(self, Texture texture):
         cdef array arr
@@ -182,7 +183,6 @@ cdef class Context:
         start = time()
         Logger.info('Context: Reloading graphics data...')
         Logger.debug('Context: Collect and flush all garbage')
-        self.gc()
         self.flush()
 
         # First step, prevent double loading by setting everything to -1
@@ -265,21 +265,10 @@ cdef class Context:
             if canvas:
                 canvas.flag_update()
 
-    def gc(self, *largs):
-        self.l_texture = [x for x in self.l_texture if x() is not None]
-        self.l_canvas = [x for x in self.l_canvas if x() is not None]
-        self.l_vbo = [x for x in self.l_vbo if x() is not None]
-        self.l_vertexbatch = [x for x in self.l_vertexbatch if x() is not None]
-        gc.collect()
-
     def gl_dealloc(self, *largs):
         # dealloc all gl resources asynchronously
         cdef GLuint i, j
         cdef array arr
-
-        # FIXME we are doing gc for each time we dealloc things. But if you have
-        # "big" apps, this might just slow it down.
-        self.gc()
 
         if len(self.lr_vbo):
             Logger.trace('Context: releasing %d vbos' % len(self.lr_vbo))
