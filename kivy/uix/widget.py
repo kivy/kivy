7ab92974eacad003e3d91d0f728507c992a6602f
qua-non <akshayaurora@gmail.com>
2013-07-02 04:50:01 +0530

    core: Widget use actual widget reference instead of proxy ref when adding to another widget.

--- kivy/uix/widget.py
+++ kivy/uix/widget.py
@@ -179,8 +179,8 @@ class Widget(WidgetBase):
     def proxy_ref(self):
         '''Return a proxy reference to the widget, ie, without taking a
         reference of the widget. See `weakref.proxy
-        <http://docs.python.org/2/library/weakref.html?highlight=proxy#weakref.proxy>`_
-        for more information about it.
+        <http://docs.python.org/2/library/weakref.html?highlight\
+        =proxy#weakref.proxy>`_ for more information about it.
 
         .. versionadded:: 1.7.2
         '''
@@ -316,6 +316,7 @@ class Widget(WidgetBase):
         >>> slider = Slider()
         >>> root.add_widget(slider)
         '''
+        widget = widget.__self__
         if widget is self:
             raise WidgetException('You cannot add yourself in a Widget')
         if not isinstance(widget, Widget):
