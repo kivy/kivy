9dd32a265a02791572381b469d3fdb2853dfd813
geojeff <geojeff@me.com>
2013-07-09 06:12:01 -0500

    Implemented pop and popitem dict ops
    With popitem, it became apparent that the change_monitor attr could be
    deleted, so protections were added. Also, to keep the change_monitor
    attr out of the dict and its methods, keys(), len and iterator methods
    of dict were overridden.

--- kivy/uix/listview.py
+++ kivy/uix/listview.py
@@ -1152,6 +1152,7 @@ class ListView(AbstractView, EventDispatcher):
         #                crod_clear       - all items deleted
         #                crod_pop         - single item
         #                crod_popitem     - single item
+        #                  [NOTE: crod_popitem is performed as crod_delitem]
 
         # Callbacks could come here from either crol or crod, and there could
         # be differences in handling.
@@ -1249,8 +1250,10 @@ class ListView(AbstractView, EventDispatcher):
                          'crod_delitem',
                          'crod_setitem_del',
                          'crod_clear',
-                         'crod_pop',
-                         'crod_popitem', ]:
+                         'crod_pop', ]:
+
+            # NOTE: There is no crod_popitem here, because it is performed as
+            #       a crod_delitem.
 
             deleted_indices = range(start_index, end_index + 1)
 
