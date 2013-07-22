5a63d9a34b2c32a534accc842eaf7c7172786db0
Mathieu Virbel <mat@kivy.org>
2013-07-22 18:35:24 +0200

    actionbar: update example

--- examples/widgets/actionbar.py
+++ examples/widgets/actionbar.py
@@ -2,40 +2,32 @@ from kivy.base import runTouchApp
 from kivy.lang import Builder
 
 runTouchApp(Builder.load_string('''
-FloatLayout
-    canvas:
-        Color:
-            rgb: .9, .9, .9
-        Rectangle:
-            size: self.size
-    ActionBar:
-        size_hint_y: None
-        height: '48dp'
-        pos_hint: {'top':1}
-        ActionView:
-            use_separator: True
-            ActionPrevious:
-                title: 'Title'
-            ActionOverflow:
+ActionBar:
+    pos_hint: {'top':1}
+    ActionView:
+        use_separator: True
+        ActionPrevious:
+            title: 'Action Bar'
+            with_previous: False
+        ActionOverflow:
+        ActionButton:
+            text: 'Btn0'
+            icon: 'atlas://data/images/defaulttheme/audio-volume-high'
+        ActionButton:
+            text: 'Btn1'
+        ActionButton:
+            text: 'Btn2'
+        ActionButton:
+            text: 'Btn3'
+        ActionButton:
+            text: 'Btn4'
+        ActionGroup:
+            text: 'Group1'
             ActionButton:
-                text: 'A'
-                size_hint_x: None
-                width: '48dp'
+                text: 'Btn5'
             ActionButton:
-                text: 'Btn1'
+                text: 'Btn6'
             ActionButton:
-                text: 'Btn2'
-            ActionButton:
-                text: 'Btn3'
-            ActionButton:
-                text: 'Btn4'
-            ActionGroup:
-                text: 'Group1'
-                ActionButton:
-                    text: 'Btn5'
-                ActionButton:
-                    text: 'Btn6'
-                ActionButton:
-                    text: 'Btn7'
+                text: 'Btn7'
 '''))
 
