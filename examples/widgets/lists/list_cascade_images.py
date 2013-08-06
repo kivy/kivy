8cd5cf9e6338ca9c0d4b9cf23b140a322da87fea
geojeff <geojeff@me.com>
2013-08-05 20:39:21 -0500

    Made selection a normal ListProperty again
    Copied the batch_delete() method from OpObservableDict to
    ObservableDict, so that the selection property can be the normal type of
    ListProperty, dispatching change events normally. This makes the API for
    binding to selection a normal affair. The detailed change info that came
    from using OpObservableList was not used in the examples. This can be
    used if there is a need for it in the future.

--- examples/widgets/lists/list_cascade_images.py
+++ examples/widgets/lists/list_cascade_images.py
@@ -45,8 +45,6 @@ class FruitsDictAdapter(DictAdapter):
     def fruit_category_changed(self, *args):
 
         fruit_categories_adapter = args[0]
-        changed_selection = args[1]
-        op_info = args[2]
 
         if fruit_categories_adapter.selection:
             key = fruit_categories_adapter.selection[0].text
