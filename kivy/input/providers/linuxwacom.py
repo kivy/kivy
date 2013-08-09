4dd062416a7681e0f644d6633c4003205fb06d45
Richard Larkin <fruitbat@ZenMint.(none)>
2013-08-09 17:05:02 +0200

    doc: revisions to input/providers/hidinput.py and linuxwacom.py

--- kivy/input/providers/linuxwacom.py
+++ kivy/input/providers/linuxwacom.py
@@ -13,7 +13,7 @@ To configure LinuxWacom, add this to your configuration::
 
 You can use a custom range for the X, Y and pressure values.
 On some drivers, the range reported is invalid.
-To fix that, you can add these options in the argument line:
+To fix that, you can add these options to the argument line:
 
 * invert_x : 1 to invert X axis
 * invert_y : 1 to invert Y axis
