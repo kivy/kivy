fa355d9a13718be94f2e69cef8a8edad077cb7d1
Richard Larkin <fruitbat@ZenKubuntu.(none)>
2013-08-23 04:53:56 +0200

    doc: revisions to modules/keybinding.py

--- kivy/modules/keybinding.py
+++ kivy/modules/keybinding.py
@@ -10,6 +10,33 @@ This module forces the mapping of some keys to functions:
 
 Note: this does't work if the application requests the keyboard beforehand.
 
+Usage
+-----
+
+For normal module usage, please see the :mod:`~kivy.modules` documentation.
+
+The Keybinding module, however, can also be imported and used just like a normal 
+python module. This has the added advantage of being able to activate and
+deactivate the module programmatically::
+
+    from kivy.app import App
+    from kivy.uix.button import Button
+    from kivy.modules import keybinding
+    from kivy.core.window import Window
+
+    class Demo(App):
+                
+        def build(self):
+            button = Button(text="Hello")
+            keybinding.start(Window, button)
+            return button
+
+    Demo().run()
+
+To remove the Keybinding, you can do the following::
+
+    Keybinding.stop(Window, button)
+
 '''
 
 from kivy.utils import platform
