960a664ce3b5d77bd8e4283ffdab575484f8ae80
Mathieu Virbel <mat@kivy.org>
2013-08-26 10:07:02 +0200

    sdl2/text: fix color bgra -> rgba

--- kivy/core/text/_text_sdl2.pyx
+++ kivy/core/text/_text_sdl2.pyx
@@ -34,9 +34,11 @@ cdef class _SurfaceContainer:
         self.h = h
 
     def __init__(self, w, h):
+        # XXX check on OSX to see if little endian/big endian make a difference
+        # here.
         self.surface = SDL_CreateRGBSurface(0,
             w, h, 32,
-            0x00ff0000, 0x0000ff00, 0x000000ff, 0xff000000)
+            0x000000ff, 0x0000ff00, 0x00ff0000, 0xff000000)
         memset(self.surface.pixels, 0, w * h * 4)
 
     def __dealloc__(self):
