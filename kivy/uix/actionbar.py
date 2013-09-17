b032a5f7938c5c10ba7d6a5ba8162b65a2a8f767
qua-non <akshayaurora@gmail.com>
2013-09-17 15:01:30 +0530

    uix:Actionbar use container.children instead of relative reference. This allows for different types of dropdowns to be used easily.

--- kivy/uix/actionbar.py
+++ kivy/uix/actionbar.py
@@ -37,12 +37,12 @@ from kivy.uix.widget import Widget
 from kivy.uix.button import Button
 from kivy.uix.togglebutton import ToggleButton
 from kivy.uix.checkbox import CheckBox
+from kivy.config import Config
 from kivy.properties import ObjectProperty, NumericProperty, \
     BooleanProperty, StringProperty, ListProperty, OptionProperty
 from kivy.uix.spinner import Spinner
 from kivy.lang import Builder
 from functools import partial
-from kivy.config import Config
 
 
 window_icon = ''
@@ -283,7 +283,9 @@ class ActionGroup(ActionItem, Spinner):
         self.is_open = not self.is_open
         ddn = self._dropdown
         ddn.size_hint_x = None
-        children = self.list_action_item
+        if not ddn.container:
+            return
+        children = ddn.container.children
         ddn.width = max([self.width,
                         children[0].minimum_width])
         for item in children:
