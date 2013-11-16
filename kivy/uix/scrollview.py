41c912fa392121faf7fe069856f953bf89242f1d
voen <voen@users.noreply.github.com>
2013-11-16 15:20:22 +0200

    Expose a property to set the scroll distance when using a mouse wheel.
    By changing scroll_distance we can achieve the same effect but this will render touch dragging unusable and buggy, especially if we set a greater value. By adding a separate property that is used only for setting the scroll distance in the case of mouse wheel scrolling we can safely use together a custom scroll_wheel_distance and touch dragging.

--- kivy/uix/scrollview.py
+++ kivy/uix/scrollview.py
@@ -127,6 +127,17 @@ class ScrollView(StencilView):
     configuration.
     '''
 
+    scroll_wheel_distance = NumericProperty(20)
+    '''Distance to move when scrolling with a mouse wheel.
+    It is advisable that you base this value on the dpi of your target device's
+    screen.
+
+    .. versionadded:: 1.8.0
+
+    :data:`scroll_wheel_distance` is a :class:`~kivy.properties.NumericProperty`,
+    defaults to 20 pixels.
+    '''
+
     scroll_timeout = NumericProperty(_scroll_timeout)
     '''Timeout allowed to trigger the :data:`scroll_distance`, in milliseconds.
     If the user has not moved :data:`scroll_distance` within the timeout,
@@ -313,10 +324,11 @@ class ScrollView(StencilView):
 
     scroll_type = OptionProperty('content', options=('content', 'bars', 'both'))
     '''Sets the type of scrolling to use for the content of the scrollview.
+    Available options are: content, bars, both.
 
     .. versionadded:: 1.8.0
 
-    :data:`scroll_type` is a :class:`~kivy.properties.OptionProperty`, default
+    :data:`scroll_type` is a :class:`~kivy.properties.OptionProperty`, defaults
     to content.
     '''
 
@@ -441,7 +453,7 @@ class ScrollView(StencilView):
         if vp and 'button' in touch.profile and \
             touch.button.startswith('scroll'):
             btn = touch.button
-            m = self.scroll_distance
+            m = sp(self.scroll_wheel_distance)
             e = None
 
             if (self.effect_x and self.do_scroll_y and vp.height > self.height
@@ -709,7 +721,7 @@ if __name__ == '__main__':
                 btn = Button(text=str(i), size_hint=(None, None),
                              size=(200, 100))
                 layout1.add_widget(btn)
-            scrollview1 = ScrollView(bar_width='2dp',)
+            scrollview1 = ScrollView(bar_width='2dp')
             scrollview1.add_widget(layout1)
 
             layout2 = GridLayout(cols=4, spacing=10, size_hint=(None, None))
@@ -719,7 +731,7 @@ if __name__ == '__main__':
                 btn = Button(text=str(i), size_hint=(None, None),
                              size=(200, 100))
                 layout2.add_widget(btn)
-            scrollview2 = ScrollView(scroll_type='bars')
+            scrollview2 = ScrollView(scroll_type='bars', scroll_wheel_distance=100)
             scrollview2.add_widget(layout2)
 
             root = GridLayout(cols=2)
