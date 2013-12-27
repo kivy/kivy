7a0fb454c452fb55a5fd6b261e91298d395acdf8
tshirtman <gabriel.pettier@gmail.com>
2013-12-28 00:47:10 +0100

    pep8 fixes for kivy/uix/slider.py

--- kivy/uix/slider.py
+++ kivy/uix/slider.py
@@ -111,7 +111,8 @@ class Slider(Widget):
         if step == 0:
             self.value = val
         else:
-            self.value = min(round((val - vmin) / step) * step + vmin, self.max)
+            self.value = min(round((val - vmin) / step) * step + vmin,
+                             self.max)
     value_normalized = AliasProperty(get_norm_value, set_norm_value,
                                      bind=('value', 'min', 'max', 'step'))
     '''Normalized value inside the :data:`range` (min/max) to 0-1 range::
