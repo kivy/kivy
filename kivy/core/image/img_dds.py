e5a907a54240063733d6945936ebda887f2e6ad9
tshirtman <gabriel.pettier@gmail.com>
2013-12-28 21:22:22 +0100

    pep8 fixes for kivy/core/image/*

--- kivy/core/image/img_dds.py
+++ kivy/core/image/img_dds.py
@@ -25,7 +25,7 @@ class ImageLoaderDDS(ImageLoaderBase):
         self.filename = filename
         width, height = dds.size
         im = ImageData(width, height, dds.dxt, dds.images[0], source=filename,
-                flip_vertical=False)
+                       flip_vertical=False)
         if len(dds.images) > 1:
             images = dds.images
             images_size = dds.images_size
