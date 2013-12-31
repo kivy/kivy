ef4a9e12ffec546562c9cd14d77dc639a473b983
Matthew Einhorn <moiein2000@gmail.com>
2013-12-31 16:06:35 -0500

    re-add ffpyplayer to init.

--- kivy/__init__.py
+++ kivy/__init__.py
@@ -190,7 +190,8 @@ else:
 kivy_options = {
     'window': ('egl_rpi', 'pygame', 'sdl', 'x11'),
     'text': ('pil', 'pygame', 'sdlttf'),
-    'video': ('gstplayer', 'ffmpeg', 'gi', 'pygst', 'pyglet', 'null'),
+    'video': ('gstplayer', 'ffmpeg', 'ffpyplayer', 'gi', 'pygst', 'pyglet',
+              'null'),
     'audio': ('gstplayer', 'pygame', 'gi', 'pygst', 'sdl'),
     'image': ('tex', 'imageio', 'dds', 'gif', 'pil', 'pygame'),
     'camera': ('opencv', 'gi', 'pygst', 'videocapture', 'avfoundation'),
