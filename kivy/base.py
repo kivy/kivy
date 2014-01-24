fccef940a00edf891306e3691f75b6c98f1eb286
kaichogami <asishrocks95@gmail.com>
2014-01-25 00:49:47 +0530

    Update base.py

--- kivy/base.py
+++ kivy/base.py
@@ -27,7 +27,7 @@ from kivy.context import register_context
 EventLoop = None
 
 
-class ExceptionHandler(object):
+class ExceptionHandler:
     '''Base handler that catches exceptions in :func:`runTouchApp`.
     You can subclass and extend it as follows::
 
