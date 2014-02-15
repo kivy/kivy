553fc4e31420ec6268563dec21fd31db7470db18
Alexander Taylor <alexanderjohntaylor@gmail.com>
2014-02-15 01:18:18 +0000

    Added new effects and improved example

--- kivy/uix/effectwidget.py
+++ kivy/uix/effectwidget.py
@@ -13,7 +13,7 @@ draw children to Fbo and apply custom shader to a RenderContext.
 
 from kivy.clock import Clock
 from kivy.lang import Builder
-from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.boxlayout import BoxLayout
 from kivy.properties import StringProperty, ObjectProperty, ListProperty
 from kivy.graphics import (RenderContext, Fbo, Color, Rectangle,
                            Translate, PushMatrix, PopMatrix,
@@ -54,7 +54,6 @@ void main (void){
      gl_FragColor = frag_color * texture2D(texture0, tex_coord0);
 }
 '''
-#    gl_FragColor = texture2D(texture0, tex_coord0);
 
 shader_footer_effect = '''
 void main (void){
@@ -108,6 +107,86 @@ vec4 effect(vec4 color, sampler2D texture, vec2 tex_coords, vec2 coords)
 }
 '''
 
+effect_mix = '''
+vec4 effect(vec4 color, sampler2D texture, vec2 tex_coords, vec2 coords)
+{
+    return vec4(color.z, color.x, color.y, 1.0);
+}
+'''
+
+effect_flash = '''
+vec4 effect(vec4 color, sampler2D texture, vec2 tex_coords, vec2 coords)
+{
+    return color * abs(sin(time));
+}
+'''
+
+effect_blur_h = '''
+vec4 effect(vec4 color, sampler2D texture, vec2 tex_coords, vec2 coords)
+{
+    float dt = 0.5 * 1.0 / resolution.x;
+    vec4 sum = vec4(0.0);
+    sum += texture2D(texture, vec2(tex_coords.x - 4.0*dt, tex_coords.y)) * 0.05;
+    sum += texture2D(texture, vec2(tex_coords.x - 3.0*dt, tex_coords.y)) * 0.09;
+    sum += texture2D(texture, vec2(tex_coords.x - 2.0*dt, tex_coords.y)) * 0.12;
+    sum += texture2D(texture, vec2(tex_coords.x - dt, tex_coords.y)) * 0.15;
+    sum += texture2D(texture, vec2(tex_coords.x, tex_coords.y)) * 0.16;
+    sum += texture2D(texture, vec2(tex_coords.x + dt, tex_coords.y)) * 0.15;
+    sum += texture2D(texture, vec2(tex_coords.x + 2.0*dt, tex_coords.y)) * 0.12;
+    sum += texture2D(texture, vec2(tex_coords.x + 3.0*dt, tex_coords.y)) * 0.09;
+    sum += texture2D(texture, vec2(tex_coords.x + 4.0*dt, tex_coords.y)) * 0.05;
+    return sum;
+}
+'''
+
+effect_blur_v = '''
+vec4 effect(vec4 color, sampler2D texture, vec2 tex_coords, vec2 coords)
+{
+    float dt = 0.5 * 1.0 / resolution.y;
+    vec4 sum = vec4(0.0);
+    sum += texture2D(texture, vec2(tex_coords.x, tex_coords.y - 4.0*dt)) * 0.05;
+    sum += texture2D(texture, vec2(tex_coords.x, tex_coords.y - 3.0*dt)) * 0.09;
+    sum += texture2D(texture, vec2(tex_coords.x, tex_coords.y - 2.0*dt)) * 0.12;
+    sum += texture2D(texture, vec2(tex_coords.x, tex_coords.y - dt)) * 0.15;
+    sum += texture2D(texture, vec2(tex_coords.x, tex_coords.y)) * 0.16;
+    sum += texture2D(texture, vec2(tex_coords.x, tex_coords.y + dt)) * 0.15;
+    sum += texture2D(texture, vec2(tex_coords.x, tex_coords.y + 2.0*dt)) * 0.12;
+    sum += texture2D(texture, vec2(tex_coords.x, tex_coords.y + 3.0*dt)) * 0.09;
+    sum += texture2D(texture, vec2(tex_coords.x, tex_coords.y + 4.0*dt)) * 0.05;
+    return sum;
+}
+'''
+
+effect_postprocessing = '''
+vec4 effect(vec4 color, sampler2D texture, vec2 tex_coords, vec2 coords)
+{
+    vec2 q = tex_coord0 * vec2(1, -1);
+    vec2 uv = 0.5 + (q-0.5);//*(0.9);// + 0.1*sin(0.2*time));
+
+    vec3 oricol = texture2D(texture0,vec2(q.x,1.0-q.y)).xyz;
+    vec3 col;
+
+    col.r = texture2D(texture0,vec2(uv.x+0.003,-uv.y)).x;
+    col.g = texture2D(texture0,vec2(uv.x+0.000,-uv.y)).y;
+    col.b = texture2D(texture0,vec2(uv.x-0.003,-uv.y)).z;
+
+    col = clamp(col*0.5+0.5*col*col*1.2,0.0,1.0);
+
+    //col *= 0.5 + 0.5*16.0*uv.x*uv.y*(1.0-uv.x)*(1.0-uv.y);
+
+    col *= vec3(0.8,1.0,0.7);
+
+    col *= 0.9+0.1*sin(10.0*time+uv.y*1000.0);
+
+    col *= 0.97+0.03*sin(110.0*time);
+
+    float comp = smoothstep( 0.2, 0.7, sin(time) );
+    //col = mix( col, oricol, clamp(-2.0+2.0*q.x+3.0*comp,0.0,1.0) );
+
+    gl_FragColor = vec4(col,1.0);
+}
+'''
+
 effect_plasma = '''
 vec4 effect(vec4 color, sampler2D texture, vec2 tex_coords, vec2 coords)
 {
@@ -141,7 +220,7 @@ class EffectFbo(Fbo):
             raise Exception('failed')
 
 
-class EffectWidget(FloatLayout):
+class EffectWidget(BoxLayout):
 
     fs = StringProperty(None)
 
@@ -193,8 +272,13 @@ class EffectWidget(FloatLayout):
         self.refresh_fbo_setup()
 
     def update_glsl(self, *largs):
-        self.canvas['time'] = Clock.get_boottime()
-        self.canvas['resolution'] = map(float, self.size)
+        time = Clock.get_boottime()
+        resolution = [float(size) for size in self.size]
+        self.canvas['time'] = time
+        self.canvas['resolution'] = resolution
+        for fbo in self.fbo_list:
+            fbo['time'] = time
+            fbo['resolution'] = resolution
 
     def on_effects(self, *args):
         self.refresh_fbo_setup()
