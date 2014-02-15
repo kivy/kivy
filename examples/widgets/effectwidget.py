8b113323dc698eba3b7988a95a0be1dbf0aae7a3
Alexander Taylor <alexanderjohntaylor@gmail.com>
2014-02-15 01:18:18 +0000

    Added new effects and improved example

--- examples/widgets/effectwidget.py
+++ examples/widgets/effectwidget.py
@@ -20,6 +20,10 @@ from kivy.uix.effectwidget import (effect_monochrome,
                                    effect_blue,
                                    effect_green,
                                    effect_invert,
+                                   effect_mix,
+                                   effect_flash,
+                                   effect_blur_h,
+                                   effect_blur_v,
                                    effect_plasma)
 
 class EffectSpinner(Spinner):
@@ -43,40 +47,115 @@ class SpinnerRow(BoxLayout):
                 effects.append(effect_green)
             if text == 'invert':
                 effects.append(effect_invert)
+            if text == 'mix':
+                effects.append(effect_mix)
+            if text == 'flash':
+                effects.append(effect_flash)
+            if text == 'blur_h':
+                effects.append(effect_blur_h)
+            if text == 'blur_v':
+                effects.append(effect_blur_v)
             if text == 'plasma':
                 effects.append(effect_plasma)
         if self.effectwidget:
             self.effectwidget.effects = effects
 
 example = Builder.load_string('''
+#:import Vector kivy.vector.Vector
 BoxLayout:
     orientation: 'vertical'
     BoxLayout:
         EffectWidget:
             id: effect1
-            Image:
-                size_hint: 1, 1
-                pos_hint: {'x': 0, 'y': 0}
-                source: 'colours.png'
-                allow_stretch: True
-                keep_ratio: False
-            Button:
-                text: 'Hello world'
-                size_hint: None, None
-                size: 150, 100
-                pos_hint: {'center_x': 0.25, 'center_y': 0.5}
-                on_press: print('Button pressed!')
+            Widget:
+                canvas:
+                    Color:
+                        rgba: 1, 0, 0, 1
+                    Ellipse:
+                        pos: Vector(self.pos) + 0.5*Vector(self.size)
+                        size: 0.4*Vector(self.size)
+                    Color:
+                        rgba: 0, 1, 0.3, 1
+                    Ellipse:
+                        pos: Vector(self.pos) + 0.1*Vector(self.size)
+                        size: 0.6*Vector(self.size)
+                    Color:
+                        rgba: 0.5, 0.5, 0.2, 1
+                    Ellipse:
+                        pos: Vector(self.pos) + Vector([0, 0.6])*Vector(self.size)
+                        size: 0.4*Vector(self.size)
+                    Color:
+                        rgba: 1, 0.8, 0.1, 1
+                    Ellipse:
+                        pos: Vector(self.pos) + Vector([0.5, 0])*Vector(self.size)
+                        size: 0.4*Vector(self.size)
+                    Color:
+                        rgba: 0, 0, 0.8, 1
+                    Line:
+                        points:
+                            [self.x, self.y,
+                            self.x + self.width, self.y + 0.3*self.height,
+                            self.x + 0.2*self.width, self.y + 0.1*self.height,
+                            self.x + 0.85*self.width, self.y + 0.72*self.height,
+                            self.x + 0.31*self.width, self.y + 0.6*self.height,
+                            self.x, self.top]
+                        width: 1
+                    Color:
+                        rgba: 0, 0.9, 0.1, 1
+                    Line:
+                        points:
+                            [self.x + self.width, self.y + self.height,
+                            self.x + 0.35*self.width, self.y + 0.6*self.height,
+                            self.x + 0.7*self.width, self.y + 0.15*self.height,
+                            self.x + 0.2*self.width, self.y + 0.22*self.height,
+                            self.x + 0.3*self.width, self.y + 0.92*self.height]
+                        width: 2
         EffectWidget:
             id: effect2
-            size: 100, 100
-            Image:
-                id: im
-                pos: self.parent.pos
-                size: self.parent.size
-                source: 'colours2.png'
-                allow_stretch: True
-                keep_ratio: False
-                on_touch_down: print(self.size, self.pos)
+            Widget:
+                canvas:
+                    Color:
+                        rgba: 1, 0, 0, 1
+                    Ellipse:
+                        pos: Vector(self.pos) + 0.5*Vector(self.size)
+                        size: 0.4*Vector(self.size)
+                    Color:
+                        rgba: 0, 1, 0.3, 1
+                    Ellipse:
+                        pos: Vector(self.pos) + 0.1*Vector(self.size)
+                        size: 0.6*Vector(self.size)
+                    Color:
+                        rgba: 0.5, 0.5, 0.2, 1
+                    Ellipse:
+                        pos: Vector(self.pos) + Vector([0, 0.6])*Vector(self.size)
+                        size: 0.4*Vector(self.size)
+                    Color:
+                        rgba: 1, 0.8, 0.1, 1
+                    Ellipse:
+                        pos: Vector(self.pos) + Vector([0.5, 0])*Vector(self.size)
+                        size: 0.4*Vector(self.size)
+                    Color:
+                        rgba: 0, 0, 0.8, 1
+                    Line:
+                        points:
+                            [self.x, self.y,
+                            self.x + self.width, self.y + 0.3*self.height,
+                            self.x + 0.2*self.width, self.y + 0.1*self.height,
+                            self.x + 0.85*self.width, self.y + 0.72*self.height,
+                            self.x + 0.31*self.width, self.y + 0.6*self.height,
+                            self.x, self.top]
+                        width: 1
+                    Color:
+                        rgba: 0, 0.9, 0.1, 1
+                    Line:
+                        points:
+                            [self.x + self.width, self.y + self.height,
+                            self.x + 0.35*self.width, self.y + 0.6*self.height,
+                            self.x + 0.7*self.width, self.y + 0.15*self.height,
+                            self.x + 0.2*self.width, self.y + 0.22*self.height,
+                            self.x + 0.3*self.width, self.y + 0.92*self.height]
+                        width: 2
+                    
     SpinnerRow:
         effectwidget: effect1
         text: 'bg effects'
@@ -100,7 +179,10 @@ BoxLayout:
 
 <EffectSpinner>:
     text: 'none'
-    values: ['none', 'monochrome', 'red', 'blue', 'green', 'invert', 'plasma']
+    values:
+        ['none', 'monochrome',
+        'invert', 'plasma', 'mix',
+        'flash', 'blur_h', 'blur_v']
         
 ''')
 
