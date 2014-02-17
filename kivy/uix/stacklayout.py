91081e03b24aad64f58246f9d1b2bf30f85c23b8
Matthew Einhorn <moiein2000@gmail.com>
2014-02-17 15:27:54 -0500

    Add more details in stacklayout doc.

--- kivy/uix/stacklayout.py
+++ kivy/uix/stacklayout.py
@@ -15,9 +15,18 @@ Stack Layout
 .. versionadded:: 1.0.5
 
 The :class:`StackLayout` arranges children vertically or horizontally, as many
-as the layout can fit.
+as the layout can fit. The size of the individual children widgets do not
+have to be uniform.
 
+For example, to display widgets that get progressively larger in width::
 
+    root = StackLayout()
+    for i in range(25):
+        btn = Button(text=str(i), width=40 + i * 5, size_hint=(None, 0.15))
+        root.add_widget(btn)
+
+.. image:: images/stacklayout_sizing.png
+    :align: left
 '''
 
 __all__ = ('StackLayout', )
@@ -86,7 +95,8 @@ class StackLayout(Layout):
     '''
 
     minimum_width = NumericProperty(0)
-    '''Minimum width needed to contain all children.
+    '''Minimum width needed to contain all children. It is automatically set
+    by the layout.
 
     .. versionadded:: 1.0.8
 
@@ -95,7 +105,8 @@ class StackLayout(Layout):
     '''
 
     minimum_height = NumericProperty(0)
-    '''Minimum height needed to contain all children.
+    '''Minimum height needed to contain all children. It is automatically set
+    by the layout.
 
     .. versionadded:: 1.0.8
 
@@ -104,7 +115,8 @@ class StackLayout(Layout):
     '''
 
     minimum_size = ReferenceListProperty(minimum_width, minimum_height)
-    '''Minimum size needed to contain all children.
+    '''Minimum size needed to contain all children. It is automatically set
+    by the layout.
 
     .. versionadded:: 1.0.8
 
