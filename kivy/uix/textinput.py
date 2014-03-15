ecf2a3ca82c9a4d3d8953e38307c296a097b3f4d
mihaineacsu <mihai.neacsu@ymail.com>
2014-03-15 18:38:44 +0200

    Fix TextInput padding_x being ignored when calculating cursor postion (#1960)

--- kivy/uix/textinput.py
+++ kivy/uix/textinput.py
@@ -771,6 +771,7 @@ class TextInput(Widget):
         '''Return the (row, col) of the cursor from an (x, y) position.
         '''
         padding_top = self.padding[1]
+        padding_left = self.padding[0]
         l = self._lines
         dy = self.line_height + self.line_spacing
         cx = x - self.x
@@ -786,7 +787,7 @@ class TextInput(Widget):
         for i in range(1, len(l[cy]) + 1):
             if _get_text_width(l[cy][:i],
                                _tab_width,
-                               _label_cached) >= cx + scrl_x:
+                               _label_cached) + padding_left >= cx + scrl_x:
                 break
             dcx = i
         cx = dcx
