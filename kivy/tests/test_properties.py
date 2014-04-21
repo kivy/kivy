9bebc676abedb8651d92b16fc833b68bac53e643
Matthew Einhorn <moiein2000@gmail.com>
2014-04-21 13:26:51 -0400

    Add nosetests for property rebind.

--- kivy/tests/test_properties.py
+++ kivy/tests/test_properties.py
@@ -391,3 +391,106 @@ class PropertiesTestCase(unittest.TestCase):
 
         bnp.set(wid, -10)
         self.assertEqual(bnp.get(wid), -5)
+
+    def test_property_rebind(self):
+        from kivy.uix.label import Label
+        from kivy.uix.togglebutton import ToggleButton
+        from kivy.lang import Builder
+        from kivy.properties import ObjectProperty, DictProperty, AliasProperty
+        from kivy.clock import Clock
+
+        class ObjWidget(Label):
+            button = ObjectProperty(None, rebind=True, allownone=True)
+
+        class ObjWidgetRebindFalse(Label):
+            button = ObjectProperty(None, rebind=False, allownone=True)
+
+        class DictWidget(Label):
+            button = DictProperty({'button': None}, rebind=True,
+                                  allownone=True)
+
+        class DictWidgetFalse(Label):
+            button = DictProperty({'button': None}, rebind=False)
+
+        class AliasWidget(Label):
+            _button = None
+
+            def setter(self, value):
+                self._button = value
+                return True
+
+            def getter(self):
+                return self._button
+            button = AliasProperty(getter, setter, rebind=True)
+
+        Builder.load_string('''
+<ObjWidget>:
+    text: self.button.state if self.button is not None else 'Unset'
+
+<ObjWidgetRebindFalse>:
+    text: self.button.state if self.button is not None else 'Unset'
+
+<AliasWidget>:
+    text: self.button.state if self.button is not None else 'Unset'
+
+<DictWidget>:
+    text: self.button.button.state if self.button.button is not None\
+    else 'Unset'
+
+<DictWidgetFalse>:
+    text: self.button.button.state if self.button.button is not None\
+    else 'Unset'
+        ''')
+
+        obj = ObjWidget()
+        obj_false = ObjWidgetRebindFalse()
+        dict_rebind = DictWidget()
+        dict_false = DictWidgetFalse()
+        alias_rebind = AliasWidget()
+        button = ToggleButton()
+        Clock.tick()
+        self.assertEqual(obj.text, 'Unset')
+        self.assertEqual(obj_false.text, 'Unset')
+        self.assertEqual(dict_rebind.text, 'Unset')
+        self.assertEqual(dict_false.text, 'Unset')
+        self.assertEqual(alias_rebind.text, 'Unset')
+
+        obj.button = button
+        obj_false.button = button
+        dict_rebind.button.button = button
+        dict_false.button.button = button
+        alias_rebind.button = button
+        Clock.tick()
+        self.assertEqual(obj.text, 'normal')
+        self.assertEqual(obj_false.text, 'normal')
+        self.assertEqual(dict_rebind.text, 'normal')
+        self.assertEqual(dict_false.text, 'Unset')
+        self.assertEqual(alias_rebind.text, 'normal')
+
+        button.state = 'down'
+        Clock.tick()
+        self.assertEqual(obj.text, 'down')
+        self.assertEqual(obj_false.text, 'normal')
+        self.assertEqual(dict_rebind.text, 'down')
+        self.assertEqual(dict_false.text, 'Unset')
+        self.assertEqual(alias_rebind.text, 'down')
+
+        button.state = 'normal'
+        Clock.tick()
+        self.assertEqual(obj.text, 'normal')
+        self.assertEqual(obj_false.text, 'normal')
+        self.assertEqual(dict_rebind.text, 'normal')
+        self.assertEqual(dict_false.text, 'Unset')
+        self.assertEqual(alias_rebind.text, 'normal')
+
+        obj.button = None
+        obj_false.button = None
+        dict_rebind.button.button = None
+        dict_false.button.button = None
+        alias_rebind.button = None
+        Clock.tick()
+        self.assertEqual(obj.text, 'Unset')
+        self.assertEqual(obj_false.text, 'Unset')
+        self.assertEqual(dict_rebind.text, 'Unset')
+        self.assertEqual(dict_false.text, 'Unset')
+        self.assertEqual(alias_rebind.text, 'Unset')
