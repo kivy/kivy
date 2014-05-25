bbc4d357040d74e28fb4512be8b0a5e4111132c6
Alexander Wolf <awolf2904@gmail.com>
2014-05-25 21:59:28 +0200

    removed nottest import. Not needed anymore.

--- kivy/tests/test_adapters.py
+++ kivy/tests/test_adapters.py
@@ -1136,8 +1136,6 @@ class AdaptersTestCase(unittest.TestCase):
         dict_adapter.sorted_keys = ['dog']
         self.assertEqual(pet_listener.current_pets, ['dog'])
 
-    from nose.tools import nottest #test
-
     def test_dict_adapter_reset_data(self):
         class PetListener(object):
             def __init__(self, pet):
