99490994f69b6d33b72535046ed3e19bc4fc41f7
Terje Skjaeveland <terje@okse.no>
2014-06-06 15:23:16 +0200

    Add note about twisted on iOS

--- kivy/support.py
+++ kivy/support.py
@@ -153,7 +153,13 @@ def install_twisted_reactor(**kwargs):
     any signals unless you set the 'installSignalHandlers' keyword argument
     to 1 explicitly. This is done to allow kivy to handle the signals as
     usual unless you specifically want the twisted reactor to handle the
-    signals (e.g. SIGINT).'''
+    signals (e.g. SIGINT).
+
+    .. note::
+        Twisted is not included in iOS build by default. To use it on iOS,
+        put the twisted distribution (and zope.interface dependency) in your
+        application directory.
+    '''
     import twisted
 
     # prevent installing more than once
