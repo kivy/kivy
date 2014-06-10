8ff279f60bd7a1fe8939c48b0909d73678932106
tshirtman <gabriel.pettier@gmail.com>
2014-06-10 19:11:37 +0200

    fix disabled widgets eating events

--- kivy/uix/widget.py
+++ kivy/uix/widget.py
@@ -399,7 +399,7 @@ class Widget(WidgetBase):
             bool. If True, the dispatching of the touch event will stop.
         '''
         if self.disabled and self.collide_point(*touch.pos):
-            return True
+            return False
         for child in self.children[:]:
             if child.dispatch('on_touch_down', touch):
                 return True
