fe223b83e97d16c99adf50cb0908e440905de4ea
Zen-CODE <zenkey.zencode@gmail.com>
2014-06-29 09:01:28 +0200

    doc: grammar tweaks to adapters/__init__.py

--- kivy/adapters/__init__.py
+++ kivy/adapters/__init__.py
@@ -16,22 +16,22 @@ On the view side is :class:`AbstractView`, which is the base view for
     :doc:`api-kivy.adapters.adapter`,
     :doc:`api-kivy.adapters.simplelistadapter`,
     :doc:`api-kivy.adapters.listadapter`,
-    :doc:`api-kivy.adapters.dictadapter`,
+    :doc:`api-kivy.adapters.dictadapter`.
 
 - **Models**: The data for which an adapter serves as a bridge to views can be
-  any sort of data. However, as a convenience, model mixin classes can ease the
-  preparation of data, or the shaping for use in the system. For selection
-  operations, :class:`SelectableDataItem` can optionally prepare data items to
-  provide and receive selection information (data items are not required to be
-  "selection-aware", but in some cases it may be desired).
+  any sort of data. However, for convenience, model mixin classes can ease the
+  preparation or shaping of data for use in the system. For selection
+  operations, the :class:`SelectableDataItem` can optionally prepare data items
+  to provide and receive selection information (data items are not required to
+  be "selection-aware", but in some cases it may be desired).
 
-    :doc:`api-kivy.adapters.models`,
+    :doc:`api-kivy.adapters.models`.
 
 - **Args Converters**: Argument converters are made by the application
   programmer to do the work of converting data items to argument dictionaries
   suitable for instantiating views.
 
-    :doc:`api-kivy.adapters.args_converters`,
+    :doc:`api-kivy.adapters.args_converters`.
 
 ----
 '''
