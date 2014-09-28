2f9257a96468fe128de676ffe72ac4d43bc0338c
Zen-CODE <zenkey.zencode@gmail.com>
2014-09-28 08:57:38 +0200

    doc: removed extraneous code from example in uix/listview.py

--- kivy/uix/listview.py
+++ kivy/uix/listview.py
@@ -141,11 +141,9 @@ argument for each Label instantiation.
 You can declare a ListView with an adapter in a kv file with special attention
 given to the way longer python blocks are indented::
 
-    from kivy.uix.modalview import ModalView
-    from kivy.uix.listview import ListView
-    from kivy.uix.gridlayout import GridLayout
+    from kivy.uix.boxlayout import BoxLayout
+    from kivy.base import runTouchApp
     from kivy.lang import Builder
-    from kivy.factory import Factory
 
     # Note the special nature of indentation in the adapter declaration, where
     # the adapter: is on one line, then the value side must be given at one
@@ -155,11 +153,8 @@ given to the way longer python blocks are indented::
     #:import label kivy.uix.label
     #:import sla kivy.adapters.simplelistadapter
 
-    <ListViewModal>:
-        size_hint: None, None
-        size: 400, 400
+    <MyListView>:
         ListView:
-            size_hint: .8, .8
             adapter:
                 sla.SimpleListAdapter(
                 data=["Item #{0}".format(i) for i in range(100)],
@@ -167,25 +162,11 @@ given to the way longer python blocks are indented::
     """)
 
 
-    class ListViewModal(ModalView):
-        def __init__(self, **kwargs):
-            super(ListViewModal, self).__init__(**kwargs)
-
-
-    class MainView(GridLayout):
-
-        def __init__(self, **kwargs):
-            kwargs['cols'] = 1
-            super(MainView, self).__init__(**kwargs)
-
-            listview_modal = ListViewModal()
-
-            self.add_widget(listview_modal)
-
+    class MyListView(BoxLayout):
+        pass
 
     if __name__ == '__main__':
-        from kivy.base import runTouchApp
-        runTouchApp(MainView(width=800))
+        runTouchApp(MyListView())
 
 ListAdapter and DictAdapter
 ---------------------------
