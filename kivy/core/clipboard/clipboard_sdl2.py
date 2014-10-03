28ee5dd1d75f7e0d9cd78800e6c70ac838fbc77f
akshayaurora <akshayaurora@gmail.com>
2014-10-04 00:49:10 +0530

    clipboard:sdl2 Convert to unicode for both py2/3

--- kivy/core/clipboard/clipboard_sdl2.py
+++ kivy/core/clipboard/clipboard_sdl2.py
@@ -5,14 +5,13 @@ Clipboard SDL2: an implementation of the Clipboard using sdl2.
 __all__ = ('ClipboardSDL2', )
 
 from kivy.utils import platform
-from kivy.compat import PY2
 from kivy.core.clipboard import ClipboardBase
 
 if platform() not in ('win', 'linux', 'macosx', 'android', 'ios'):
     raise SystemError('unsupported platform for pygame clipboard')
 
 try:
-    from _clipboard_sdl2 import _get_text, _has_text, _set_text
+    from kivy.core.clipboard._clipboard_sdl2 import _get_text, _has_text, _set_text
 except ImportError:
     raise SystemError('extension not copiled??')
 
@@ -23,8 +22,7 @@ class ClipboardSDL2(ClipboardBase):
         return _get_text() if _has_text() else ''
 
     def copy(self, data=''):
-        if PY2:
-            data = data.encode('utf-8')
+        data = data.encode('utf-8')
         _set_text(data)
 
     def get_types(self):
