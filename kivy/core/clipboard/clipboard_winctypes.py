6ebd51e2920fd4e02e2f59a7cda67e334265f2b5
akshayaurora <akshayaurora@gmail.com>
2014-11-02 01:13:53 +0530

    core:clipboard. Minor optimisation.

--- kivy/core/clipboard/clipboard_winctypes.py
+++ kivy/core/clipboard/clipboard_winctypes.py
@@ -10,42 +10,47 @@ from kivy.core.clipboard import ClipboardBase
 if platform != 'win':
     raise SystemError('unsupported platform for Windows clipboard')
 
+user32 = ctypes.windll.user32
+kernel32 = ctypes.windll.kernel32
+msvcrt = ctypes.cdll.msvcrt
+c_char_p = ctypes.c_char_p
+
 
 class ClipboardWindows(ClipboardBase):
 
     def get(self, mimetype='text/plain'):
-        ctypes.windll.user32.OpenClipboard(0)
+        user32.OpenClipboard(0)
         # 1 is CF_TEXT
-        pcontents = ctypes.windll.user32.GetClipboardData(1)
-        data = ctypes.c_char_p(pcontents).value
+        pcontents = user32.GetClipboardData(1)
+        data = c_char_p(pcontents).value
         #ctypes.windll.kernel32.GlobalUnlock(pcontents)
-        ctypes.windll.user32.CloseClipboard()
+        user32.CloseClipboard()
         return data
 
     def put(self, data, mimetype='text/plain'):
         GMEM_DDESHARE = 0x2000
-        ctypes.windll.user32.OpenClipboard(0)
-        ctypes.windll.user32.EmptyClipboard()
+        user32.OpenClipboard(0)
+        user32.EmptyClipboard()
         try:
             # works on Python 2 (bytes() only takes one argument)
-            hCd = ctypes.windll.kernel32.GlobalAlloc(
+            hCd = kernel32.GlobalAlloc(
                 GMEM_DDESHARE, len(bytes(text)) + 1)
         except TypeError:
             # works on Python 3 (bytes() requires an encoding)
-            hCd = ctypes.windll.kernel32.GlobalAlloc(
+            hCd = kernel32.GlobalAlloc(
                 GMEM_DDESHARE, len(bytes(text, 'ascii')) + 1)
-        pchData = ctypes.windll.kernel32.GlobalLock(hCd)
+        pchData = kernel32.GlobalLock(hCd)
         try:
             # works on Python 2 (bytes() only takes one argument)
-            ctypes.cdll.msvcrt.strcpy(
-                ctypes.c_char_p(pchData), bytes(text))
+            msvcrt.strcpy(
+                c_char_p(pchData), bytes(text))
         except TypeError:
             # works on Python 3 (bytes() requires an encoding)
-            ctypes.cdll.msvcrt.strcpy(
-                ctypes.c_char_p(pchData), bytes(text, 'ascii'))
-        ctypes.windll.kernel32.GlobalUnlock(hCd)
-        ctypes.windll.user32.SetClipboardData(1, hCd)
-        ctypes.windll.user32.CloseClipboard()
+            msvcrt.strcpy(
+                c_char_p(pchData), bytes(text, 'ascii'))
+        kernel32.GlobalUnlock(hCd)
+        user32.SetClipboardData(1, hCd)
+        user32.CloseClipboard()
 
     def get_types(self):
         return list('text/plain',)
