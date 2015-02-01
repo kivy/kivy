6c707babe04b61190d9cf8dd75e7816356a5a180
Charles Merriam <charles.merriam@gmail.com>
2015-02-01 11:46:32 -0800

    examples/canvas/mesh.py update docstring

--- examples/canvas/mesh.py
+++ examples/canvas/mesh.py
@@ -2,6 +2,9 @@
 Mesh test
 =========
 
+This demonstrates the use of a mesh mode to distort an image.  You should see
+a line of buttons across the bottom of a canvas.  Pressing them displays
+the mesh, a small circle of points, with different mesh.mode settings.
 '''
 
 from kivy.uix.button import Button
