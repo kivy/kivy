23a59bbf924d9448565dc9efad55363d1c8aa0f6
Charles Merriam <charles.merriam@gmail.com>
2015-02-01 11:26:16 -0800

    Added examples/canvas/lines_extended.py docstring

--- examples/canvas/lines_extended.py
+++ examples/canvas/lines_extended.py
@@ -1,3 +1,12 @@
+'''
+Lines Extended Demo
+===================
+
+This demonstrates how to use the extended line drawing routines such
+as circles, elipses, and rectangles.  You should see a static image of
+labelled shapes on the screen.
+'''
+
 from kivy.app import App
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.widget import Widget
