df71137086766a5399d9f0006849be745b68dc78
Charles Merriam <charles.merriam@gmail.com>
2015-03-07 02:43:20 -0800

    Dynamically change kivy_catalog button color
    - Set the ‘Render Now’ button to red as soon as the .kv text is out of
    sync, and then set it to green when back in sync.   It provides a
    visual clue when the code renders.
    - Pick up items from the directory instead of having them hard coded in
    the kivycatalog.kv file.
    - Add categories, using the file names for the category clues.   A
    category spinner stays in sync with the current item.
    - Add left and right buttons to quickly scroll through items.
    - Remove the internal ‘’Container’ class and screen manager.  The right
    pane must be cleared and redrawn on every .kv change anyway.
    - Fix a bug where turning on Auto Redraw when the text had changed
    wouldn’t trigger a redraw.
    - Update doc string and screenshot for new features.
    - Document use of keycodes.
    - Remove dead code, e.g., self.carousel and unused parameters.
    - Rename existing example kv files so that they have categories.
    -

--- examples/demo/kivycatalog/main.py
+++ examples/demo/kivycatalog/main.py
@@ -3,22 +3,23 @@
 Kivy Catalog
 ============
 
-The Kivy Catalog viewer showcases widgets available in Kivy
+The Kivy Catalog viewer showcases demonstrations of widgets available in Kivy
 and allows interactive editing of kivy language code to get immediate
-feedback. You should see a two panel screen with a menu spinner button
-(starting with 'Welcome') and other controls across the top.The left pane
-contains kivy (.kv) code, and the right side is that code rendered. You can
-edit the left pane, though changes will be lost when you use the menu
-spinner button. The catalog will show you dozens of .kv examples controlling
-different widgets and layouts.
+feedback. You should see a two panel screen with controls across the top.
+The left pane contains kivy (.kv) code, and the right pane is that code
+rendered. When you edit the left pane, the 'Render Now' button will turn
+red until you pause for a moment and then the edited .kv code will be
+rendered in the right pane. Across the top, the spinner on
+the left jumps through categories of items while the spinner on the right
+lists each individual item. The catalog contains dozens of .kv examples
+controlling different widgets and layouts.
 
 The catalog's interface is set in the file kivycatalog.kv, while the
 interfaces for each menu option are set in containers_kvs directory. To
 add a new .kv file to the Kivy Catalog, add a .kv file into the container_kvs
-directory and reference that file in the ScreenManager section of
-kivycatalog.kv.
+directory. If you want the .kv file in a particular category, name it
+'MyCategoryName__MyMenuName.kv'.
 
-Known bugs include some issue with the drop
 '''
 import kivy
 kivy.require('1.4.2')
@@ -27,7 +28,7 @@ import sys
 from kivy.app import App
 from kivy.factory import Factory
 from kivy.lang import Builder, Parser, ParserException
-from kivy.properties import ObjectProperty
+from kivy.properties import ObjectProperty, BooleanProperty
 from kivy.config import Config
 from kivy.compat import PY2
 
@@ -35,51 +36,16 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.codeinput import CodeInput
 from kivy.animation import Animation
 from kivy.clock import Clock
-
-CATALOG_ROOT = os.path.dirname(__file__)
+from glob import glob
+import re
 
 #Config.set('graphics', 'width', '1024')
 #Config.set('graphics', 'height', '768')
 
-'''List of classes that need to be instantiated in the factory from .kv files.
-'''
-CONTAINER_KVS = os.path.join(CATALOG_ROOT, 'container_kvs')
-CONTAINER_CLASSES = [c[:-3] for c in os.listdir(CONTAINER_KVS)
-    if c.endswith('.kv')]
-
-
-class Container(BoxLayout):
-    '''A container is essentially a class that loads its root from a known
-    .kv file.
-
-    The name of the .kv file is taken from the Container's class.
-    We can't just use kv rules because the class may be edited
-    in the interface and reloaded by the user.
-    See :meth: change_kv where this happens.
-    '''
-
-    def __init__(self, **kwargs):
-        super(Container, self).__init__(**kwargs)
-        self.previous_text = open(self.kv_file).read()
-        parser = Parser(content=self.previous_text)
-        widget = Factory.get(parser.root.name)()
-        Builder._apply_rule(widget, parser.root, parser.root)
-        self.add_widget(widget)
-
-    @property
-    def kv_file(self):
-        '''Get the name of the kv file, a lowercase version of the class
-        name.
-        '''
-        return os.path.join(CONTAINER_KVS, self.__class__.__name__ + '.kv')
-
-
-for class_name in CONTAINER_CLASSES:
-    globals()[class_name] = type(class_name, (Container,), {})
-
 
 class KivyRenderTextInput(CodeInput):
     def keyboard_on_key_down(self, window, keycode, text, modifiers):
+        ''' Handle ctrl-s (or Meta for OS/X) to re-render '''
         is_osx = sys.platform == 'darwin'
         # Keycodes on OSX:
         ctrl, cmd = 64, 1024
@@ -117,56 +83,104 @@ class Catalog(BoxLayout):
     You do not need to edit any python code, just .kv language files!
     '''
     language_box = ObjectProperty()
-    screen_manager = ObjectProperty()
+    current_render = ObjectProperty()
+    need_reload = BooleanProperty()  # render synced with language
+    category_spinner = ObjectProperty()
+    name_spinner = ObjectProperty()
 
-    def __init__(self, **kwargs):
-        self._previously_parsed_text = ''
+    def __init__(self, infos, **kwargs):
+        self.infos = infos
+        self.categories = Info.get_categories(infos)
+        self.category_list = sorted(set(self.categories.values()))
+        self.previous_text = ''
+        self.previous_index = 0
+
         super(Catalog, self).__init__(**kwargs)
-        self.show_kv(None, 'Welcome')
-        self.carousel = None
+        # for info in infos:
+        #    c = Container(kv_file=info.fullname, name=info.name)
+        #    self.screen_manager.add_widget(c)
+        self.show_kv()
 
-    def show_kv(self, instance, value):
-        '''Called when an a item is selected, we need to show the .kv language
-        file associated with the newly revealed container.'''
+    def guess_index(self, name, default=None):
+        ''' return index into infos array of the name. If there
+        are two copies of the name in the index, pick one. If the name
+        isn't in the infos array, return default.
+        '''
+        info = [index for index, inf in enumerate(self.infos)
+                if inf.name.lower() == name.lower()]
+        if info:
+            return info[0]   # TODO: keep track of last index to guess better
+            # guessing only needed for two or more identically named items.
+        else:
+            return default
 
-        self.screen_manager.current = value
+    def show_kv(self, name=None, category=None, prev=None, next=None):
+        ''' Called when a different kv file is selected. When selected,
+        the .kv file is loaded from disk.
 
-        child = self.screen_manager.current_screen.children[0]
-        with open(child.kv_file, 'rb') as file:
-            self.language_box.text = file.read().decode('utf8')
+        One or zero of the parameters will specify which kv file is
+        selected.
+        '''
+
+        if name is not None:
+            index = self.guess_index(name, default=0)
+        elif category is not None:
+            index = min([i for i, info in enumerate(self.infos)
+                         if info.category == category])
+        elif prev is not None:
+            index = (self.previous_index - 1) % len(self.infos)
+        elif next is not None:
+            index = (self.previous_index + 1) % len(self.infos)
+        else:
+            index = 0
+
+        info = self.infos[index]
+        self.previous_index = index
+        self.category_spinner.text = info.category
+        self.name_spinner.text = info.name
+
+        with open(info.fullname, 'rb') as f:
+            self.language_box.text = f.read().decode('utf8')
         Clock.unschedule(self.change_kv)
         self.change_kv()
-        # reset undo/redo history
         self.language_box.reset_undo()
 
     def schedule_reload(self):
+        ''' if the language_box text changed since last called, note a
+        reload is needed and schedule one for a couple seconds from now.
+        '''
+
+        txt = self.language_box.text
+        if txt == self.previous_text:
+            return
+        self.need_reload = True
         if self.auto_reload:
-            txt = self.language_box.text
-            child = self.screen_manager.current_screen.children[0]
-            if txt == child.previous_text:
-                return
-            child.previous_text = txt
+            self.previous_text = txt
             Clock.unschedule(self.change_kv)
             Clock.schedule_once(self.change_kv, 2)
 
-    def change_kv(self, *largs):
-        '''Called when the update button is clicked. Needs to update the
-        interface for the currently active kv widget, if there is one based
-        on the kv file the user entered. If there is an error in their kv
+    def change_kv(self, *_):
+        ''' Called to rerender the widget, e.g., when the update button
+        is clicked, the update timer clicks, or the user selects a new
+        screen. It rerenders the widget from the language_box text.
+        If there is an error in the kv
         syntax, show a nice popup.'''
 
         txt = self.language_box.text
-        kv_container = self.screen_manager.current_screen.children[0]
+        kv_container = self.current_render
         try:
             parser = Parser(content=txt)
             kv_container.clear_widgets()
             widget = Factory.get(parser.root.name)()
             Builder._apply_rule(widget, parser.root, parser.root)
             kv_container.add_widget(widget)
+            self.need_reload = False
         except (SyntaxError, ParserException) as e:
             self.show_error(e)
+            self.need_reload = True
         except Exception as e:
             self.show_error(e)
+            self.need_reload = True
 
     def show_error(self, e):
         self.info_label.text = str(e)
@@ -176,16 +190,81 @@ class Catalog(BoxLayout):
         self.anim.start(self.info_label)
 
 
+class Info():
+    ''' info on one named screen and its associated .kv file '''
+
+    def __init__(self, filename):
+        self.fullname = filename
+        self.base = os.path.basename(filename)
+        pat = r"(?:(.+)__)?(.*)\.kv"
+        m = re.match(pat, self.base)
+        assert m is not None, "File name must end in .kv"
+        g = m.groups()
+        self.demo_name = g[1]
+        self.category = g[0] if g[0] is not None else "Other"
+        self.name = self.demo_name
+        self.name = re.sub('_', ' ', self.name)
+        # add space between camel case words)
+        self.name = re.sub(r'([^ ])([A-Z][a-z])', r'\1 \2', self.name)
+        print "name is ", self.name
+
+    @classmethod
+    def yield_kv_filenames(cls, globs):
+        ''' yield each filename named by the globs given or by directories
+        named in those globs. '''
+        for g in globs:
+            glob_list = glob(g)
+            for fn in glob_list:
+                if os.path.isdir(fn):
+                    for filename in os.listdir(fn):
+                        fullname = os.path.join(fn, filename)
+                        if (os.path.isfile(fullname) and
+                                filename.endswith('.kv')):
+                            yield fullname
+                elif os.path.isfile(fn) and fn.endswith('.kv'):
+                    yield fn
+
+    @classmethod
+    def get_infos(cls, *globs):
+        ''' takes filenames, directory names, and names with wildcards (globs)
+        and returns an array of info[] with an item for each filename passed.
+        '''
+        infos = []
+        for filename in cls.yield_kv_filenames(globs):
+            if filename in [i.fullname for i in infos]:
+                continue  # exact filename was specified more than once
+            info = cls(filename)
+            infos.append(info)
+            print "Found ", filename
+        return sorted(infos, key=lambda x: x.base)
+
+    @classmethod
+    def get_default_infos(cls):
+        kvdir = os.path.join(os.path.dirname(__name__), 'container_kvs')
+        infos = cls.get_infos(kvdir)
+        return infos
+
+    @classmethod
+    def get_categories(cls, infos):
+        ''' returns a dict of [fullname] = category name for infos given. '''
+        categories = {}
+        for info in infos:
+            categories[info.fullname] = info.category
+        return categories
+
+
 class KivyCatalogApp(App):
     '''The kivy App that runs the main root. All we do is build a catalog
     widget into the root.'''
 
     def build(self):
-        return Catalog()
+        infos = Info.get_default_infos()
+        return Catalog(infos)
 
     def on_pause(self):
         return True
 
-
 if __name__ == "__main__":
+
+    Info.get_infos('container_kvs', 'container_kvs/TextContainer.kv')
     KivyCatalogApp().run()
