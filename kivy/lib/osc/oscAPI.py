f1a9ef14431093a7ec69199000bb3683f8bce8e5
Anders H <anders@hogqvist.se>
2015-04-27 10:40:28 +0200

    Update oscAPI.py
    Variable name changed to thread_id

--- kivy/lib/osc/oscAPI.py
+++ kivy/lib/osc/oscAPI.py
@@ -239,30 +239,30 @@ def listen(ipAddr='127.0.0.1', port=9001):
     defaults to ipAddr='127.0.0.1', port 9001
     '''
     global oscThreads
-    _id = '%s:%d' % (ipAddr, port)
-    if _id in oscThreads:
+    thread_id = '%s:%d' % (ipAddr, port)
+    if thread_id in oscThreads:
         return
-    Logger.debug('OSC: Start thread <%s>' % _id)
-    oscThreads[_id] = OSCServer(ipAddr=ipAddr, port=port)
-    oscThreads[_id].start()
-    return _id
+    Logger.debug('OSC: Start thread <%s>' % thread_id)
+    oscThreads[thread_id] = OSCServer(ipAddr=ipAddr, port=port)
+    oscThreads[thread_id].start()
+    return thread_id
 
 
-def dontListen(_id = None):
+def dontListen(thread_id = None):
     '''closes the socket and kills the thread
     '''
     global oscThreads
-    if _id and _id in oscThreads:
-        ids = [_id]
+    if thread_id and thread_id in oscThreads:
+        ids = [thread_id]
     else:
         ids = list(oscThreads.keys())
-    for _id in ids:
-        #oscThreads[_id].socket.close()
-        Logger.debug('OSC: Stop thread <%s>' % _id)
-        oscThreads[_id].isRunning = False
-        oscThreads[_id].join()
-        Logger.debug('OSC: Stop thread <%s> finished' % _id)
-        del oscThreads[_id]
+    for thread_id in ids:
+        #oscThreads[thread_id].socket.close()
+        Logger.debug('OSC: Stop thread <%s>' % thread_id)
+        oscThreads[thread_id].isRunning = False
+        oscThreads[thread_id].join()
+        Logger.debug('OSC: Stop thread <%s> finished' % thread_id)
+        del oscThreads[thread_id]
 
 if __name__ == '__main__':
     # example of how to use oscAPI
