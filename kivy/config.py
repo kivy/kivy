2219de07e16b37d0ee734d9580a4b22d7f12632b
dessant <dessant@users.noreply.github.com>
2015-06-20 21:52:01 +0300

    do not overwrite config options which were already set

--- kivy/config.py
+++ kivy/config.py
@@ -754,11 +754,11 @@ if not environ.get('KIVY_DOC_INCLUDE'):
             Config.setdefault('kivy', 'pause_on_minimize', '0')
 
         elif version == 12:
-            Config.set('graphics', 'window_state', 'visible')
+            Config.setdefault('graphics', 'window_state', 'visible')
 
         elif version == 13:
-            Config.set('graphics', 'minimum_width', '0')
-            Config.set('graphics', 'minimum_height', '0')
+            Config.setdefault('graphics', 'minimum_width', '0')
+            Config.setdefault('graphics', 'minimum_height', '0')
 
         # elif version == 1:
         #    # add here the command for upgrading from configuration 0 to 1
