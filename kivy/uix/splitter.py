ea827f56586fc2082fc1fc56af591dc619913aad
akshayaurora <akshayaurora@gmail.com>
2015-08-02 02:25:33 +0530

    uix:Splitter closes #3495

--- kivy/uix/splitter.py
+++ kivy/uix/splitter.py
@@ -271,6 +271,8 @@ class Splitter(BoxLayout):
         self.rescale_parent_proportion()
 
     def rescale_parent_proportion(self, *args):
+        if not self.parent:
+            return
         if self.rescale_with_parent:
             parent_proportion = self._parent_proportion
             if self.sizable_from in ('top', 'bottom'):
