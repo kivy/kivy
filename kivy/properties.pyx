a57b77e93c761c42091e3320d96b0b61c9076113
Zen-CODE <zenkey.zencode@gmail.com>
2015-08-03 22:40:09 +0200

    doc: separated explanations for ValueError handling in properties.pyx

--- kivy/properties.pyx
+++ kivy/properties.pyx
@@ -86,16 +86,15 @@ Error Handling
 ~~~~~~~~~~~~~~
 
 If setting a value would otherwise raise a ValueError, you have two options to
-handle the error gracefully within the property. An errorvalue is a substitute
-for the invalid value. An errorhandler is a callable (single argument function
-or lambda) which can return a valid substitute.
-
-errorvalue parameter::
+handle the error gracefully within the property. The first option is to use an
+errorvalue parameter. An errorvalue is a substitute for the invalid value.::
 
     # simply returns 0 if the value exceeds the bounds
     bnp = BoundedNumericProperty(0, min=-500, max=500, errorvalue=0)
 
-errorhandler parameter::
+The second option in to use an errorhandler parameter. An errorhandler is a
+callable (single argument function or lambda) which can return a valid
+substitute.::
 
     # returns the boundary value when exceeded
     bnp = BoundedNumericProperty(0, min=-500, max=500,
