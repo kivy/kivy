dbea8d8232835a9e2953c91036728a9eadc4eeac
Andres Vargas <andnovar@gmail.com>
2015-08-03 10:29:14 -0400

    Addding a pop up to save a file (progress)

--- kivy/ext/mpl/backend_kivy.py
+++ kivy/ext/mpl/backend_kivy.py
@@ -46,6 +46,8 @@ from kivy.uix.button import Button
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.relativelayout import RelativeLayout
+from kivy.uix.popup import Popup
+from kivy.properties import ObjectProperty
 
 import numpy as np
 import io
@@ -63,6 +65,12 @@ from math import cos, sin, pi
 _debug = True
 
 app = None
+
+
+class SaveDialog(FloatLayout):
+    save = ObjectProperty(None)
+    text_input = ObjectProperty(None)
+    cancel = ObjectProperty(None)
 
 
 class MPLKivyApp(App):
@@ -318,6 +326,9 @@ class RendererKivy(RendererBase):
 
 class NavigationToolbar2Kivy(NavigationToolbar2):
 
+    savefile = ObjectProperty(None)
+    text_input = ObjectProperty(None)
+
     def __init__(self, canvas, **kwargs):
         self.actionbar = ActionBar(pos_hint={'top': 1.0})
         super(NavigationToolbar2Kivy, self).__init__(canvas)
@@ -346,6 +357,23 @@ class NavigationToolbar2Kivy(NavigationToolbar2):
 
     def configure_subplots(self):
         pass
+
+    def dismiss_popup(self):
+        self._popup.dismiss()
+
+    def show_save(self):
+        content = SaveDialog(save=self.save, cancel=self.dismiss_popup)
+        self._popup = Popup(title="Save file", content=content,
+                            size_hint=(0.9, 0.9))
+        self._popup.open()
+
+    def save(self, path, filename):
+        with open(os.path.join(path, filename), 'w') as stream:
+            stream.write(self.text_input.text)
+        self.dismiss_popup()
+
+    def save_figure(self, *args):
+        self.show_save()
 
 
 class ToolbarKivy(ToolContainerBase, ActionView):
