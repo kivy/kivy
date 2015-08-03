323b4fead0df51e6f1d67e7ff4e64771ef96f4e8
Andres Vargas <andnovar@gmail.com>
2015-08-03 17:59:07 -0400

    __init.py for crossplatform compatibility.

---
+++ kivy/ext/mpl/__init__.py
@@ -0,0 +1,8 @@
+from kivy.ext.mpl.backend_kivy import FigureCanvasKivy, FigureManagerKivy,\
+                                    RendererKivy, GraphicsContextKivy,\
+                                    NavigationToolbar2Kivy, MPLKivyApp
+
+__all__ = (FigureCanvasKivy.__name__, FigureManagerKivy.__name__,
+           RendererKivy.__name__, GraphicsContextKivy.__name__,
+           NavigationToolbar2Kivy.__name__, MPLKivyApp.__name__)
+
