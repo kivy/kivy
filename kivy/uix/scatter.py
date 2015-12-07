6e46076b613debc7403a1deebafcacf7e467ad46
The Cheaterman <the.cheaterman@gmail.com>
2015-12-07 16:45:36 +0100

    Some fixes to ScatterBehavior's touch handlers

--- kivy/uix/scatter.py
+++ kivy/uix/scatter.py
@@ -327,9 +327,10 @@ class ScatterBehavior(object):
         x, y = touch.pos
 
         if self.do_dispatch_after_children:
-            if not self.collide_point(x, y):
-                touch.ud[self._get_scatter_behavior_uid('scatter_avoid')] = True
-                return False
+            if not self.do_collide_after_children:
+                if not self.collide_point(x, y):
+                    touch.ud[self._get_scatter_behavior_uid('scatter_avoid')] = True
+                    return False
 
             touch.grab(self)
             self._touches.append(touch)
@@ -399,7 +400,8 @@ class ScatterBehavior(object):
             ud = touch.ud.get(uid, {})
 
             if self._get_scatter_behavior_uid('scatter_avoid') in touch.ud:
-                return False
+                if not self.do_collide_after_children:
+                    return False
             elif ud.get('_mode') == 'dispatch':
                 touch.push()
                 touch.apply_transform_2d(self.to_local)
