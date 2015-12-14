a87d082710a24c23038f69374cc1423d073ae505
dessant <dessant@users.noreply.github.com>
2015-12-15 01:49:30 +0200

    make cython a compile time dep

--- setup.py
+++ setup.py
@@ -1016,4 +1016,5 @@ setup(
         'Topic :: Software Development :: User Interfaces'],
     dependency_links=[
         'https://github.com/kivy-garden/garden/archive/master.zip'],
-    install_requires=['cython>=' + MIN_CYTHON_STRING, 'Kivy-Garden>=0.1.4'])
+    install_requires=['Kivy-Garden>=0.1.4'],
+    setup_requires=['cython>=' + MIN_CYTHON_STRING])
