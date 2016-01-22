ceee503b270adc73268e31614285c69cb182ce1f
Zen-CODE <zenkey.zencode@gmail.com>
2016-01-22 15:56:20 +0200

    Corrected link to osc

--- kivy/lib/__init__.py
+++ kivy/lib/__init__.py
@@ -6,7 +6,7 @@ Kivy comes with other python/C libraries:
 
 * :mod:`~kivy.lib.ddsfile` - used for parsing and saving
   `DDS <https://en.wikipedia.org/wiki/DirectDraw_Surface>`_ files.
-* :mod:`kivy.lib.osc` - a modified/optimized version of PyOSC for using
+* :mod:`~kivy.lib.osc` - a modified/optimized version of PyOSC for using
   the `Open Sound Control <https://en.wikipedia.org/wiki/Open_Sound_Control>`_
   protocol.
 * mtdev
