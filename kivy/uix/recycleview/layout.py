609bb3a6300a7371e20ef0a3f9486d5549a63ea8
Matthew Einhorn <moiein2000@gmail.com>
2016-03-21 17:43:40 -0400

    Fix rv selection.

--- kivy/uix/recycleview/layout.py
+++ kivy/uix/recycleview/layout.py
@@ -11,12 +11,41 @@ class LayoutChangeException(Exception):
     pass
 
 
-class LayoutSelectionMixIn(CompoundSelectionBehavior):
+class LayoutSelectionBehavior(CompoundSelectionBehavior):
+    '''The :class:`LayoutSelectionBehavior` can be combined with
+    :class:`RecycleLayoutManagerBehavior` to allow its derived classes
+    selection behaviors similarly to how
+    :class:`~kivy.uix.behaviors.compoundselection.CompoundSelectionBehavior`
+    can be used to add selection behaviors to normal layout.
 
-    key_selection = StringProperty('')
-    '''The key used to decide whether a view of a data item can be selected
-    with touch or the keyboard. All data items can be selected directly
-    using `select_node`.
+    :class:`RecycleLayoutManagerBehavior` manages its children
+    differently than normal layouts or widgets so this class adapts
+    :class:`~kivy.uix.behaviors.compoundselection.CompoundSelectionBehavior`
+    based selection to work with :class:`RecycleLayoutManagerBehavior` as well.
+
+    Similarly to
+    :class:`~kivy.uix.behaviors.compoundselection.CompoundSelectionBehavior`,
+    one can select using the keyboard or touch, which calls :meth:`select_node`
+    or :meth:`deselect_node`, or one can call these methods directly. When a
+    item is selected or deselected :meth:`apply_selection` is called. See
+    :meth:`apply_selection`.
+
+
+    '''
+
+    key_selection = StringProperty(None, allownone=True)
+    '''The key used to check whether a view of a data item can be selected
+    with touch or the keyboard.
+
+    :attr:`key_selection` is the key in data, which if present and ``True``
+    will enable selection for this item from the keyboard or with a touch.
+    When None, the default, not item will be selectable.
+
+    :attr:`key_selection` is a :class:`StringProperty` and defaults to None.
+
+    .. note::
+        All data items can be selected directly using :meth:`select_node` or
+        :meth:`deselect_node`, even if :attr:`key_selection` is False.
     '''
 
     _selectable_nodes = []
@@ -24,18 +53,21 @@ class LayoutSelectionMixIn(CompoundSelectionBehavior):
 
     def __init__(self, **kwargs):
         self.nodes_order_reversed = False
-        super(LayoutSelectionMixIn, self).__init__(**kwargs)
+        super(LayoutSelectionBehavior, self).__init__(**kwargs)
 
-    def compute_layout(self, *largs, **kwargs):
+    def compute_sizes_from_data(self, data, flags):
         # overwrite this method so that when data changes we update
         # selectable nodes.
         key = self.key_selection
-        nodes = self._selectable_nodes = [
-            i for i, d in enumerate(self.recycleview.data) if d.get(key)]
-        self._nodes_map = {v: k for k, v in enumerate((nodes))}
-        return super(
-            LayoutSelectionMixIn, self).compute_layout(
-                *largs, **kwargs)
+        if key is None:
+            nodes = self._selectable_nodes = []
+        else:
+            nodes = self._selectable_nodes = [
+                i for i, d in enumerate(data) if d.get(key)]
+
+        self._nodes_map = {v: k for k, v in enumerate(nodes)}
+        return super(LayoutSelectionBehavior, self).compute_sizes_from_data(
+            data, flags)
 
     def get_selectable_nodes(self):
         # the indices of the data is used as the nodes
@@ -46,25 +78,50 @@ class LayoutSelectionMixIn(CompoundSelectionBehavior):
         return self._nodes_map[node]
 
     def goto_node(self, key, last_node, last_node_idx):
-        node, idx = super(LayoutSelectionMixIn, self).goto_node(
+        node, idx = super(LayoutSelectionBehavior, self).goto_node(
             key, last_node, last_node_idx)
         if node is not last_node:
-            self.show_index_view(node)
+            self.goto_view(node)
         return node, idx
 
     def select_node(self, node):
-        if super(LayoutSelectionMixIn, self).select_node(node):
+        if super(LayoutSelectionBehavior, self).select_node(node):
             view = self.recycleview.view_adapter.get_visible_view(node)
             if view is not None:
                 self.apply_selection(node, view, True)
 
     def deselect_node(self, node):
-        if super(LayoutSelectionMixIn, self).deselect_node(node):
+        if super(LayoutSelectionBehavior, self).deselect_node(node):
             view = self.recycleview.view_adapter.get_visible_view(node)
             if view is not None:
                 self.apply_selection(node, view, False)
 
     def apply_selection(self, index, view, is_selected):
+        '''Applies the selection to the view. This is called internally when
+        a view is displayed and it needs to be shown as selected or as not
+        selected.
+
+        It is called when :meth:`select_node` or :meth:`dselect_node` is called
+        or when a view needs to be refreshed. Its function is purely to update
+        the view to reflect the selection state. So the function may be
+        called multiple times even if the selection state may not have changed.
+
+        If the view is a instance of
+        :class:`~kivy.uix.recycleview.views.RecycleDataViewBehavior`, its
+        :meth:`~kivy.uix.recycleview.views.RecycleDataViewBehavior.\
+apply_selection` method will be called everything the view needs to refresh
+        the selection state. Otherwise, the this method is responsible
+        for applying the selection.
+
+        :Parameters:
+
+            `index`: int
+                The index of the data item that is associated with the view.
+            `view`: widget
+                The widget that is the view of this data item.
+            `is_selected`: bool
+                Whether the item is selected.
+        '''
         viewclass = view.__class__
         if viewclass not in _view_base_cache:
             _view_base_cache[viewclass] = isinstance(view, RecycleDataViewBehavior)
@@ -73,7 +130,7 @@ class LayoutSelectionMixIn(CompoundSelectionBehavior):
             view.apply_selection(self.recycleview, index, is_selected)
 
     def refresh_view_layout(self, index, pos, size, size_hint, view, viewport):
-        super(LayoutSelectionMixIn, self).refresh_view_layout(
+        super(LayoutSelectionBehavior, self).refresh_view_layout(
             index, pos, size, size_hint, view, viewport)
         self.apply_selection(index, view, index in self.selected_nodes)
 
