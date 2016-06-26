5261c30ef3ac707508dda1b7c786d258ad20a65e
gabriel pettier <dev@tangibledisplay.com>
2016-06-26 21:30:07 +0200

    osc: convert to bytes in a lot of places, for python3
    this only concerns the sending part of the osc api, which we don't
    support officially, but which is kind of useful for projects who needs
    an osc or tuio implementation.

--- kivy/lib/osc/oscAPI.py
+++ kivy/lib/osc/oscAPI.py
@@ -142,7 +142,7 @@ def createBundle():
     '''
     b = OSC.OSCMessage()
     b.address = ""
-    b.append("#bundle")
+    b.append(b"#bundle")
     b.append(0)
     b.append(0)
     return b
@@ -151,7 +151,7 @@ def createBundle():
 def appendToBundle(bundle, oscAddress, dataArray):
     '''create OSC mesage and append it to a given bundle
     '''
-    bundle.append( createBinaryMsg(oscAddress, dataArray),  'b')
+    bundle.append(createBinaryMsg(oscAddress, dataArray),  b'b')
 
 
 def sendBundle(bundle, ipAddr='127.0.0.1', port=9000) :
