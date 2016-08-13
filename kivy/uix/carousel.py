e7f81e3ccb219caea4316aacc6eb13de9522460e
Atis <Bakterija@users.noreply.github.com>
2016-08-13 17:16:32 +0300

    pep8

--- kivy/uix/carousel.py
+++ kivy/uix/carousel.py
@@ -529,11 +529,11 @@ class Carousel(StencilView):
 
     def on_touch_move(self, touch):
         if self.touch_mode_change == False:
-            if self.ignore_perpendicular_swipes and self.direction in ('top','bottom'):
+            if self.ignore_perpendicular_swipes and self.direction in ('top', 'bottom'):
                 if abs(touch.ox - touch.x) > self.scroll_distance:
                     self._change_touch_mode()
                     self.touchModeChange = True
-            elif self.ignore_perpendicular_swipes and self.direction in ('right','left'):
+            elif self.ignore_perpendicular_swipes and self.direction in ('right', 'left'):
                 if abs(touch.oy - touch.y) > self.scroll_distance:
                     self._change_touch_mode()
                     self.touchModeChange = True
