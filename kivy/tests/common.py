7e2aee7fbffa21f6f3ff00f204d68daa42adfa0f
Piotr Kasprzyk <ciri@ciri.pl>
2016-09-02 12:02:12 +0200

    Fix comparison misspelling

--- kivy/tests/common.py
+++ kivy/tests/common.py
@@ -186,7 +186,7 @@ class GraphicUnitTest(_base):
                     fd.write('<td><img src="test_%s"/></td>' %
                              basename(reffn))
                 else:
-                    fd.write('<td>First time, no comparaison.</td>')
+                    fd.write('<td>First time, no comparison.</td>')
                 fd.write('<td><pre>%s</pre></td>' % sourcecode)
                 fd.write('</table></div>')
         finally:
