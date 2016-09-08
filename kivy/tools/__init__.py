29c463bc53c08c5e4c18e11387739826e2d7b611
Richard <zenkey.zencode@gmail.com>
2016-09-08 22:47:28 +0200

    doc: bullted list, extended kviewer, packaging docs

--- kivy/tools/__init__.py
+++ kivy/tools/__init__.py
@@ -5,16 +5,16 @@ Tools
 The tools module provides various utility scripts and examples. These include
 scripts for:
 
-#. viewing kv files
-#. benchmarking
-#. debug reports
-#. texture compression
-#. building kv extensions
-#. text editor highlighting
-#. packaging
-#. pep8 checking
-#. theming
-#. travis continuous integration
+* :mod:`~kivy.tools.kviewer`: for viewing kv files with automatic updating
+* benchmarking
+* debug reports
+* texture compression
+* building kv extensions
+* text editor highlighting
+* :mod:`~kivy.tools.packaging`
+* pep8 checking
+* theming
+* travis continuous integration
 
 This help documenmt is a work-in-progress and currently under construction.
 
