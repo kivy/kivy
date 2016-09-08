098fc52f9e32f84e41f48bd4a38385e571c84cec
Richard <zenkey.zencode@gmail.com>
2016-09-08 21:59:22 +0200

    doc: Started with tools docs

---
+++ kivy/tools/__init__.py
@@ -0,0 +1,22 @@
+'''
+Tools
+=====
+
+The tools module provides various utility scripts and examples. These include
+scripts for:
+
+#. viewing kv files
+#. benchmarking
+#. debug reports
+#. texture compression
+#. building kv extensions
+#. text editor highlighting
+#. packaging
+#. pep8 checking
+#. theming
+#. travis continuous integration
+
+This help documenmt is a work-in-progress and currently under construction.
+
+'''
+
