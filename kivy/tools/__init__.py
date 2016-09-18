809cc9b9d1129d1a829a33752a969ddd2cf6e957
Richard <zenkey.zencode@gmail.com>
2016-09-18 14:51:58 +0200

    docs: extended tools docs

--- kivy/tools/__init__.py
+++ kivy/tools/__init__.py
@@ -6,17 +6,22 @@ The tools module provides various utility scripts and examples. These include
 scripts such as:
 
 * :file:`kviewer.py`: for viewing kv files with automatic updating
-* :file:`benchmark.py': provides detailed OpenGL hardware information as well
+* :file:`benchmark.py`: provides detailed OpenGL hardware information as well
   as some benchmarks measuring kivy specific performance
-* debug reports
-* texture compression
-* building kv extensions
+* :file:`reports.py`: provides a comprehensive report covering your systems
+  providers, libraries, configuration, environment, input devices and options
+* :file:`texturecompress.py`: a command line utility for compressing images into
+  PVRTC or ETC1 formats
+* :file:`generate-icons.py`: generates set of icons suitable for the various
+  store and package formats
+* :file:`extensions/make-kivyext.py`: a helper script for building kivy
+  extensions
 * text editor highlighting
 * :mod:`~kivy.tools.packaging`
 * pep8 checking
 * theming
 * travis continuous integration
 
-This help documenmt is a work-in-progress and currently under construction.
+This help document is a work-in-progress and currently under construction.
 
 """
