b6be8a89e25361cee193c0fa59807ae78133a6f1
Susmit <susmit9370@gmail.com>
2016-12-14 17:13:05 +0530

    Layout exploitation example.
    The following code  shows all possible layouts possible in the Kivy. The parent layout is PageLayout. Various nesting of layouts is also implemented.

---
+++ layouts/main.py
@@ -0,0 +1,18 @@
+from kivy.app import App
+from kivy.uix.button import Button
+from kivy.uix.label import Label
+from kivy.uix.anchorlayout import AnchorLayout
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.pagelayout import PageLayout
+from kivy.uix.relativelayout import RelativeLayout
+from kivy.uix.scatterlayout import ScatterLayout
+from kivy.uix.stacklayout import StackLayout
+
+
+class Testapp(App):
+    pass
+
+if __name__ == '__main__':
+    Testapp().run()
