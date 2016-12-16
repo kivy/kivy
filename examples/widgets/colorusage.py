1c94c9af439ae953d3c20de513e96691f2a09b37
dessant <git@rmn.space>
2016-12-16 10:52:07 +0200

    use gridlayout and a different color for color usage example

--- examples/widgets/colorusage.py
+++ examples/widgets/colorusage.py
@@ -1,12 +1,12 @@
 from kivy.app import App
-from kivy.uix.label import Label
-from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.gridlayout import GridLayout
 from kivy.lang import Builder
 
-Builder.load_string(''' #:import hex kivy.utils.get_color_from_hex
+Builder.load_string("""
+#:import hex kivy.utils.get_color_from_hex
 
 <Root>:
-    orientation: 'vertical'
+    cols: 2
     canvas:
         Color:
             rgba: 1, 1, 1, 1
@@ -14,52 +14,49 @@ Builder.load_string(''' #:import hex kivy.utils.get_color_from_hex
             pos: self.pos
             size: self.size
 
-    BoxLayout:
-        Label:
-            canvas.before:
-                Color:
-                    rgb: 205.0/255, 220.0/255, 57.0/255
-                Rectangle:
-                    pos: self.pos
-                    size: self.size
-            text: "rgb: 205.0/255, 220.0/255, 57.0/255"
-
-        Label:
-            canvas.before:
-                Color:
-                    rgba: 205.0/255, 220.0/255, 57.0/255, 0.8
-                Rectangle:
-                    pos: self.pos
-                    size: self.size
-            text: "rgba: 205.0/255, 220.0/255, 57.0/255, 0.8"
-
-    BoxLayout:
-        Label:
-            canvas.before:
-                Color:
-                    hsv: 66.0/360, 0.741, 0.863  # rgb->hsv conversion required
-                Rectangle:
-                    pos: self.pos
-                    size: self.size
-            text: "hsv: 66.0/360, 0.741, 0.863"
-
-        Label:
-            canvas.before:
-                Color:
-                    rgba: hex('#CDDC39')
-                Rectangle:
-                    pos: self.pos
-                    size: self.size
-            text: "rgba: hex('#CDDC39')" ''')
+    Label:
+        canvas.before:
+            Color:
+                rgb: 39/255., 174/255., 96/255.
+            Rectangle:
+                pos: self.pos
+                size: self.size
+        text: "rgb: 39/255., 174/255., 96/255."
+    Label:
+        canvas.before:
+            Color:
+                rgba: 39/255., 174/255., 96/255., 1
+            Rectangle:
+                pos: self.pos
+                size: self.size
+        text: "rgba: 39/255., 174/255., 96/255., 1"
+    Label:
+        canvas.before:
+            Color:
+                hsv: 145/360., 77.6/100, 68.2/100
+            Rectangle:
+                pos: self.pos
+                size: self.size
+        text: "hsv: 145/360., 77.6/100, 68.2/100"
+    Label:
+        canvas.before:
+            Color:
+                rgba: hex('#27ae60')
+            Rectangle:
+                pos: self.pos
+                size: self.size
+        text: "rgba: hex('#27ae60')"
+""")
 
 
-class Root(BoxLayout):
+class Root(GridLayout):
     pass
 
 
 class ColorusageApp(App):
     def build(self):
         return Root()
+
 
 if __name__ == "__main__":
     ColorusageApp().run()
