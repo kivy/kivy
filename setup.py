3f91469e588f1bd29c8b583500a26b77bb2b290f
matham <moiein2000@gmail.com>
2016-12-18 17:35:59 -0500

    Add missing gl link.

--- setup.py
+++ setup.py
@@ -553,7 +553,7 @@ def determine_gl_flags():
         c_options['use_x11'] = True
         c_options['use_egl'] = True
     else:
-        pass
+        flags['libraries'] = ['GL']
     return flags, base_flags
 
 
