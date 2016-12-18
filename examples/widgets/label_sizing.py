a8879b5be9886177a55a2a02204b2da56bc09ba4
Phunsukwangdu <susmit9370@gmail.com>
2016-12-18 13:11:40 +0530

    corrected some more pep8errors

--- examples/widgets/label_sizing.py
+++ examples/widgets/label_sizing.py
@@ -18,9 +18,9 @@ _example_title_text = 'A Tale of Two Cities, by Charles Dickens\n'
 _example_text = """It was the best of times, it was the worst of times,
 it was the age of wisdom, it was the age of foolishness, it was the epoch of
 belief, it was the epoch of incredulity, it was the season of Light, it was
-the season of Darkness, it was the spring of hope, it was the winter of despair,
-we had everything before us, we had nothing before us, we were all going
-direct to Heaven, we were all going direct the other way - in short,
+the season of Darkness, it was the spring of hope, it was the winter of
+despair,we had everything before us, we had nothing before us, we were all
+going direct to Heaven, we were all going direct the other way - in short,
 the period was so far like the present period, that some of its noisiest
 authorities insisted on its being received, for good or for evil, in the
 superlative degree of comparison only.
@@ -37,7 +37,8 @@ BoxLayout:
         text: 'These modify all demonstration Labels'
 
     StackLayout:
-        # Button is a subclass of Label and can be sized to text in the same way
+        # Button is a subclass of Label and can be sized
+        # to text in the same way
 
         Button:
             text: 'Reset'
