f7e114fbbcd4a2a2c4fc0140e04e8fe6fcc73882
yaki29 <yashjain.lnm@gmail.com>
2016-12-30 21:56:26 +0530

    date formate corrected

--- examples/camera/main.py
+++ examples/camera/main.py
@@ -20,34 +20,33 @@ from kivy.uix.boxlayout import BoxLayout
 import time
 Builder.load_string('''
 <CameraClick>:
-    BoxLayout:
-        orientation: 'vertical'
-        Camera:
-            id: camera
-            resolution: (640, 480)
-            play: False
-        ToggleButton:
-            text: 'Play'
-            on_press: camera.play = not camera.play
-            size_hint_y: None
-            height: '48dp'
-        Button:
-            text: 'Capture'
-            size_hint_y: None
-            height: '48dp'
-            on_press: root.click_pic()
+    orientation: 'vertical'
+    Camera:
+        id: camera
+        resolution: (640, 480)
+        play: False
+    ToggleButton:
+        text: 'Play'
+        on_press: camera.play = not camera.play
+        size_hint_y: None
+        height: '48dp'
+    Button:
+        text: 'Capture'
+        size_hint_y: None
+        height: '48dp'
+        on_press: root.Capture()
 '''
 )
 class CameraClick(BoxLayout):
-    def click_pic(self):
+    def Capture(self):
         '''
         Function to capture the images and give them the names 
         according to their captured time and date.
         '''
         camera = self.ids['camera']
-        timestr = time.strftime("%d-%m-%Y_%H:%M:%S")
+        timestr = time.strftime("%Y%m%d_%H%M%S")
         camera.export_to_png("IMG_" + timestr)
-        print "Captured"
+        print ("Captured")
         
 
 class TestCamera(App):
