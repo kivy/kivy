4af511430012dbc04b6c612666ab923f9b163810
phunsukwangdu <susmit9370@gmail.com>
2017-01-08 18:01:15 +0530

    updating code

--- kivy/uix/behaviors/focus.py
+++ kivy/uix/behaviors/focus.py
@@ -1,19 +1,23 @@
 '''
 Focus Behavior
 ==============
+
 The :class:`~kivy.uix.behaviors.FocusBehavior`
 `mixin <https://en.wikipedia.org/wiki/Mixin>`_ class provides
 keyboard focus behavior. When combined with other
 FocusBehavior widgets it allows one to cycle focus among them by pressing
 tab. In addition, upon gaining focus, the instance will automatically
 receive keyboard input.
+
 Focus, very different from selection, is intimately tied with the keyboard;
 each keyboard can focus on zero or one widgets, and each widget can only
 have the focus of one keyboard. However, multiple keyboards can focus
 simultaneously on different widgets. When escape is hit, the widget having
 the focus of that keyboard will de-focus.
+
 Managing focus
 --------------
+
 In essence, focus is implemented as a doubly linked list, where each
 node holds a (weak) reference to the instance before it and after it,
 as visualized when cycling through the nodes using tab (forward) or
@@ -24,41 +28,54 @@ means that the :attr:`~kivy.uix.widget.Widget.children` list and
 walked to find the next focusable widget, unless :attr:`focus_next` or
 :attr:`focus_previous` is set to the `StopIteration` class, in which case
 focus stops there.
+
 For example, to cycle focus between :class:`~kivy.uix.button.Button`
 elements of a :class:`~kivy.uix.gridlayout.GridLayout`::
+
     class FocusButton(FocusBehavior, Button):
       pass
+
     grid = GridLayout(cols=4)
     for i in range(40):
         grid.add_widget(FocusButton(text=str(i)))
     # clicking on a widget will activate focus, and tab can now be used
     # to cycle through
+
 When using a software keyboard, typical on mobile and touch devices, the
 keyboard display behavior is determined by the
 :attr:`~kivy.core.window.WindowBase.softinput_mode` property. You can use
 this property to ensure the focused widget is not covered or obscured by the
 keyboard.
+
 Initializing focus
 ------------------
+
 Widgets needs to be visible before they can receive the focus. This means that
 setting their *focus* property to True before they are visible will have no
 effect. To initialize focus, you can use the 'on_parent' event::
+
     from kivy.app import App
     from kivy.uix.textinput import TextInput
+
     class MyTextInput(TextInput):
         def on_parent(self, widget, parent):
             self.focus = True
+
     class SampleApp(App):
         def build(self):
             return MyTextInput()
+
     SampleApp().run()
+
 If you are using a :class:`~kivy.uix.popup`, you can use the 'on_open' event.
+
 For an overview of behaviors, please refer to the :mod:`~kivy.uix.behaviors`
 documentation.
+
 .. warning::
+
     This code is still experimental, and its API is subject to change in a
     future version.
-.
 '''
 
 __all__ = ('FocusBehavior', )
@@ -82,7 +99,9 @@ class FocusBehavior(object):
     tab. Please see the
     :mod:`focus behavior module documentation <kivy.uix.behaviors.focus>`
     for more information.
+
     .. versionadded:: 1.9.0
+
     '''
 
     _requested_keyboard = False
@@ -94,11 +113,15 @@ class FocusBehavior(object):
     every touch that is not in :attr:`ignored_touch` will defocus all the
     focused widgets if the config keyboard mode is not multi. Touches on
     focusable widgets that were used to focus are automatically added here.
+
     Example usage::
+
         class Unfocusable(Widget):
+
             def on_touch_down(self, touch):
                 if self.collide_point(*touch.pos):
                     FocusBehavior.ignored_touch.append(touch)
+
     Notice that you need to access this as a class, not an instance variable.
     '''
 
@@ -120,11 +143,13 @@ class FocusBehavior(object):
     keyboard = AliasProperty(_get_keyboard, _set_keyboard,
                              bind=('_keyboard', ))
     '''The keyboard to bind to (or bound to the widget) when focused.
+
     When None, a keyboard is requested and released whenever the widget comes
     into and out of focus. If not None, it must be a keyboard, which gets
     bound and unbound from the widget whenever it's in or out of focus. It is
     useful only when more than one keyboard is available, so it is recommended
     to be set to None when only one keyboard is available.
+
     If more than one keyboard is available, whenever an instance gets focused
     a new keyboard will be requested if None. Unless the other instances lose
     focus (e.g. if tab was used), a new keyboard will appear. When this is
@@ -134,30 +159,39 @@ class FocusBehavior(object):
     each group, only one FocusBehavior will have focus, and will receive input
     from the correct keyboard. See `keyboard_mode` in :mod:`~kivy.config` for
     more information on the keyboard modes.
+
     **Keyboard and focus behavior**
+
     When using the keyboard, there are some important default behaviors you
     should keep in mind.
+
     * When Config's `keyboard_mode` is multi, each new touch is considered
       a touch by a different user and will set the focus (if clicked on a
       focusable) with a new keyboard. Already focused elements will not lose
       their focus (even if an unfocusable widget is touched).
+
     * If the keyboard property is set, that keyboard will be used when the
       instance gets focused. If widgets with different keyboards are linked
       through :attr:`focus_next` and :attr:`focus_previous`, then as they are
       tabbed through, different keyboards will become active. Therefore,
       typically it's undesirable to link instances which are assigned
       different keyboards.
+
     * When a widget has focus, setting its keyboard to None will remove its
       keyboard, but the widget will then immediately try to get
       another keyboard. In order to remove its keyboard, rather set its
       :attr:`focus` to False.
+
     * When using a software keyboard, typical on mobile and touch devices, the
       keyboard display behavior is determined by the
       :attr:`~kivy.core.window.WindowBase.softinput_mode` property. You can use
       this property to ensure the focused widget is not covered or obscured.
+
     :attr:`keyboard` is an :class:`~kivy.properties.AliasProperty` and defaults
     to None.
+
     .. warning:
+
         When assigning a keyboard, the keyboard must not be released while
         it is still assigned to an instance. Similarly, the keyboard created
         by the instance on focus and assigned to :attr:`keyboard` if None,
@@ -169,6 +203,7 @@ class FocusBehavior(object):
     is_focusable = BooleanProperty(_is_desktop)
     '''Whether the instance can become focused. If focused, it'll lose focus
     when set to False.
+
     :attr:`is_focusable` is a :class:`~kivy.properties.BooleanProperty` and
     defaults to True on a desktop (i.e. `desktop` is True in
     :mod:`~kivy.config`), False otherwise.
@@ -176,22 +211,27 @@ class FocusBehavior(object):
 
     focus = BooleanProperty(False)
     '''Whether the instance currently has focus.
+
     Setting it to True will bind to and/or request the keyboard, and input
     will be forwarded to the instance. Setting it to False will unbind
     and/or release the keyboard. For a given keyboard, only one widget can
     have its focus, so focusing one will automatically unfocus the other
     instance holding its focus.
+
     When using a software keyboard, please refer to the
     :attr:`~kivy.core.window.WindowBase.softinput_mode` property to determine
     how the keyboard display is handled.
+
     :attr:`focus` is a :class:`~kivy.properties.BooleanProperty` and defaults
     to False.
     '''
 
     focused = focus
     '''An alias of :attr:`focus`.
+
     :attr:`focused` is a :class:`~kivy.properties.BooleanProperty` and defaults
     to False.
+
     .. warning::
         :attr:`focused` is an alias of :attr:`focus` and will be removed in
         2.0.0.
@@ -221,12 +261,15 @@ class FocusBehavior(object):
     '''The :class:`FocusBehavior` instance to acquire focus when
     tab is pressed and this instance has focus, if not `None` or
     `StopIteration`.
+
     When tab is pressed, focus cycles through all the :class:`FocusBehavior`
     widgets that are linked through :attr:`focus_next` and are focusable. If
     :attr:`focus_next` is `None`, it instead walks the children lists to find
     the next focusable widget. Finally, if :attr:`focus_next` is
     the `StopIteration` class, focus won't move forward, but end here.
+
     .. note:
+
         Setting :attr:`focus_next` automatically sets :attr:`focus_previous`
         of the other instance to point to this instance, if not None or
         `StopIteration`. Similarly, if it wasn't None or `StopIteration`, it
@@ -235,6 +278,7 @@ class FocusBehavior(object):
         required to set one of the :attr:`focus_previous` or
         :attr:`focus_next` links since the other side will be set
         automatically.
+
     :attr:`focus_next` is an :class:`~kivy.properties.ObjectProperty` and
     defaults to `None`.
     '''
@@ -257,13 +301,16 @@ class FocusBehavior(object):
     focus_previous = ObjectProperty(None, allownone=True)
     '''The :class:`FocusBehavior` instance to acquire focus when
     shift+tab is pressed on this instance, if not None or `StopIteration`.
+
     When shift+tab is pressed, focus cycles through all the
     :class:`FocusBehavior` widgets that are linked through
     :attr:`focus_previous` and are focusable. If :attr:`focus_previous` is
     `None`, it instead walks the children tree to find the
     previous focusable widget. Finally, if :attr:`focus_previous` is the
     `StopIteration` class, focus won't move backward, but end here.
+
     .. note:
+
         Setting :attr:`focus_previous` automatically sets :attr:`focus_next`
         of the other instance to point to this instance, if not None or
         `StopIteration`. Similarly, if it wasn't None or `StopIteration`, it
@@ -271,6 +318,7 @@ class FocusBehavior(object):
         :attr:`focus_previous` to `None`. Therefore, it is only required
         to set one of the :attr:`focus_previous` or :attr:`focus_next`
         links since the other side will be set automatically.
+
     :attr:`focus_previous` is an :class:`~kivy.properties.ObjectProperty` and
     defaults to `None`.
     '''
@@ -280,6 +328,7 @@ class FocusBehavior(object):
     result in the standard behaviour of showing/hiding on focus. 'managed'
     requires setting the keyboard visibility manually, or calling the helper
     functions :meth:`show_keyboard` and :meth:`hide_keyboard`.
+
     :attr:`keyboard_mode` is an :class:`~kivy.properties.OptionsProperty` and
     defaults to 'auto'. Can be one of 'auto' or 'managed'.
     '''
@@ -288,7 +337,9 @@ class FocusBehavior(object):
                                                  'mail', 'datetime', 'tel',
                                                  'address'))
     '''The kind of input keyboard to request.
+
     .. versionadded:: 1.8.0
+
     :attr:`input_type` is an :class:`~kivy.properties.OptionsProperty` and
     defaults to 'text'. Can be one of 'text', 'number', 'url', 'mail',
     'datetime', 'tel' or 'address'.
@@ -297,12 +348,14 @@ class FocusBehavior(object):
     unfocus_on_touch = BooleanProperty(_keyboard_mode not in
                                        ('multi', 'systemandmulti'))
     '''Whether a instance should lose focus when clicked outside the instance.
+
     When a user clicks on a widget that is focus aware and shares the same
     keyboard as this widget (which in the case of only one keyboard, are
     all focus aware widgets), then as the other widgets gains focus, this
     widget loses focus. In addition to that, if this property is `True`,
     clicking on any widget other than this widget, will remove focus form this
     widget.
+
     :attr:`unfocus_on_touch` is a :class:`~kivy.properties.BooleanProperty` and
     defaults to `False` if the `keyboard_mode` in :attr:`~kivy.config.Config`
     is `'multi'` or `'systemandmulti'`, otherwise it defaults to `True`.
@@ -452,13 +505,16 @@ class FocusBehavior(object):
 
     def keyboard_on_key_down(self, window, keycode, text, modifiers):
         '''The method bound to the keyboard when the instance has focus.
+
         When the instance becomes focused, this method is bound to the
         keyboard and will be called for every input press. The parameters are
         the same as :meth:`kivy.core.window.WindowBase.on_key_down`.
+
         When overwriting the method in the derived widget, super should be
         called to enable tab cycling. If the derived widget wishes to use tab
         for its own purposes, it can call super after it has processed the
         character (if it does not wish to consume the tab).
+
         Similar to other keyboard functions, it should return True if the
         key was consumed.
         '''
@@ -477,13 +533,16 @@ class FocusBehavior(object):
 
     def keyboard_on_key_up(self, window, keycode):
         '''The method bound to the keyboard when the instance has focus.
+
         When the instance becomes focused, this method is bound to the
         keyboard and will be called for every input release. The parameters are
         the same as :meth:`kivy.core.window.WindowBase.on_key_up`.
+
         When overwriting the method in the derived widget, super should be
         called to enable de-focusing on escape. If the derived widget wishes
         to use escape for its own purposes, it can call super after it has
         processed the character (if it does not wish to consume the escape).
+
         See :meth:`keyboard_on_key_down`
         '''
         if keycode[1] == 'escape':
