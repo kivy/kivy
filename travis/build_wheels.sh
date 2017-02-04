9a48057788248ae7bbf892ce40b4389d857fe20b
Peter Badida <KeyWeeUsr@users.noreply.github.com>
2017-02-04 20:06:57 +0100

    Yum first, RPM later

--- travis/build_wheels.sh
+++ travis/build_wheels.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
 
 # Install a system package required by our library
+yum check-update
+yum install \
+    make \
+    mercurial \
+    automake \
+    gcc \
+    gcc-c++ \
+    khrplatform-devel \
+    mesa-libGLES \
+    mesa-libGLES-devel \
+    gstreamer-plugins-good \
+    gstreamer \
+    gstreamer-python \
+    mtdev-devel \
+    python-devel \
+    python-pip
+
 wget ftp://mirror.switch.ch/pool/4/mirror/scientificlinux/7.0/x86_64/updates/fastbugs/mesa-libGLES-devel-9.2.5-6.20131218.el7_0.i686.rpm
 wget ftp://mirror.switch.ch/pool/4/mirror/scientificlinux/7.0/x86_64/updates/fastbugs/mesa-libGLES-devel-9.2.5-6.20131218.el7_0.x86_64.rpm
 
@@ -42,23 +59,6 @@ rpm -ivh SDL2_mixer-devel-2.0.1-2.fc24.x86_64.rpm
 
 rpm -ivh SDL2_ttf-devel-2.0.14-2.fc25.i686.rpm
 rpm -ivh SDL2_ttf-devel-2.0.14-2.fc25.x86_64.rpm
-
-yum check-update
-yum install \
-    make \
-    mercurial \
-    automake \
-    gcc \
-    gcc-c++ \
-    khrplatform-devel \
-    mesa-libGLES \
-    mesa-libGLES-devel \
-    gstreamer-plugins-good \
-    gstreamer \
-    gstreamer-python \
-    mtdev-devel \
-    python-devel \
-    python-pip
 
 # Compile wheels
 for PYBIN in /opt/python/*/bin; do
