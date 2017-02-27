fbc75199f78d4c21e12d815470a7c59236e66e40
Manthan Sharma <hitechmanthan@gmail.com>
2017-02-28 03:03:28 +0530

    Update Twisted framework integration example to py3
    - With maintaining support to Py2

--- examples/frameworks/twisted/echo_server_app.py
+++ examples/frameworks/twisted/echo_server_app.py
@@ -1,21 +1,21 @@
-# install_twisted_rector must be called before importing  and using the reactor
+# install_twisted_rector must be called before importing and using the reactor
 from kivy.support import install_twisted_reactor
-install_twisted_reactor()
 
+install_twisted_reactor()
 
 from twisted.internet import reactor
 from twisted.internet import protocol
 
 
-class EchoProtocol(protocol.Protocol):
+class EchoServer(protocol.Protocol):
     def dataReceived(self, data):
         response = self.factory.app.handle_message(data)
         if response:
             self.transport.write(response)
 
 
-class EchoFactory(protocol.Factory):
-    protocol = EchoProtocol
+class EchoServerFactory(protocol.Factory):
+    protocol = EchoServer
 
     def __init__(self, app):
         self.app = app
@@ -26,20 +26,23 @@ from kivy.uix.label import Label
 
 
 class TwistedServerApp(App):
+    label = None
+
     def build(self):
         self.label = Label(text="server started\n")
-        reactor.listenTCP(8000, EchoFactory(self))
+        reactor.listenTCP(8000, EchoServerFactory(self))
         return self.label
 
     def handle_message(self, msg):
-        self.label.text = "received:  %s\n" % msg
+        msg = msg.decode('utf-8')
+        self.label.text = "received:  {}\n".format(msg)
 
         if msg == "ping":
-            msg = "pong"
+            msg = "Pong"
         if msg == "plop":
-            msg = "kivy rocks"
-        self.label.text += "responded: %s\n" % msg
-        return msg
+            msg = "Kivy Rocks!!!"
+        self.label.text += "responded: {}\n".format(msg)
+        return msg.encode('utf-8')
 
 
 if __name__ == '__main__':
