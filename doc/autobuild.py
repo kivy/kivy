24fb5e210dd76696c31877803023d6ab354d60d3
Peter Badida <KeyWeeUsr@users.noreply.github.com>
2017-03-01 22:51:59 +0100

    Fix comma typo

--- doc/autobuild.py
+++ doc/autobuild.py
@@ -14,8 +14,8 @@ ignore_list = (
     'kivy.graphics.vbo',
     'kivy.graphics.vertex',
     'kivy.uix.recycleview.__init__',
-    'kivy.setupconfig'
-    'kivy.version',
+    'kivy.setupconfig',
+    'kivy.version'
 )
 
 import os
