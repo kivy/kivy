ef1aa72786443ddbbd3368822a3e6bb9d51cb174
Bakterija <fixmix@inbox.lv>
2017-04-04 04:38:31 +0300

    Revert "Fix Recycleview.scroll_to method crash"
    This reverts commit 67cc4a157c9745693d5f680a474259934710c3f7.

--- kivy/uix/scrollview.py
+++ kivy/uix/scrollview.py
@@ -898,11 +898,10 @@ class ScrollView(StencilView):
 
         # if _viewport is layout and has pending operation, reschedule
         if hasattr(self._viewport, 'do_layout'):
-            if hasattr(self._viewport._trigger_layout, 'is_triggered'):
-                if self._viewport._trigger_layout.is_triggered:
-                    Clock.schedule_once(
-                         lambda *dt: self.scroll_to(widget, padding, animate))
-                    return
+            if self._viewport._trigger_layout.is_triggered:
+                Clock.schedule_once(
+                     lambda *dt: self.scroll_to(widget, padding, animate))
+                return
 
         if isinstance(padding, (int, float)):
             padding = (padding, padding)
