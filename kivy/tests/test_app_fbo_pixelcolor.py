0a516d6eee1a01811f43e2cd9ba042626be022ee
Peter Badida <KeyWeeUsr@users.noreply.github.com>
2017-04-10 09:17:09 +0200

    Add test for Fbo.get_pixel_color

---
+++ kivy/tests/test_app_fbo_pixelcolor.py
@@ -0,0 +1,92 @@
+import unittest
+
+from kivy.app import App
+from kivy.clock import Clock
+from kivy.uix.widget import Widget
+from kivy.core.window import Window
+from kivy.graphics import Fbo, Color, Rectangle
+
+
+class FboTest(Widget):
+    def __init__(self, **kwargs):
+        super(FboTest, self).__init__(**kwargs)
+        self.positions = (
+            (260.0, 260.0),
+            (192.0, 192.0),
+            (96.0, 192.0),
+            (192.0, 96.0),
+            (96.0, 96.0),
+            (32.0, 192.0),
+            (192.0, 32.0),
+            (32.0, 32.0)
+        )
+
+        with self.canvas:
+            self.fbo = Fbo(size=(256, 256))
+
+        # draw on canvas too to make visible ^
+        with self.fbo:
+            Color(0.56789, 0, 0, 1)
+            Rectangle(size=(256, 64))
+            Color(0, 0.56789, 0, 1)
+            Rectangle(size=(64, 256))
+            Color(0.56789, 0, 0, .5)
+            Rectangle(pos=(64, 64), size=(192, 64))
+            Color(0, 0.56789, 0, .5)
+            Rectangle(pos=(64, 64), size=(64, 192))
+
+
+class Test(unittest.TestCase):
+    def on_window_flip(self, win):
+        # get_pixel_color doesn't work until
+        # window is flipped (returns zero tuple)
+        app = self.app
+        fbow = self.fbow
+
+        values = (
+            # out of bounds of FBO
+            (tuple, int, (0, 0, 0, 0)),
+            # in FBO, black
+            (list, int, [0, 0, 0, 0]),
+            # Color(0, 0.56789, 0, .5)
+            (list, int, [0, 72, 0, 128]),
+            # Color(0.56789, 0, 0, .5)
+            (list, int, [72, 0, 0, 128]),
+            # overlap above 2 w/ alpha
+            (list, int, [36, 72, 0, 255]),
+            # Color(0, 0.56789, 0, 1)
+            (list, int, [0, 145, 0, 255]),
+            # Color(0.56789, 0, 0, 1)
+            (list, int, [145, 0, 0, 255]),
+            # overlap above 2 w/o alpha
+            (list, int, [0, 145, 0, 255]),
+        )
+
+        for i, pos in enumerate(fbow.positions):
+            c = fbow.fbo.get_pixel_color(pos[0], pos[1])
+            # returned class
+            self.assertTrue(isinstance(c, values[i][0]))
+            # returned types in container
+            for v in c:
+                self.assertTrue(isinstance(v, values[i][1]))
+            # returned values
+            self.assertEqual(c, values[i][2])
+
+        # kill app
+        win.unbind(on_flip=self.on_window_flip)
+        app.stop()
+
+    def set_env(self, app, *args):
+        # set up app environment
+        self.fbow = FboTest(size=Window.size)
+        app.root.add_widget(self.fbow)
+
+    def test_app_fbo_getpixelcolor(self):
+        self.app = App()
+        Window.bind(on_flip=self.on_window_flip)
+        Clock.schedule_once(lambda *t: self.set_env(self.app), 0)
+        self.app.run()
+
+
+if __name__ == '__main__':
+    unittest.main()
