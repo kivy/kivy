7fd05aaa60b4afb683f594e454d85c42bf5fbc5c
Peter Badida <KeyWeeUsr@users.noreply.github.com>
2017-05-04 02:09:38 +0200

    Fix KV include for quoted paths (#5153)
    * Fix KV include for quoted paths
    
    * Better IF for quotes
    
    * Strip only two quotes around
    
    * Strip only equal amount of quotes
    
    * Nice way to ignore two quotes

--- kivy/lang/parser.py
+++ kivy/lang/parser.py
@@ -422,6 +422,11 @@ class Parser(object):
                     ref = ref[6:].strip()
                     force_load = True
 
+                # if #:include [force] "path with quotes around"
+                if ref[0] == ref[-1] and ref[0] in ('"', "'"):
+                    c = ref[:3].count(ref[0])
+                    ref = ref[c:-c] if c != 2 else ref
+
                 if ref[-3:] != '.kv':
                     Logger.warn('Lang: {0} does not have a valid Kivy'
                                 'Language extension (.kv)'.format(ref))
