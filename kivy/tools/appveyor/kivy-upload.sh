9d332928d477698ba2b5981db9c62b65d632962a
Peter Badida <KeyWeeUsr@users.noreply.github.com>
2017-05-13 20:44:06 +0200

    Fix path

--- kivy/tools/appveyor/kivy-upload.sh
+++ kivy/tools/appveyor/kivy-upload.sh
@@ -1,3 +1,3 @@
 pacman -S --noconfirm git rsync
-$p=cygpath -u "$WHEEL_DIR"
+$p=$(cygpath -u "$WHEEL_DIR")
 rsync -azvhen "ssh -p 2458 StrictHostKeyChecking=no" "$p" root@159.203.106.198:/web/downloads/appveyor/kivy
