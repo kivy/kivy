01fa4ac53f272addf8d9d2cdab72ae4b15f52489
Peter Badida <KeyWeeUsr@users.noreply.github.com>
2017-05-13 21:08:11 +0200

    Bad quotes

--- kivy/tools/appveyor/kivy-upload.sh
+++ kivy/tools/appveyor/kivy-upload.sh
@@ -3,4 +3,4 @@ ls /c
 ls /c/kivy_wheels
 $p=$(cygpath -u "$WHEEL_DIR")
 ls $p
-rsync -azvh -n -e "ssh -p 2458 StrictHostKeyChecking=no" "$(cygpath -u "$WHEEL_DIR")" root@159.203.106.198:/web/downloads/appveyor/kivy
+rsync -azvh -n -e "ssh -p 2458 StrictHostKeyChecking=no" "$(cygpath -u '$WHEEL_DIR')" root@159.203.106.198:/web/downloads/appveyor/kivy
