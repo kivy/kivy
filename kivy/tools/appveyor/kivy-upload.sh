2b7d36d687fccd5ece4a62af7c100302fbf1d557
Peter Badida <KeyWeeUsr@users.noreply.github.com>
2017-05-13 21:05:36 +0200

    Try list folders

--- kivy/tools/appveyor/kivy-upload.sh
+++ kivy/tools/appveyor/kivy-upload.sh
@@ -1,3 +1,6 @@
 pacman -S --noconfirm git rsync
+ls /c
+ls /c/kivy_wheels
 $p=$(cygpath -u "$WHEEL_DIR")
-rsync -azvh -n -e "ssh -p 2458 StrictHostKeyChecking=no" "$p" root@159.203.106.198:/web/downloads/appveyor/kivy
+ls $p
+rsync -azvh -n -e "ssh -p 2458 StrictHostKeyChecking=no" "$(cygpath -u "$WHEEL_DIR")" root@159.203.106.198:/web/downloads/appveyor/kivy
