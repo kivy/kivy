b34b9e11c9bd8d902ee779a63b380ce7983aeff1
Peter Badida <KeyWeeUsr@users.noreply.github.com>
2017-05-13 20:52:20 +0200

    Split options

--- kivy/tools/appveyor/kivy-upload.sh
+++ kivy/tools/appveyor/kivy-upload.sh
@@ -1,3 +1,3 @@
 pacman -S --noconfirm git rsync
 $p=$(cygpath -u "$WHEEL_DIR")
-rsync -azvhen "ssh -p 2458 StrictHostKeyChecking=no" "$p" root@159.203.106.198:/web/downloads/appveyor/kivy
+rsync -azvh -n -e "ssh -p 2458 StrictHostKeyChecking=no" "$p" root@159.203.106.198:/web/downloads/appveyor/kivy
