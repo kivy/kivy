3b8a5d1795b86106b00cd8dff7d38b6e273089d1
Peter Badida <KeyWeeUsr@users.noreply.github.com>
2017-05-13 20:34:21 +0200

    Convert path

--- kivy/tools/appveyor/kivy-upload.sh
+++ kivy/tools/appveyor/kivy-upload.sh
@@ -1,2 +1,3 @@
 pacman -S --noconfirm git rsync
-rsync -azvhen "ssh -p 2458 StrictHostKeyChecking=no" "$WHEEL_DIR/" root@159.203.106.198:/web/downloads/appveyor/kivy
+$p=cygpath -u "$WHEEL_DIR"
+rsync -azvhen "ssh -p 2458 StrictHostKeyChecking=no" "$p" root@159.203.106.198:/web/downloads/appveyor/kivy
