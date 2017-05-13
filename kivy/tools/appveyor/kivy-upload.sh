0303e806dde73a0fb23663cef8743a11c62e6ce9
matham <moiein2000@gmail.com>
2017-05-13 16:32:43 -0400

    Update kivy-upload.sh

--- kivy/tools/appveyor/kivy-upload.sh
+++ kivy/tools/appveyor/kivy-upload.sh
@@ -3,5 +3,5 @@ if [ ! -d "/home/appveyor/.ssh" ]; then
   mkdir "/home/appveyor/.ssh"
 fi
 echo -e "Host 159.203.106.198\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
-cp $(cygpath -u "C:\projects\kivy\kivy\tools\appveyor\id_rsa.enc") ~/.ssh/id_rsa.enc
+cp $(cygpath -u "C:\projects\kivy\kivy\tools\appveyor\id_rsa") ~/.ssh/id_rsa
 rsync -avh -n -e "ssh -p 2458" "/c/kivy_wheels/" root@159.203.106.198:/web/downloads/appveyor/kivy
