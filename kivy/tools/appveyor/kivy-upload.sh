3a90c5e84d747c50b019007196a48dfd46c10d4f
Peter Badida <KeyWeeUsr@users.noreply.github.com>
2017-05-13 22:20:22 +0200

    Abs path

--- kivy/tools/appveyor/kivy-upload.sh
+++ kivy/tools/appveyor/kivy-upload.sh
@@ -1,6 +1,6 @@
 pacman -S --noconfirm git rsync
-if [ ! -d "~/.ssh" ]; then
-  mkdir "~/.ssh"
+if [ ! -d "/home/appveyor/.ssh" ]; then
+  mkdir "/home/appveyor/.ssh"
 fi
 echo -e "Host 159.203.106.198\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
 cp $(cygpath -u "C:\projects\kivy\kivy\tools\appveyor\id_rsa.enc") ~/.ssh/id_rsa.enc
