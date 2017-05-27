2bd664d274552b7cc4d18b67314e251361e9d5d3
Peter Badida <KeyWeeUsr@users.noreply.github.com>
2017-05-27 23:05:59 +0200

    Add proper image sizing (RST)
    ref #4390

--- kivy/uix/rst.py
+++ kivy/uix/rst.py
@@ -975,17 +975,38 @@ class _Visitor(nodes.NodeVisitor):
             assert(self.text == '')
 
         elif cls is nodes.image:
+            # docutils parser breaks path with spaces
+            # e.g. "C:/my path" -> "C:/mypath"
             uri = node['uri']
+            align = node.get('align', 'center')
+            image_size=[
+                node.get('width'),
+                node.get('height')
+            ]
+
+            # use user's size if defined
+            def set_size(img, size):
+                img.size = [
+                    size[0] or img.width,
+                    size[1] or img.height
+                ]
+
             if uri.startswith('/') and self.root.document_root:
                 uri = join(self.root.document_root, uri[1:])
+
             if uri.startswith('http://') or uri.startswith('https://'):
                 image = RstAsyncImage(source=uri)
+                image.bind(on_load=lambda *a: set_size(image, image_size))
             else:
                 image = RstImage(source=uri)
+                set_size(image, image_size)
 
-            align = node.get('align', 'center')
-            root = AnchorLayout(size_hint_y=None, anchor_x=align,
-                                height=image.height)
+            root = AnchorLayout(
+                size_hint_y=None,
+                anchor_x=align,
+                height=image.height
+            )
+
             image.bind(height=root.setter('height'))
             root.add_widget(image)
             self.current.add_widget(root)
