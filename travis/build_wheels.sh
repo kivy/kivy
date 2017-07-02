fb914d535269c4d79f56ee4b48a456831513ddab
Peter Badida <KeyWeeUsr@users.noreply.github.com>
2017-07-02 22:30:06 +0200

    Switch to matham's solution

--- travis/build_wheels.sh
+++ travis/build_wheels.sh
@@ -66,64 +66,179 @@ yum install -y \
 # ibus-1.0
 # fcitx-libs
 
-# Make SDL2 packages
-SDL="SDL2-2.0.5"
-TTF="SDL_ttf-2.0.14"
-MIX="SDL_mixer-2.0.1"
-IMG="SDL_image-2.0.1"
-curl -sL https://www.libsdl.org/release/${SDL}.tar.gz > ${SDL}.tar.gz
-curl -sL https://www.libsdl.org/projects/SDL_image/release/${IMG}.tar.gz > ${IMG}.tar.gz
-curl -sL https://www.libsdl.org/projects/SDL_ttf/release/${TTF}.tar.gz > ${TTF}.tar.gz
-curl -sL https://www.libsdl.org/projects/SDL_mixer/release/${MIX}.tar.gz > ${MIX}.tar.gz
+# ---------------------------------------------------------------
 
-# SDL2
-tar xzf ${SDL}.tar.gz
-cd $SDL
-./configure
-# --enable-png --disable-png-shared --enable-jpg --disable-jpg-shared
+yum -y install libass libass-devel autoconf automake bzip2 cmake freetype-devel gcc gcc-c++ git libtool make mercurial pkgconfig zlib-devel enca-devel fontconfig-devel openssl openssl-devel
+mkdir ~/ffmpeg_sources;
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$HOME/ffmpeg_build/lib;
+
+cd ~/ffmpeg_sources;
+git clone --depth 1 https://github.com/spurious/SDL-mirror.git
+cd SDL-mirror;
+./configure --prefix="$HOME/ffmpeg_build" --bindir="$HOME/ffmpeg_build/bin";
+make;
+make install;
+make distclean;
+
+cd ~/ffmpeg_sources;
+wget http://www.libsdl.org/projects/SDL_mixer/release/SDL2_mixer-2.0.1.tar.gz;
+tar xzf SDL2_mixer-2.0.1.tar.gz;
+cd SDL2_mixer-2.0.1;
+PATH="$HOME/ffmpeg_build/bin:$PATH" PKG_CONFIG_PATH="$HOME/ffmpeg_build/lib/pkgconfig" ./configure --prefix="$HOME/ffmpeg_build" --bindir="$HOME/ffmpeg_build/bin";
+PATH="$HOME/ffmpeg_build/bin:$PATH" make;
+make install;
+make distclean;
+
+cd ~/ffmpeg_sources;
+wget https://www.openssl.org/source/openssl-1.0.2l.tar.gz;
+tar xzf openssl-1.0.2l.tar.gz;
+cd openssl-1.0.2l;
+./config -fpic shared --prefix="$HOME/ffmpeg_build";
+make;
+make install;
+
+cd ~/ffmpeg_sources;
+wget http://www.tortall.net/projects/yasm/releases/yasm-1.3.0.tar.gz;
+tar xzf yasm-1.3.0.tar.gz;
+cd yasm-1.3.0;
+./configure --prefix="$HOME/ffmpeg_build" --bindir="$HOME/ffmpeg_build/bin";
+make;
+make install;
+make distclean;
+
+cd ~/ffmpeg_sources;
+wget http://www.nasm.us/pub/nasm/releasebuilds/2.13.01/nasm-2.13.01.tar.gz;
+tar -xvzf nasm-2.13.01.tar.gz;
+cd nasm-2.13.01;
+./configure --prefix="$HOME/ffmpeg_build" --bindir="$HOME/ffmpeg_build/bin";
+make;
+make install;
+make distclean;
+
+cd ~/ffmpeg_sources;
+wget http://download.videolan.org/pub/x264/snapshots/last_x264.tar.bz2;
+tar xjf last_x264.tar.bz2;
+cd x264-snapshot*;
+PATH="$HOME/ffmpeg_build/bin:$PATH" ./configure --prefix="$HOME/ffmpeg_build" --bindir="$HOME/ffmpeg_build/bin" --enable-shared --extra-cflags="-fPIC";
+PATH="$HOME/ffmpeg_build/bin:$PATH" make;
+make install;
+make distclean;
+
+cd ~/ffmpeg_sources;
+wget http://downloads.sourceforge.net/project/lame/lame/3.99/lame-3.99.5.tar.gz;
+tar xzf lame-3.99.5.tar.gz;
+cd lame-3.99.5;
+./configure --prefix="$HOME/ffmpeg_build" --enable-nasm --enable-shared;
+make;
+make install;
+make distclean;
+
+cd ~/ffmpeg_sources
+wget --no-check-certificate http://fribidi.org/download/fribidi-0.19.7.tar.bz2
+tar xjf fribidi-0.19.7.tar.bz2
+cd fribidi-0.19.7
+./configure --prefix="$HOME/ffmpeg_build" --enable-shared;
 make
 make install
-export KIVY_SDL2_PATH=$PWD
-cd ..
 
-# SDL image
-tar xzf ${IMG}.tar.gz
-cd $IMG
-./configure
-# --enable-png --disable-png-shared --enable-jpg --disable-jpg-shared
+cd ~/ffmpeg_sources
+curl -sLO https://github.com/libass/libass/releases/download/0.13.7/libass-0.13.7.tar.gz
+tar xzf libass-0.13.7.tar.gz
+cd libass-0.13.7
+PATH="$HOME/ffmpeg_build/bin:$PATH" PKG_CONFIG_PATH="$HOME/ffmpeg_build/lib/pkgconfig" ./configure --prefix="$HOME/ffmpeg_build" --enable-shared --disable-require-system-font-provider;
+PATH="$HOME/ffmpeg_build/bin:$PATH" make
+make install
+
+cd ~/ffmpeg_sources
+wget --no-check-certificate http://www.cmake.org/files/v2.8/cmake-2.8.10.2.tar.gz
+tar xzf cmake-2.8.10.2.tar.gz
+cd cmake-2.8.10.2
+./configure --prefix=/usr/local/cmake-2.8.10.2
+gmake
 make
 make install
-export KIVY_SDL2_PATH=$KIVY_SDL2_PATH:$PWD
-cd ..
 
-# SDL ttf
-tar xzf ${TTF}.tar.gz
-cd $TTF
-./configure
+cd ~/ffmpeg_sources
+wget https://bitbucket.org/multicoreware/x265/get/default.tar.gz
+tar xzf default.tar.gz
+cd multicoreware-x265-*/build/linux
+PATH="/usr/local/cmake-2.8.10.2/bin:$HOME/ffmpeg_build/bin:$PATH" PKG_CONFIG_PATH="$HOME/ffmpeg_build/lib/pkgconfig" cmake -G "Unix Makefiles" -DCMAKE_INSTALL_PREFIX="$HOME/ffmpeg_build" -DENABLE_SHARED:bool=on ../../source
 make
 make install
-export KIVY_SDL2_PATH=$KIVY_SDL2_PATH:$PWD
-cd ..
 
-# SDL mixer
-tar xzf ${MIX}.tar.gz
-cd $MIX
-./configure --enable-music-mod --disable-music-mod-shared \
-            --enable-music-ogg  --disable-music-ogg-shared \
-            --enable-music-flac  --disable-music-flac-shared \
-            --enable-music-mp3  --disable-music-mp3-shared
+cd ~/ffmpeg_sources
+git clone https://github.com/mstorsjo/fdk-aac
+cd fdk-aac
+git apply /io/.travis/fdk.patch
+for file in libtool ltdl
+do
+  ln -s /usr/share/aclocal/$file.m4 /usr/local/share/aclocal/$file.m4
+done
+autoreconf -fiv
+./configure --prefix="$HOME/ffmpeg_build" --enable-shared
 make
 make install
-cd ..
+
+cd ~/ffmpeg_sources
+curl -O https://archive.mozilla.org/pub/opus/opus-1.1.5.tar.gz
+tar xzvf opus-1.1.5.tar.gz
+cd opus-1.1.5
+./configure --prefix="$HOME/ffmpeg_build" --enable-shared
+make
+make install
+
+cd ~/ffmpeg_sources
+curl -O http://downloads.xiph.org/releases/ogg/libogg-1.3.2.tar.gz
+tar xzvf libogg-1.3.2.tar.gz
+cd libogg-1.3.2
+./configure --prefix="$HOME/ffmpeg_build" --enable-shared
+make
+make install
+
+cd ~/ffmpeg_sources;
+wget http://downloads.xiph.org/releases/theora/libtheora-1.1.1.tar.gz
+tar xzvf libtheora-1.1.1.tar.gz
+cd libtheora-1.1.1
+PATH="$HOME/ffmpeg_build/bin:$PATH" PKG_CONFIG_PATH="$HOME/ffmpeg_build/lib/pkgconfig" ./configure --prefix="$HOME/ffmpeg_build" --enable-shared;
+PATH="$HOME/ffmpeg_build/bin:$PATH" make;
+make install
+
+cd ~/ffmpeg_sources
+curl -O http://downloads.xiph.org/releases/vorbis/libvorbis-1.3.4.tar.gz
+tar xzvf libvorbis-1.3.4.tar.gz
+cd libvorbis-1.3.4
+PATH="$HOME/ffmpeg_build/bin:$PATH" PKG_CONFIG_PATH="$HOME/ffmpeg_build/lib/pkgconfig" ./configure --prefix="$HOME/ffmpeg_build" --with-ogg="$HOME/ffmpeg_build" --enable-shared
+PATH="$HOME/ffmpeg_build/bin:$PATH" make
+make install
+
+cd ~/ffmpeg_sources
+git clone --depth 1 https://chromium.googlesource.com/webm/libvpx.git
+cd libvpx
+PATH="$HOME/ffmpeg_build/bin:$PATH" ./configure --prefix="$HOME/ffmpeg_build" --disable-examples  --as=yasm --enable-shared --disable-unit-tests
+PATH="$HOME/ffmpeg_build/bin:$PATH" make
+make install
+
+cd ~/ffmpeg_sources;
+wget http://ffmpeg.org/releases/ffmpeg-snapshot.tar.bz2;
+tar xjf ffmpeg-snapshot.tar.bz2;
+cd ffmpeg;
+PATH="$HOME/ffmpeg_build/bin:$PATH" PKG_CONFIG_PATH="$HOME/ffmpeg_build/lib/pkgconfig:/usr/lib/pkgconfig/" ./configure --prefix="$HOME/ffmpeg_build" --extra-cflags="-I$HOME/ffmpeg_build/include -fPIC" --extra-ldflags="-L$HOME/ffmpeg_build/lib" --bindir="$HOME/ffmpeg_build/bin" --enable-gpl --enable-libmp3lame --enable-libx264 --enable-libx265 --enable-libfdk_aac --enable-nonfree --enable-libass --enable-libvorbis --enable-libtheora --enable-libfreetype --enable-libopus --enable-libvpx --enable-openssl --enable-shared;
+PATH="$HOME/ffmpeg_build/bin:$PATH" make;
+make install;
+make distclean;
+hash -r;
+
+# ---------------------------------------------------------------
 
 PYTHONS="cp27-cp27mu cp34-cp34m cp35-cp35m cp36-cp36m"
 
+mkdir wheelhouse
 # Compile wheels
 for PY in $PYTHONS; do
     rm -rf /io/Setup /io/build/
     PYBIN="/opt/python/${PY}/bin"
     "${PYBIN}/pip" install --upgrade cython nose
-    "${PYBIN}/pip" wheel /io/ -w wheelhouse/
+    PKG_CONFIG_PATH="$HOME/ffmpeg_build/lib/pkgconfig" "${PYBIN}/pip" wheel /io/ -w wheelhouse/
 done
 
 # Bundle external shared libraries into the wheels
@@ -134,6 +249,11 @@ done
 # Install packages and test
 for PY in $PYTHONS; do
     PYBIN="/opt/python/${PYBIN}/bin/"
-    "${PYBIN}/pip" install . --no-index -f /io/wheelhouse
+    PKG_CONFIG_PATH="$HOME/ffmpeg_build/lib/pkgconfig" "${PYBIN}/pip" install . --no-index -f /io/wheelhouse
     (cd "$HOME"; "${PYBIN}/nosetests" kivy)
+done
+
+# Bundle external shared libraries into the wheels
+for whl in wheelhouse/*.whl; do
+    auditwheel repair "$whl" -w /io/wheelhouse/
 done
