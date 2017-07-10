fd3b5a226306a812a03e46fdbecd7648e1b7e4e6
Peter Badida <KeyWeeUsr@users.noreply.github.com>
2017-07-11 00:41:14 +0200

    I wonder why I forgot -y

--- travis/build_wheels.sh
+++ travis/build_wheels.sh
@@ -209,7 +209,7 @@ echo "---"
 # enable display
 #/sbin/start-stop-daemon --start --quiet --pidfile /tmp/custom_xvfb_99.pid --make-pidfile --background --exec /usr/bin/Xvfb -- :99 -screen 0 1280x720x24 -ac +extension GLX;
 # for "daemon"
-yum install initscripts
+yum -y install initscripts
 export DISPLAY=:99.0
 source /etc/init.d/functions
 source /etc/rc.d/init.d/functions
