674a23d568001005bb021e4712a1d2fd043659c9
Peter Badida <KeyWeeUsr@users.noreply.github.com>
2017-07-11 14:57:28 +0200

    Show if the libs are installed

--- travis/test_wheels.sh
+++ travis/test_wheels.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 echo "Testing wheels:"
-pip install $(pwd)/wheelhouse/Kivy-1.10.1.dev0-cp27-cp27mu-manylinux1_x86_64.whl
+pip install $(pwd)/wheelhouse/Kivy-1.10.1.dev0-cp27-cp27mu-manylinux1_x86_64.whl --verbose
 cd ../..
 nosetests kivy
