16a7441a56184259db18cbe106209b222ae04cdf
Peter Badida <KeyWeeUsr@users.noreply.github.com>
2017-07-12 14:15:58 +0200

    Manually select tests folder

--- travis/test_wheels.sh
+++ travis/test_wheels.sh
@@ -5,7 +5,7 @@ pip install $(pwd)/wheelhouse/Kivy-1.10.1.dev0-cp27-cp27mu-manylinux1_x86_64.whl
 cd ~
 pwd
 cd /home/travis/
-nosetests kivy
+nosetests -w /opt/python/2.7.13/lib/python2.7/site-packages/kivy/tests
 echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
 LD_LIBRARY_PATH=/home/travis/build/KeyWeeUsr/kivy/.lib/:/home/travis/build/KeyWeeUsr/kivy/kivy/.lib/:/opt/python/2.7.13/lib/python2.7/site-packages/kivy/.lib/:/opt/python/2.7.13/lib/python2.7/site-packages/kivy/kivy/.lib:$LD_LIBRARY_PATH nosetests kivy
 ls -lah /home/travis/build/KeyWeeUsr/kivy/.lib/
