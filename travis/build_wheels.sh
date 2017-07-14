ff9163e277f607ef754c811100a1bd223800c092
Peter Badida <KeyWeeUsr@users.noreply.github.com>
2017-07-15 01:15:42 +0200

    Compile SDL2-mixer manually, fix broken stuff, make it more readable, check missing packages

--- travis/build_wheels.sh
+++ travis/build_wheels.sh
@@ -7,86 +7,131 @@ yum list installed
 
 echo "====================== DOWNLADING NEW ONES ======================"
 
-# add nux-desktop repo (for ffmpeg)
+# add nux-desktop repo (only for ffmpeg)
+# http://li.nux.ro/download/nux/dextop/el7/x86_64/
 rpm --import http://li.nux.ro/download/nux/RPM-GPG-KEY-nux.ro
 rpm -Uvh http://li.nux.ro/download/nux/dextop/el7/x86_64/nux-dextop-release-0-1.el7.nux.noarch.rpm
 yum repolist
 
 # add EPEL repo (SDL2* packages) https://centos.pkgs.org/7/epel-x86_64/
-wget http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-9.noarch.rpm
-rpm -Uvh epel-release*rpm
+#wget http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-9.noarch.rpm
+#rpm -Uvh epel-release*rpm
 
-# get RPM
+# repo: http://mirror.centos.org/centos/7/os/x86_64/Packages/
 yum check-update
+
+# general stuff
 yum install -y \
-    cmake \
     gcc \
     gcc-c++ \
-    mesa-libGLU \
-    mesa-libGLU-devel \
-    mesa-libGL \
-    mesa-libGL-devel \
-    mesa-libGLES \
-    mesa-libGLES-devel \
+    git \
+    pkgconfig \
     python-devel \
     dbus-devel \
-    xorg-x11-server-Xvfb \
+    xorg-x11-server-Xvfb
+# ok ok ok ok ok ok ok
+
+# general SDL2 stuff
+# https://hg.libsdl.org/SDL/file/default/docs/README-linux.md#l18
+yum install -y \
+    mercurial \
+    make \
+    cmake \
+    autoconf \
+    automake \
+    libtool \
+    libedit \
+    libass \
+    libass-devel \
+    pulseaudio \
+    pulseaudio-libs \
+    pulseaudio-libs-devel \
+    libX11-devel \
     libXext-devel \
     libXrandr-devel \
     libXcursor-devel \
+    libXi-devel \
     libXinerama-devel \
     libXxf86vm-devel \
     libXScrnSaver-devel \
-    libsamplerate-devel \
-    libjpeg-devel \
-    libtiff-devel \
-    libX11-devel \
-    libXi-devel \
-    libtool \
-    libedit \
-    pulseaudio \
-    pulseaudio-devel \
-    swscale-devel \
-    avformat-devel \
-    avcodev-devel \
-    mtdev-devel \
+    mesa-libGL \
+    mesa-libGL-devel \
     esd0-devel \
     udev-devel \
+    mesa-libGLU \
+    mesa-libGLU-devel \
+    mesa-libGLES \
+    mesa-libGLES-devel \
     ibus-1.0-devel \
     fcitx-libs \
+    libsamplerate-devel
+# ok ok ok ok ok ok ok no no ok ok ok ok ok ok ok ok
+# ok ok ok ok ok no no ok ok ok ok ok ok ok no ok
+
+# SDL2 image
+yum install -y \
+    libtiff-devel \
+    libpng-devel \
+    libjpeg-devel \
+    libjpeg-turbo-devel \
+    libwebp-devel
+# ok ok no ok ok
+
+# SDL2 mixer
+yum install -y \
+    libvorbis-devel
+# ok
+
+# SDL2 ttf
+yum install -y \
+    freetype-devel
+# ok
+
+# SDL2
+yum install -y \
+    systemd-devel \
+    mesa-libEGL \
+    mesa-libEGL-devel \
+    libxkbcommon-devel \
+    alsa-lib-devel
+# ok ok ok no ok
+
+# FFMpeg
+# libavcodec.so libavdevice.so libavfilter.so libavformat.so libavresample.so
+# libavutil.so libpostproc.so libswresample.so libswscale.so
+yum install -y ffmpeg-devel
+
+yum install -y \
+    mtdev-devel \
     ffmpeg \
-    ffmpeg-devel \
     smpeg-devel \
+    bzip2 \
+    bzip2-devel \
+    zlib-devel \
+    enca-devel \
+    fontconfig-devel \
+    openssl \
+    openssl-devel
+# ok ok no ok ok ok no ok ok ok
+
+# GStreamer
+# gstreamer1.0-alsa is in gstreamer1-plugins-base
+yum install -y \
     gstreamer1-devel \
     gstreamer1-plugins-base \
-    gstreamer1-plugins-base-devel \
-    SDL2_mixer \
-    SDL2_mixer-devel \
-    # SDL2 \
-    # SDL2-devel \
-    # SDL2_image \
-    # SDL2_image-devel \
-    # SDL2_ttf \
-    # SDL2_ttf-devel \
-    # maybe for future use
-    # SDL2_net \
-    # SDL2_net-devel \
-
-# gstreamer1.0-alsa is in gstreamer1-plugins-base
-
-# https://hg.libsdl.org/SDL/file/default/docs/README-linux.md#l18
-yum -y install libass libass-devel autoconf automake bzip2 cmake freetype-devel gcc gcc-c++ git libtool make mercurial pkgconfig zlib-devel enca-devel fontconfig-devel openssl openssl-devel
+    gstreamer1-plugins-base-devel
+# ok ok ok
 
 
-# # Make SDL2 packages
+# Make SDL2 packages
 SDL="SDL2-2.0.5"
-IMG="SDL_image-2.0.1"
-TTF="SDL_ttf-2.0.14"
-# MIX="SDL_mixer-2.0.1"
-curl -sL https://www.libsdl.org/release/${SDL}.tar.gz > ${SDL}.tar.gz
-curl -sL https://www.libsdl.org/projects/SDL_image/release/${IMG}.tar.gz > ${IMG}.tar.gz
-curl -sL https://www.libsdl.org/projects/SDL_ttf/release/${TTF}.tar.gz > ${TTF}.tar.gz
-# curl -sL https://www.libsdl.org/projects/SDL_mixer/release/${MIX}.tar.gz > ${MIX}.tar.gz
+IMG="SDL2_image-2.0.1"
+TTF="SDL2_ttf-2.0.14"
+MIX="SDL2_mixer-2.0.1"
+wget https://www.libsdl.org/release/${SDL}.tar.gz
+wget https://www.libsdl.org/projects/SDL_image/release/${IMG}.tar.gz
+wget https://www.libsdl.org/projects/SDL_ttf/release/${TTF}.tar.gz
+wget https://www.libsdl.org/projects/SDL_mixer/release/${MIX}.tar.gz
 
 # SDL image
 tar xzf ${IMG}.tar.gz
@@ -105,6 +150,32 @@ tar xzf ${TTF}.tar.gz
 pushd $TTF
 # https://hg.libsdl.org/SDL_ttf/file/3b93536d291a/debian/rules
 ./configure
+make
+make install
+popd
+
+# SDL mixer
+tar xzf ${MIX}.tar.gz
+pushd $MIX
+# https://hg.libsdl.org/SDL_mixer/file/15571e1ac71f/debian/rules
+./configure --enable-music-cmd \
+            --enable-music-mp3 \
+            --enable-music-mp3-smpeg \
+            --disable-music-mp3-mad-gpl \
+            --enable-music-mod \
+            --enable-music-mod-modplug \
+            --disable-music-mod-mikmod \
+            --enable-music-ogg \
+            --enable-music-wave \
+            --enable-music-midi \
+            --enable-music-midi-fluidsynth \
+            --enable-music-midi-timidity \
+            --disable-music-flac-shared \
+            --disable-music-ogg-shared \
+            --disable-music-mp3-smpeg-shared \
+            --disable-music-mod-mikmod-shared \
+            --disable-music-mod-modplug-shared \
+            --disable-music-midi-fluidsynth-shared \
 make
 make install
 popd
@@ -129,18 +200,8 @@ pushd $SDL
 make -j4
 make install
 popd
+# end SDL2
 
-# # SDL mixer
-# tar xzf ${MIX}.tar.gz
-# cd $MIX
-# ./configure --enable-music-mod --disable-music-mod-shared \
-            # --enable-music-ogg  --disable-music-ogg-shared \
-            # --enable-music-flac  --disable-music-flac-shared \
-            # --enable-music-mp3  --disable-music-mp3-shared
-# make
-# make install
-# cd ..
-# # end SDL2
 
 PYTHONS="cp27-cp27mu cp34-cp34m cp35-cp35m cp36-cp36m"
 mkdir libless_wheelhouse
@@ -264,7 +325,8 @@ done;
 ls -lah /io/wheelhouse
 
 
-# echo "====================== BACKING UP PACKAGES ======================"
+echo "====================== BACKING UP PACKAGES ======================"
+ls -lah /var/cache/yum
 # # ##
 # # note: if it all works, just backup all required AND installed RPMs somewhere
 # # in case of another EOL until ported to newer OS.
