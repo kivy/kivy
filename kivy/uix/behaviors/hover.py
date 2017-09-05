5756f45c60e08ce80a4a12ce85960987b3d12196
Atis <atiskr@gmail.com>
2017-09-05 21:55:52 +0300

    Replaced Weakproxy with widget.proxy_ref and changed "touch" argument name to "event"

--- kivy/uix/behaviors/hover.py
+++ kivy/uix/behaviors/hover.py
@@ -1,24 +1,23 @@
 __all__ = ('HoverBehavior', )
 
 from kivy.properties import BooleanProperty, NumericProperty, ObjectProperty
-from kivy.weakproxy import WeakProxy
 
 
 class HoverBehavior(object):
     hovering = BooleanProperty(False)
     _last_hover = None
 
-    def on_motion(self, touch):
-        if super(HoverBehavior, self).on_motion(touch):
+    def on_motion(self, event):
+        if super(HoverBehavior, self).on_motion(event):
             if self.hovering:
                 self.hovering = False
             return True
-        if self.collide_point(touch.x, touch.y):
+        if self.collide_point(event.x, event.y):
             self.hovering = True
             last = HoverBehavior._last_hover
             if last and last != self:
                 HoverBehavior._last_hover.hovering = False
-            HoverBehavior._last_hover = WeakProxy(self)
+            HoverBehavior._last_hover = self.proxy_ref
             return True
         elif self.hovering:
             self.hovering = False
