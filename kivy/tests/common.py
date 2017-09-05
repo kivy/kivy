085caeeecc661601a7b2bac6edb1382b8205cdb1
Peter Badida <KeyWeeUsr@users.noreply.github.com>
2017-09-05 21:50:15 +0200

    Make frames incrementing in GraphicUnitTest visible as a method

--- kivy/tests/common.py
+++ kivy/tests/common.py
@@ -283,3 +283,18 @@ class GraphicUnitTest(_base):
         root.mainloop()
 
         return self.retval
+
+    def advance_frames(self, count):
+        '''Render the new frames and:
+
+        * tick the Clock
+
+        * dispatch input from all registered providers
+
+        * flush all the canvas operations
+
+        * redraw Window canvas if necessary
+        '''
+        from kivy.base import EventLoop
+        for i in range(count):
+            EventLoop.idle()
