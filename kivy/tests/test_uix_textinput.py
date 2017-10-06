a96d6f376de678b94add89fa15a456b89b4f5e7d
Peter Badida <KeyWeeUsr@users.noreply.github.com>
2017-10-07 00:11:19 +0200

    Add unittest for TextInput.on_text_validate + focus switch

--- kivy/tests/test_uix_textinput.py
+++ kivy/tests/test_uix_textinput.py
@@ -5,6 +5,7 @@ uix.textinput tests
 
 import unittest
 
+from kivy.tests.common import GraphicUnitTest
 from kivy.uix.textinput import TextInput
 
 
@@ -34,3 +35,48 @@ class TextInputTest(unittest.TestCase):
         # If so Secondvery... should start from the 7th line
         pos_S = self.test_txt.index('S')
         self.assertEquals(instance.get_cursor_from_index(pos_S), (0, 6))
+
+
+class TextInputGraphicTest(GraphicUnitTest):
+    def test_text_validate(self):
+        ti = TextInput(multiline=False)
+        ti.focus = True
+
+        self.render(ti)
+        self.assertFalse(ti.multiline)
+        self.assertTrue(ti.focus)
+        self.assertTrue(ti.text_validate_unfocus)
+
+        ti.validate_test = None
+
+        ti.bind(on_text_validate=lambda *_: setattr(
+            ti, 'validate_test', True
+        ))
+        ti._key_down(
+            (
+                None,     # displayed_str
+                None,     # internal_str
+                'enter',  # internal_action
+                1         # scale
+            ),
+            repeat=False
+        )
+        self.assertTrue(ti.validate_test)
+        self.assertFalse(ti.focus)
+
+        ti.validate_test = None
+        ti.text_validate_unfocus = False
+        ti.focus = True
+        self.assertTrue(ti.focus)
+
+        ti._key_down(
+            (None, None, 'enter', 1),
+            repeat=False
+        )
+        self.assertTrue(ti.validate_test)
+        self.assertTrue(ti.focus)
+
+
+if __name__ == '__main__':
+    import unittest
+    unittest.main()
