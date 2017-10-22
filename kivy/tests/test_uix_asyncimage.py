6f2602031629b48e688762375614819ad8258097
Peter Badida <KeyWeeUsr@users.noreply.github.com>
2017-10-22 20:20:05 +0200

    Add unittest for AsyncImage + local .zip sequence

--- kivy/tests/test_uix_asyncimage.py
+++ kivy/tests/test_uix_asyncimage.py
@@ -3,28 +3,33 @@ from kivy.tests.common import GraphicUnitTest
 from kivy.uix.image import AsyncImage
 from kivy.config import Config
 
+from zipfile import ZipFile
+from os.path import join, dirname, abspath
+from os import remove
+
+try:
+    from urllib import urlretrieve
+except ImportError:
+    from urllib.request import urlretrieve
+
 
 class AsyncImageTestCase(GraphicUnitTest):
-    def test_remote_zipsequence(self):
-        # cube ZIP has 63 PNGs used for animation
-        ZIP = (
-            'https://github.com/kivy/kivy/'
-            'raw/master/examples/widgets/'
-            'sequenced_images/data/images/cube.zip'
-        )
+    def setUp(self):
+        self.maxfps = Config.getint('graphics', 'maxfps')
+        super(AsyncImageTestCase, self).setUp()
 
-        # !!! try to open the file and check
-        # textures manually (stronger test)
-        ZIP_pngs = 63
+    def zip_frames(self, path):
+        with ZipFile(path) as zipf:
+            return len(zipf.namelist())
 
+    def load_zipimage(self, source, frames):
         # keep running the test until loaded (or timeout)
-        maxfps = Config.getint('graphics', 'maxfps')
+        maxfps = self.maxfps
         timeout = 30 * maxfps
 
-        # load *remote* ZIP with images
-        # (named 000.png, 001.png, ...)
+        # load ZIP with images named: 000.png, 001.png, ...
         image = AsyncImage(
-            source=ZIP,
+            source=source,
             anim_delay=0.0333333333333333
         )
         image.test_loaded = False
@@ -43,13 +48,46 @@ class AsyncImageTestCase(GraphicUnitTest):
 
         proxyimg = image._coreimage
         self.assertTrue(proxyimg.anim_available)
-        self.assertEqual(len(proxyimg.image.textures), ZIP_pngs)
+        self.assertEqual(len(proxyimg.image.textures), frames)
+        return image
 
+    def test_remote_zipsequence(self):
+        # cube ZIP has 63 PNGs used for animation
+        ZIP = (
+            'https://github.com/kivy/kivy/'
+            'raw/master/examples/widgets/'
+            'sequenced_images/data/images/cube.zip'
+        )
+
+        # ref Loader._load_urllib
+        tempf, headers = urlretrieve(ZIP)
+        ZIP_pngs = self.zip_frames(tempf)
+        remove(tempf)
+
+        image = self.load_zipimage(ZIP, ZIP_pngs)
         # pure delay * fps isn't enough and
         # just +1 isn't either (index collisions)
         self.assertTrue(self.check_sequence_frames(
-            proxyimg,
-            int(proxyimg.anim_delay * maxfps + 3)
+            image._coreimage,
+            int(image._coreimage.anim_delay * self.maxfps + 3)
+        ))
+
+    def test_local_zipsequence(self):
+        # cube ZIP has 63 PNGs used for animation
+        ZIP = join(
+            # kivy/examples/.../cube.zip
+            dirname(dirname(dirname(abspath(__file__)))),
+            'examples', 'widgets', 'sequenced_images',
+            'data', 'images', 'cube.zip'
+        )
+        ZIP_pngs = self.zip_frames(ZIP)
+
+        image = self.load_zipimage(ZIP, ZIP_pngs)
+        # pure delay * fps isn't enough and
+        # just +1 isn't either (index collisions)
+        self.assertTrue(self.check_sequence_frames(
+            image._coreimage,
+            int(image._coreimage.anim_delay * self.maxfps + 3)
         ))
 
     def check_sequence_frames(self, img, frames, slides=5):
