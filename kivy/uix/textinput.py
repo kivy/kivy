2e2b4452d883be7873567b28250f4bb20a0727aa
Boyan Hristov <bhristov96@gmail.com>
2017-11-12 16:52:51 +0100

    refactored textinput::_key_down

--- kivy/uix/textinput.py
+++ kivy/uix/textinput.py
@@ -2247,9 +2247,22 @@ class TextInput(FocusBehavior, Widget):
 
     def _key_down(self, key, repeat=False):
         displayed_str, internal_str, internal_action, scale = key
+
+        # handle deletion
+        if self._selection and internal_action in (None, 'del', 'backspace', 'enter'):
+            self.delete_selection()
+        elif internal_action == 'del':
+            # Move cursor one char to the right. If that was successful,
+            # do a backspace (effectively deleting char right of cursor)
+            cursor = self.cursor
+            self.do_cursor_movement('cursor_right')
+            if cursor != self.cursor:
+                self.do_backspace(mode='del')
+        elif internal_action == 'backspace':
+            self.do_backspace()
+
+        # handle action keys and text insertion
         if internal_action is None:
-            if self._selection:
-                self.delete_selection()
             self.insert_text(displayed_str)
         elif internal_action in ('shift', 'shift_L', 'shift_R'):
             if not self._selection:
@@ -2274,22 +2287,8 @@ class TextInput(FocusBehavior, Widget):
                 self._update_selection()
             else:
                 self.cancel_selection()
-        elif self._selection and internal_action in ('del', 'backspace'):
-            self.delete_selection()
-        elif internal_action == 'del':
-            # Move cursor one char to the right. If that was successful,
-            # do a backspace (effectively deleting char right of cursor)
-            cursor = self.cursor
-            self.do_cursor_movement('cursor_right')
-            if cursor != self.cursor:
-                self.do_backspace(mode='del')
-        elif internal_action == 'backspace':
-            self.do_backspace()
         elif internal_action == 'enter':
-            if self.multiline and self._selection:
-                self.delete_selection()
-                self.insert_text(u'\n')
-            elif self.multiline:
+            if self.multiline:
                 self.insert_text(u'\n')
             else:
                 self.dispatch('on_text_validate')
@@ -2297,9 +2296,8 @@ class TextInput(FocusBehavior, Widget):
                     self.focus = False
         elif internal_action == 'escape':
             self.focus = False
-        if internal_action != 'escape':
-            # self._recalc_size()
-            pass
+
+
 
     def _key_up(self, key, repeat=False):
         displayed_str, internal_str, internal_action, scale = key
