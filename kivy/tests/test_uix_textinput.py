12cc116b20f11086708879bd4b22eaf78d37114b
Emil Milanov <emo_mlnv@yahoo.com>
2017-11-19 20:44:43 +0100

    Test for Ctrl + Arrow Keys

--- kivy/tests/test_uix_textinput.py
+++ kivy/tests/test_uix_textinput.py
@@ -290,6 +290,38 @@ class TextInputGraphicTest(GraphicUnitTest):
         )
         self.assertEqual(ti.cursor, (len(text) - steps_skip, 0))
 
+    def test_cursor_movement_control(self):
+        text = "these are\nmany words"
+        ti = TextInput(multiline=True, text=text)
+
+        ti.focus = True
+
+        self.render(ti)
+        self.assertTrue(ti.focus)
+
+        # assert cursor is here:
+        self.assertEqual(
+            ti.cursor, (
+                len(text.split('\n')[-1]),
+                len(text.split('\n')) - 1
+            )
+        )
+
+        options = (
+                ('cursor_left', (5, 1)),
+                ('cursor_left', (0, 1)),
+                ('cursor_left', (6, 0)),
+                ('cursor_right', (9, 0)),
+                ('cursor_right', (4, 1)))
+
+        for key, pos in options:
+            ti._key_down((None, None, 'ctrl_L', 1), repeat=False)
+            ti._key_down((None, None, key, 1), repeat=False)
+
+            self.assertEqual(ti.cursor, pos)
+
+            ti._key_up((None, None, 'ctrl_L', 1), repeat=False)
+
 
 if __name__ == '__main__':
     import unittest
