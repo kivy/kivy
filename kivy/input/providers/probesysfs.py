05fc6142412ed49a614dac3e7e66886bf0ce6476
Ray Gomez <codenomad@gmail.com>
2017-12-12 20:26:04 -0700

    Fix FileNotFoundError when sys path doesn't exist.
    Fixed whitespace
    
    Updated based on comments from PR

--- kivy/input/providers/probesysfs.py
+++ kivy/input/providers/probesysfs.py
@@ -82,7 +82,12 @@ else:
 
         def get_capabilities(self):
             path = os.path.join(self.path, "device", "capabilities", "abs")
-            line = read_line(path)
+            line = "0"
+            try:
+                line = read_line(path)
+            except OSError:
+                return []
+
             capabilities = []
             long_bit = getconf("LONG_BIT")
             for i, word in enumerate(line.split(" ")):
