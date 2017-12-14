2bb984fc66d53384c8754dd009046235bf1caff0
Matthew Einhorn <moiein2000@gmail.com>
2017-12-14 16:52:34 -0500

    Update example to use trio.

--- examples/async/existing_async_loop.py
+++ examples/async/existing_async_loop.py
@@ -1,4 +1,7 @@
-import asyncio
+import trio
+import os
+
+os.environ['KIVY_EVENTLOOP'] = 'trio'
 
 from kivy.app import App
 from kivy.lang.builder import Builder
@@ -15,45 +18,51 @@ BoxLayout:
 '''
 
 
-async def update_label1(root):
-    label = root.ids.label
-
-    i = 0
-    async for _ in root.ids.btn.async_bind('on_release'):
-        label.text = 'Pressed x{}'.format(i)
-        if i == 7:
-            break
-        i += 1
-    print('Done with update_label1')
-
-
 class MyApp(App):
 
     def build(self):
         return Builder.load_string(kv)
 
-    def on_start(self):
-        loop = asyncio.get_event_loop()
-        loop.create_task(update_label1(self.root))
-
 
 app_done = False
 
 
-async def run_app():
+async def run_app_happily():
     global app_done
     await MyApp().async_run()
     print('App done')
     app_done = True
 
 
-async def waste_time():
+async def watch_button_closely():
+    while App.get_running_app() is None or App.get_running_app().root is None:
+        await trio.sleep(.1)
+    root = App.get_running_app().root
+    print('app started')
+
+    label = root.ids.label
+    i = 0
+    async for _ in root.ids.btn.async_bind('on_release'):
+        label.text = 'Pressed x{}'.format(i)
+        if i == 7:
+            break
+        i += 1
+
+    label.text = 'Goodbye :('
+    print('Done with update_label1')
+
+
+async def waste_time_freely():
     while not app_done:
         print('Sitting on the beach')
-        await asyncio.sleep(2)
+        await trio.sleep(2)
     print('Done wasting time')
 
 if __name__ == '__main__':
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(asyncio.wait([waste_time(), run_app()]))
-    loop.close()
+    async def root_func():
+        async with trio.open_nursery() as nursery:
+            nursery.start_soon(run_app_happily)
+            nursery.start_soon(waste_time_freely)
+            nursery.start_soon(watch_button_closely)
+
+    trio.run(root_func)
