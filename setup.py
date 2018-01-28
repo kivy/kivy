b3ec9558935299b23d85244af856829c069f8aa2
Alexander Taylor <alexanderjohntaylor@gmail.com>
2018-01-28 10:56:07 +0000

    Added Python 3.6 to setup.py categories

--- setup.py
+++ setup.py
@@ -1080,6 +1080,7 @@ if not build_examples:
             'Programming Language :: Python :: 3.3',
             'Programming Language :: Python :: 3.4',
             'Programming Language :: Python :: 3.5',
+            'Programming Language :: Python :: 3.6',
             'Topic :: Artistic Software',
             'Topic :: Games/Entertainment',
             'Topic :: Multimedia :: Graphics :: 3D Rendering',
