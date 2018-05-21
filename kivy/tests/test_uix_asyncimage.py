36d4e03b08b28ea32ecd493a43b56f4c45c8232b
Gabriel Pettier <gabriel@tangibledisplay.com>
2018-05-21 21:02:18 +0200

    add test for #5761

--- kivy/tests/test_uix_asyncimage.py
+++ kivy/tests/test_uix_asyncimage.py
@@ -107,6 +107,29 @@ class AsyncImageTestCase(GraphicUnitTest):
 
         return True
 
+    def test_reload_asyncimage(self):
+        from kivy.resources import resource_find
+        from tempfile import mkdtemp
+        from os import symlink, unlink
+        from shutil import rmtree
+
+        fn = resource_find('data/logo/kivy-icon-16.png')
+        t = mkdtemp()
+        source = join(t, 'source.png')
+        symlink(fn, source)
+        image = AsyncImage(source=source)
+        self.render(image, framecount=2)
+        self.assertEqual(image.texture_size, [16, 16])
+        unlink(source)
+
+        fn = resource_find('data/logo/kivy-icon-32.png')
+        symlink(fn, source)
+        image.reload()
+        self.render(image, framecount=2)
+        self.assertEqual(image.texture_size, [32, 32])
+        unlink(source)
+        rmtree(t)
+
 
 if __name__ == '__main__':
     import unittest
