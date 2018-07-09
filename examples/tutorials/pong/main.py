ed17d45e2c21c11b7708b43702a23b7c80203a4e
erm3nda <erm3nda@users.noreply.github.com>
2018-07-09 23:12:36 +0200

    Added ball speed limit
    Depending on window width, theres a max ball speed due to framerate. I've hardcoded to 26 to make endles bouncing instead of fail.

--- examples/tutorials/pong/main.py
+++ examples/tutorials/pong/main.py
@@ -39,7 +39,12 @@ class PongPaddle(Widget):
             vx, vy = ball.velocity
             offset = (ball.center_y - self.center_y) / (self.height / 2)
             bounced = Vector(-1 * vx, vy)
-            vel = bounced * 1.1
+
+            # limiting ball max speed
+            if abs(vx) < 26:
+                vel = bounced * 1.1
+            else:
+                vel = bounced
             ball.velocity = vel.x, vel.y + offset
 
             # calling bounce callback, using player1.width and velocity_x as arguments to calculate
