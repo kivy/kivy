36828e8f554aaa996aa126b3f2130f0def6b325b
erm3nda <erm3nda@users.noreply.github.com>
2018-07-09 23:18:54 +0200

    Added step6 files with bounce manager content

---
+++ examples/tutorials/pong/steps/step6/main.py
@@ -0,0 +1,109 @@
+import kivy
+kivy.require('1.1.1')
+
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.properties import NumericProperty, ReferenceListProperty,\
+    ObjectProperty
+from kivy.vector import Vector
+from kivy.clock import Clock
+
+
+class BounceManager():
+    can_bounce = True
+
+    def bounce_callback(self, width, velx):
+        self.disable_bounce()
+        timer = self.get_timer(width, velx)
+        Clock.schedule_once(self.enable_bounce, timer)
+
+    def get_timer(self, width, velx):
+        fps = Clock.get_fps()
+        vel = abs(velx)
+        # calculate time needed to callback enable_bounce
+        value = width / vel / fps
+        return value
+
+    def enable_bounce(self, *args):
+        self.can_bounce = True
+
+    def disable_bounce(self, *args):
+        self.can_bounce = False
+
+
+class PongPaddle(Widget):
+    score = NumericProperty(0)
+
+    def bounce_ball(self, ball):
+        if self.collide_widget(ball) and self.parent.bouncer.can_bounce:
+            vx, vy = ball.velocity
+            offset = (ball.center_y - self.center_y) / (self.height / 2)
+            bounced = Vector(-1 * vx, vy)
+
+            # limiting ball max speed
+            if abs(vx) < 26:
+                vel = bounced * 1.1
+            else:
+                vel = bounced
+            ball.velocity = vel.x, vel.y + offset
+
+            # calling bounce callback, using player1.width and velocity_x as arguments to calculate
+            width = self.parent.player1.width # any paddle is ok
+            self.parent.bouncer.bounce_callback(width, vx)
+
+
+class PongBall(Widget):
+    velocity_x = NumericProperty(0)
+    velocity_y = NumericProperty(0)
+    velocity = ReferenceListProperty(velocity_x, velocity_y)
+
+    def move(self):
+        self.pos = Vector(*self.velocity) + self.pos
+
+
+class PongGame(Widget):
+    ball = ObjectProperty(None)
+    player1 = ObjectProperty(None)
+    player2 = ObjectProperty(None)
+    bouncer = BounceManager()
+
+    def serve_ball(self, vel=(4, 0)):
+        self.ball.center = self.center
+        self.ball.velocity = vel
+
+    def update(self, dt):
+        self.ball.move()
+
+        # bounce ball off paddles
+        self.player1.bounce_ball(self.ball)
+        self.player2.bounce_ball(self.ball)
+
+        # bounce ball off bottom or top
+        if (self.ball.y < self.y) or (self.ball.top > self.top):
+            self.ball.velocity_y *= -1
+
+        # went off a side to score point?
+        if self.ball.x < self.x:
+            self.player2.score += 1
+            self.serve_ball(vel=(4, 0))
+        if self.ball.x > self.width:
+            self.player1.score += 1
+            self.serve_ball(vel=(-4, 0))
+
+    def on_touch_move(self, touch):
+        if touch.x < self.width / 3:
+            self.player1.center_y = touch.y
+        if touch.x > self.width - self.width / 3:
+            self.player2.center_y = touch.y
+
+
+class PongApp(App):
+    def build(self):
+        game = PongGame()
+        game.serve_ball()
+        Clock.schedule_interval(game.update, 1.0 / 60.0)
+        return game
+
+
+if __name__ == '__main__':
+    PongApp().run()
