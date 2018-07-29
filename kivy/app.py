888df858c5ea009a920c3c8905ab6abfa8e3e3bb
naman653 <naman653@gmail.com>
2018-07-29 19:55:37 +0530

    Added changes to build_config method in App class

--- kivy/app.py
+++ kivy/app.py
@@ -511,7 +511,7 @@ class App(EventDispatcher):
                 Use this to add default section / key / value items
 
         '''
-	pass
+        pass
 
     def build_settings(self, settings):
         '''.. versionadded:: 1.0.7
