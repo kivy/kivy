38fe9bba837ee2957b7d31bf2e615207bedb1b06
Kjetil Andre Liknes <kliknes@gmail.com>
2019-04-27 11:48:03 +0200

    Set cookie header workaround (#6256)
    * Implemented small workaround to parse the Set-Cookie header so we do not lose cookies if multiple Set-Cookie headers are present.
    
    * Forgot to actually use the parsed headers

--- kivy/network/urlrequest.py
+++ kivy/network/urlrequest.py
@@ -460,11 +460,22 @@ class UrlRequest(Thread):
             except IndexError:
                 return
             if resp:
+                # Small workaround in order to prevent the situation mentioned
+                # in the comment below
+                final_cookies = ""
+                parsed_headers = []
+                for key, value in resp.getheaders():
+                    if key == "Set-Cookie":
+                        final_cookies += "{};".format(value)
+                    else:
+                        parsed_headers.append((key, value))
+                parsed_headers.append(("Set-Cookie", final_cookies[:-1]))
+
                 # XXX usage of dict can be dangerous if multiple headers
                 # are set even if it's invalid. But it look like it's ok
                 # ?  http://stackoverflow.com/questions/2454494/..
                 # ..urllib2-multiple-set-cookie-headers-in-response
-                self._resp_headers = dict(resp.getheaders())
+                self._resp_headers = dict(parsed_headers)
                 self._resp_status = resp.status
             if result == 'success':
                 status_class = resp.status // 100
