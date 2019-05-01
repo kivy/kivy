853b9a3d2def3521f351fa37bc016b93b3f6c17c
Akshay Arora <akshayaurora@gmail.com>
2019-05-01 22:17:05 +0530

    example for adding, `background_color` to Label (#6282)
    Example for adding, `background_color` to Label

--- kivy/uix/label.py
+++ kivy/uix/label.py
@@ -58,6 +58,30 @@ text at a certain width:
         text_size: root.width, None
         size: self.texture_size
 
+How to have a custom background color in the label:
+
+.. code-block:: kv
+
+    # Define your background color Template
+    <BackgroundColor@Widget>
+        background_color: 1, 1, 1, 1
+        canvas.before:
+            Color:
+                rgba: root.background_color
+            Rectangle:
+                size: self.size
+                pos: self.pos
+    # Now you can simply Mix the `BackgroundColor` class with almost
+    # any other widget... to give it a background.
+    <BackgroundLabel@Label+BackgroundColor>
+        background_color: 0, 0, 0, 0
+        # Default the background color for this label
+        # to r 0, g 0, b 0, a 0
+    # Use the BackgroundLabel any where in your kv code like below
+    BackgroundLabel
+        text: 'Hello'
+        background_color: 1, 0, 0, 1
+
 Text alignment and wrapping
 ---------------------------
 
