f57649d5052aca053f056b88ac02a93cbb5347d9
gottadiveintopython <flow4re2c@gmail.com>
2019-06-01 11:04:50 +0900

    add tests for #6338

--- kivy/tests/test_screen.py
+++ kivy/tests/test_screen.py
@@ -1,3 +1,11 @@
+import pytest
+
+TRANSITION_CLASS_NAMES = (
+    'ShaderTransition', 'SlideTransition', 'SwapTransition', 'FadeTransition',
+    'WipeTransition', 'FallOutTransition', 'RiseInTransition', 'NoTransition',
+    'CardTransition',
+)
+
 
 def test_switch_to():
     import kivy.uix.screenmanager as sm
@@ -12,3 +20,19 @@ def test_switch_to():
     manager.switch_to(screen1)
     manager.current = 'second'
     manager.switch_to(screen2)
+
+
+@pytest.mark.parametrize('transition_cls_name', TRANSITION_CLASS_NAMES)
+def test_switching_does_not_affect_a_list_of_screens(transition_cls_name):
+    import kivy.uix.screenmanager as sm
+    transition_cls = getattr(sm, transition_cls_name)
+    scrmgr = sm.ScreenManager()
+    for i in range(3):
+        scrmgr.add_widget(sm.Screen(name=str(i)))
+    names = scrmgr.screen_names.copy()
+    scrmgr.transition = transition_cls()
+    scrmgr.current = '1'
+    assert names == scrmgr.screen_names
+    scrmgr.transition = transition_cls()
+    scrmgr.current = '2'
+    assert names == scrmgr.screen_names
