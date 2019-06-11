b40580dbc85a14bcf56e2b955794ca8457fd2c33
Matthew Einhorn <moiein2000@gmail.com>
2019-06-11 02:07:15 -0400

    Fix permission issue.

--- .ci/rpi_test.sh
+++ .ci/rpi_test.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
+
 set -e -x
 cd /io;
 
-yes | sudo add-apt-repository ppa:gstreamer-developers/ppa;
-sudo apt update;
-sudo apt -y install libsdl2-dev libsdl2-ttf-dev libsdl2-image-dev libsdl2-mixer-dev;
-sudo apt -y install libgstreamer1.0-dev gstreamer1.0-alsa gstreamer1.0-plugins-base;
-sudo apt -y install python3 python3-dev libsmpeg-dev libswscale-dev libavformat-dev libavcodec-dev libjpeg-dev libtiff4-dev libX11-dev libmtdev-dev;
-sudo apt -y install python3-setuptools build-essential libgl1-mesa-dev libgles2-mesa-dev;
-sudo apt -y install xvfb pulseaudio xsel;
+yes | add-apt-repository ppa:gstreamer-developers/ppa;
+apt update;
+apt -y install libsdl2-dev libsdl2-ttf-dev libsdl2-image-dev libsdl2-mixer-dev;
+apt -y install libgstreamer1.0-dev gstreamer1.0-alsa gstreamer1.0-plugins-base;
+apt -y install python3 python3-dev libsmpeg-dev libswscale-dev libavformat-dev libavcodec-dev libjpeg-dev libtiff4-dev libX11-dev libmtdev-dev;
+apt -y install python3-setuptools build-essential libgl1-mesa-dev libgles2-mesa-dev;
+apt -y install xvfb pulseaudio xsel;
 export CYTHON_INSTALL=$(KIVY_NO_CONSOLELOG=1 python3 -c "from kivy.tools.packaging.cython_cfg import get_cython_versions; print(get_cython_versions()[0])"  --config "kivy:log_level:error");
 python3 -m pip install -I "$CYTHON_INSTALL";
 python3 -m pip install --upgrade pillow pytest coveralls docutils PyInstaller;
