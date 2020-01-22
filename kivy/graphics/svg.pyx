67c88e8cb5d97d55780a457578d632c8f55f8ed2
Ludwig Kraatz <ludwigkraatz@users.noreply.github.com>
2020-01-22 12:26:05 +0100

    update docstrings

--- kivy/graphics/svg.pyx
+++ kivy/graphics/svg.pyx
@@ -385,8 +385,13 @@ cdef class Svg(RenderContext):
             subdivide circular and elliptic arcs. Defaults to 10.
         :param color the default color to use for Svg elements that specify "currentColor"
 
+        .. note:: if you want to use SVGs from string, you can parse the source yourself
+            using `from xml.etree.cElementTree import fromstring` and pass the result it to 
+            Svg().set_tree(). This will trigger the rendering of the Svg - as an alternative
+            to assigning a filepath to Svg.source. This is also viable to trigger reloading.
+
         .. versionchanged:: 2.0.0
-            Parameter `filename` changed to `source`.
+            Parameter `filename` changed to `source`, and its optional now.
         '''
 
         super(Svg, self).__init__(fs=SVG_FS, vs=SVG_VS,
@@ -519,6 +524,13 @@ cdef class Svg(RenderContext):
             fd.close()
 
     def set_tree(self, tree):
+        '''
+        sets the tree used to render the Svg and triggers reloading.
+	
+	:param xml.etree.cElementTree tree: the tree parsed from the SVG source
+
+        .. versionadded:: 2.0.0
+        '''
         self.tree = tree
         self.reload()
 
