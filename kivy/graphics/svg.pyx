36b65f6de9925cc761fd040ccbd5e84586aba9ce
Ludwig Kraatz <ludwigkraatz@users.noreply.github.com>
2020-01-22 12:30:09 +0100

    minor beauty fixes

--- kivy/graphics/svg.pyx
+++ kivy/graphics/svg.pyx
@@ -386,12 +386,12 @@ cdef class Svg(RenderContext):
         :param color the default color to use for Svg elements that specify "currentColor"
 
         .. note:: if you want to use SVGs from string, you can parse the source yourself
-            using `from xml.etree.cElementTree import fromstring` and pass the result it to 
+            using `from xml.etree.cElementTree import fromstring` and pass the result to 
             Svg().set_tree(). This will trigger the rendering of the Svg - as an alternative
             to assigning a filepath to Svg.source. This is also viable to trigger reloading.
 
         .. versionchanged:: 2.0.0
-            Parameter `filename` changed to `source`, and its optional now.
+            Parameter `filename` changed to `source` and made optional.
         '''
 
         super(Svg, self).__init__(fs=SVG_FS, vs=SVG_VS,
@@ -527,7 +527,7 @@ cdef class Svg(RenderContext):
         '''
         sets the tree used to render the Svg and triggers reloading.
 	
-	:param xml.etree.cElementTree tree: the tree parsed from the SVG source
+        :param xml.etree.cElementTree tree: the tree parsed from the SVG source
 
         .. versionadded:: 2.0.0
         '''
