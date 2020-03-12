a8ee3c36f885b8c585bedcd528b977da381000f8
pythonic64 <pythonic64@gmail.com>
2020-03-13 00:02:37 +0100

    Better handling of grabbed event in HoverBehavior.

--- kivy/uix/behaviors/hover.py
+++ kivy/uix/behaviors/hover.py
@@ -16,9 +16,10 @@ class HoverBehavior(object):
     def on_motion(self, etype, me):
         if me.type_name != 'hover':
             return super().on_motion(etype, me)
-        if me.grab_current is self \
-                and self.handle_hover_event_on_self(etype, me):
-            return True
+        if me.grab_current is self:
+            if self.handle_hover_event_on_self(etype, me):
+                return True
+            return super().on_motion(etype, me)
         if self.hover_mode == 'default':
             if super().on_motion(etype, me):
                 return True
@@ -31,19 +32,25 @@ class HoverBehavior(object):
                  self.handle_hover_event_on_self(etype, me))
             )
 
+    def on_hover_ids(self, widget, hover_ids):
+        self.hovered = bool(hover_ids)
+
     def handle_hover_event_on_self(self, etype, me):
         if me.grab_current is self:
-            if etype == 'end':
+            if etype == 'end' and me.device_id in self.hover_ids:
                 self.hover_ids.remove(me.device_id)
-                self.hovered = bool(self.hover_ids)
                 self.dispatch('on_hover_end', me)
             return True
         if self.collide_point(*me.pos):
+            if etype == 'end':
+                if me.device_id in self.hover_ids:
+                    self.hover_ids.remove(me.device_id)
+                    self.dispatch('on_hover_end', me)
+                return True
             if me.device_id in self.hover_ids:
                 self.dispatch('on_hover_move', me)
             else:
                 self.hover_ids.append(me.device_id)
-                self.hovered = True
                 self.dispatch('on_hover_start', me)
             me.grab(self)
             return True
