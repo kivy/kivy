7ac1dcc2025dffd6e203612c09d0652fd3d54038
Sander Land <sander.land@cognite.com>
2020-06-01 16:06:06 +0200

    black

--- kivy/tests/test_issues/test_issue_6909.py
+++ kivy/tests/test_issues/test_issue_6909.py
@@ -8,4 +8,4 @@ class CodecLoggingTestCase(unittest.TestCase):
         Config.set("kivy", "log_enable", 1)
         Config.set("kivy", "log_level", "trace")
         for string in ["한국어", "Niñas and niños"]:
-            Logger.trace('Lang: call_fn => value=%r' % (string,))
+            Logger.trace("Lang: call_fn => value=%r" % (string,))
