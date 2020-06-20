2c021f87b1b2a39b791550a1e2687032060004dc
matham <moiein2000@gmail.com>
2020-06-19 20:32:33 -0400

    Raise an exception if key has more than one period.

--- kivy/uix/recycleview/views.py
+++ kivy/uix/recycleview/views.py
@@ -113,7 +113,7 @@ class RecycleDataViewBehavior(object):
 
 class RecycleKVIDsDataViewBehavior(RecycleDataViewBehavior):
     """Similar to :class:`RecycleDataViewBehavior`, except that the data keys
-    can signify properties of objects named with an id in KV.
+    can signify properties of an object named with an id in the root KV rule.
 
     E.g. given a KV rule::
 
@@ -127,6 +127,11 @@ class RecycleKVIDsDataViewBehavior(RecycleDataViewBehavior):
     ``rv.data = [{'name.text': 'Kivy user', 'value.text': '12'}]`` would
     automatically set the corresponding labels.
 
+    So, if the key doesn't have a period, the named property of the root widget
+    will be set to the corresponding value. If there is a period, the named
+    property of the widget with the id listed before the period will be set to
+    the corresponding value.
+
     .. versionadded:: 2.0.0
     """
 
@@ -134,8 +139,11 @@ class RecycleKVIDsDataViewBehavior(RecycleDataViewBehavior):
         sizing_attrs = RecycleDataAdapter._sizing_attrs
         for key, value in data.items():
             if key not in sizing_attrs:
-                name, *ids = key.split('.', maxsplit=1)
+                name, *ids = key.split('.')
                 if ids:
+                    if len(ids) != 1:
+                        raise ValueError(
+                            f'Data key "{key}" has more than one period')
                     setattr(self.ids[name], ids[0], value)
                 else:
                     setattr(self, name, value)
