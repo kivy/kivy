ae0d71cf4cc8ec981320075f6ef917b223078376
fruitbat <zenkey.zencode@gmail.com>
2020-07-05 21:37:37 +0200

    Apply same changes to dropdown

--- kivy/uix/dropdown.py
+++ kivy/uix/dropdown.py
@@ -308,10 +308,12 @@ class DropDown(ScrollView):
         return True
 
     def on_touch_up(self, touch):
-        if self.auto_dismiss and not self._touch_started_inside:
+        # Explicitly test for False as None occurs when shown by on_touch_down
+        if self.auto_dismiss and self._touch_started_inside is False:
             self.dismiss()
-            return True
-        super(DropDown, self).on_touch_up(touch)
+        else:
+            super(DropDown, self).on_touch_up(touch)
+        self._touch_started_inside = None
         return True
 
     def _reposition(self, *largs):
