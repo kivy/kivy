a8f0ea8968f09b051bedfa524e1d08a304d0650b
gottadiveintopython <flow4re2c@gmail.com>
2020-11-06 16:41:16 +0900

    make sure the Bubble overwrites a certain properties

--- kivy/tests/test_uix_bubble.py
+++ kivy/tests/test_uix_bubble.py
@@ -1,6 +1,17 @@
 import pytest
 
 
+@pytest.mark.parametrize('prop_name', (
+    '_fills_row_first',
+    '_fills_from_left_to_right',
+    '_fills_from_top_to_bottom',
+))
+def test_a_certain_properties_exist_in_any_of_the_super_classes(prop_name):
+    from kivy.uix.bubble import Bubble
+    super_classes = Bubble.mro()[1:]  # exclude Bubble itself
+    assert any(hasattr(klass, prop_name) for klass in super_classes)
+
+
 @pytest.mark.parametrize('orientation', ('vertical', 'horizontal'))
 def test_always_lr_tb(orientation):
     from kivy.uix.bubble import Bubble
