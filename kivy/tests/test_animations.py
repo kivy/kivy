6a5f388d0cb13c4ef5800ebd540c46a8748535be
gottadiveintopython <flow4re2c@gmail.com>
2020-11-18 21:22:08 +0900

    Revert "tighten a margin of error"
    This reverts commit 14b21181e44a622710858408e0fb2e7332cd5a5b.

--- kivy/tests/test_animations.py
+++ kivy/tests/test_animations.py
@@ -35,12 +35,6 @@ def ec_cls():
     return EventCounter
 
 
-@pytest.fixture(scope='module')
-def approx():
-    from functools import partial
-    return partial(pytest.approx, abs=1)
-
-
 @pytest.fixture(autouse=True)
 def cleanup():
     from kivy.animation import Animation
@@ -67,14 +61,14 @@ def sleep(t):
 
 class TestAnimation:
 
-    def test_start_animation(self, approx):
+    def test_start_animation(self):
         from kivy.animation import Animation
         from kivy.uix.widget import Widget
         a = Animation(x=100, d=1)
         w = Widget()
         a.start(w)
         sleep(1.5)
-        assert w.x == approx(100)
+        assert w.x == pytest.approx(100)
         assert no_animations_being_played()
 
     def test_animation_duration_0(self):
@@ -86,7 +80,7 @@ class TestAnimation:
         sleep(.5)
         assert no_animations_being_played()
 
-    def test_stop_animation(self, approx):
+    def test_stop_animation(self):
         from kivy.animation import Animation
         from kivy.uix.widget import Widget
         a = Animation(x=100, d=1)
@@ -94,8 +88,8 @@ class TestAnimation:
         a.start(w)
         sleep(.5)
         a.stop(w)
-        assert w.x != approx(100)
-        assert w.x != approx(0)
+        assert w.x != pytest.approx(100)
+        assert w.x != pytest.approx(0)
         assert no_animations_being_played()
 
     def test_stop_all(self):
@@ -133,16 +127,16 @@ class TestAnimation:
         a = Animation(x=100)
         assert a.animated_properties == {'x': 100, }
 
-    def test_animated_instruction(self, approx):
+    def test_animated_instruction(self):
         from kivy.graphics import Scale
         from kivy.animation import Animation
         a = Animation(x=100, d=1)
         instruction = Scale(3)
         a.start(instruction)
         assert a.animated_properties == {'x': 100, }
-        assert instruction.x == approx(3)
+        assert instruction.x == pytest.approx(3)
         sleep(1.5)
-        assert instruction.x == approx(100)
+        assert instruction.x == pytest.approx(100)
         assert no_animations_being_played()
 
     def test_weakref(self):
