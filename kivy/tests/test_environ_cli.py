29db0aeeaf50c313cfd9a8711145b072e8aabbf1
pwoolvett <pablowoolvett@gmail.com>
2021-01-11 15:43:19 -0300

    implemented suggestions
    * updated patchenv func
    * added `KIVY_ENVS_TO_EXCLUDE = ('KIVY_UNITTEST', 'KIVY_PACKAGING')` because `__init__.py` treats them equal. 
    * removed remanent lower

--- kivy/tests/test_environ_cli.py
+++ kivy/tests/test_environ_cli.py
@@ -10,8 +10,9 @@ import sys
 import pytest
 
 ENV_NAME="KIVY_NO_ARGS"
-EXPECTED_STR = "Kivy Usage"
+KIVY_ENVS_TO_EXCLUDE = ('KIVY_UNITTEST', 'KIVY_PACKAGING')
 
+EXPECTED_STR = "Kivy Usage"
 
 TRUTHY = {'true', '1', 'yes'}
 FALSY = {'false', '0', 'no', "anything-else"}
@@ -20,15 +21,12 @@ SAMPLE_VALUES = {
     *TRUTHY, *FALSY
 }
 
-def _patch_env(**kw):
+def _patch_env(*filtered_keys, **kw):
     env = {
         k: v
         for k, v in environ.items()
-        if "kivy" not in k.lower()
+        if k not in filtered_keys
     }
-
-    env.update(**kw)
-    return env
 
 def _kivy_subproces_import(env):
     return subprocess.check_output(
@@ -40,15 +38,15 @@ def _kivy_subproces_import(env):
 
 @pytest.mark.parametrize("value", SAMPLE_VALUES)
 def test_env_exist(value):
-    env = _patch_env(**{ENV_NAME:value})
+    env = _patch_env(ENV_NAME, *KIVY_ENVS_TO_EXCLUDE, **{ENV_NAME:value})
     stdout = _kivy_subproces_import(env)
 
-    if value.lower() in TRUTHY:
+    if value in TRUTHY:
         assert EXPECTED_STR not in stdout
     else:
         assert EXPECTED_STR in stdout
 
 def test_env_not_exist():
-    env = _patch_env()
+    env = _patch_env(ENV_NAME *KIVY_ENVS_TO_EXCLUDE)
     stdout = _kivy_subproces_import(env)
     assert EXPECTED_STR in stdout
