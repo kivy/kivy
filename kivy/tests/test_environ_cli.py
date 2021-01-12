f418abca17192f2c952c263125708fc6cd4f37fd
pwoolvett <pablowoolvett@gmail.com>
2021-01-11 21:04:47 -0300

    Update kivy/tests/test_environ_cli.py
    Co-authored-by: matham <moiein2000@gmail.com>

--- kivy/tests/test_environ_cli.py
+++ kivy/tests/test_environ_cli.py
@@ -26,7 +26,7 @@ def _patch_env(*filtered_keys, **kw):
 
 def _kivy_subproces_import(env):
     return subprocess.run(
-        [sys.executable, "-c", "'import kivy'", "--help"],
+        [sys.executable, "-c", "import kivy", "--help"],
         stdout=subprocess.PIPE,
         stderr=subprocess.PIPE,
         env=env,
