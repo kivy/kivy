0f38de794b90fdc1b448678ddaa6a7f1b3415438
Steven Wells <stevewells20@gmail.com>
2021-01-14 07:35:36 -0800

    Added documentation for RecycleView viewclass statefullness

--- kivy/uix/recycleview/__init__.py
+++ kivy/uix/recycleview/__init__.py
@@ -8,6 +8,9 @@ The RecycleView provides a flexible model for viewing selected sections of
 large data sets. It aims to prevent the performance degradation that can occur
 when generating large numbers of widgets in order to display many data items.
 
+.. warning::
+    Because RecycleView reuses widgets, any state change to a single widget will stay with that widget, even if the data assigned to it by the RecycleView changes.
+
 The view is generatad by processing the :attr:`~RecycleView.data`, essentially
 a list of dicts, and uses these dicts to generate instances of the
 :attr:`~RecycleView.viewclass` as required. Its design is based on the
@@ -154,6 +157,94 @@ as follows::
 
 Please see the `examples/widgets/recycleview/basic_data.py` file for a more
 complete example.
+
+## Viewclass State
+
+As the viewclass widgets are reused or instantiated as needed by the RecycleView, the order and content of the widgets are mutable. Any state change to a single widget will stay with that widget, even if the data assigned to it from the RecycleView.data dict changes.
+
+Two methods for managing state changes in viewclass widgets:
+
+* Store state in the RecycleView.data Model
+* Generate state changes on-the-fly by catching updates or binding
+
+An example::
+
+    from kivy.app import App
+    from kivy.lang import Builder
+    from kivy.uix.boxlayout import BoxLayout
+    from kivy.uix.recycleview import RecycleView
+    from kivy.uix.recycleview.views import RecycleDataViewBehavior
+    from kivy.properties import BooleanProperty, StringProperty
+
+    Builder.load_string('''
+    <StatefulLabel>:
+        active: storedStateExample.active
+        CheckBox:
+            id: storedStateExample
+            active: root.active
+            on_release: root.store_checkbox_state()
+        Label:
+            text: root.text
+        Label:
+            id: generatedStateExample
+            text: root.generated_state_text
+                
+    <RV>:
+        viewclass: 'StatefulLabel'
+        RecycleBoxLayout:
+            size_hint_y: None
+            height: self.minimum_height
+            orientation: 'vertical'
+    ''')
+
+
+    class StatefulLabel(RecycleDataViewBehavior, BoxLayout):
+        text = StringProperty()
+        generated_state_text = StringProperty()
+        active = BooleanProperty()
+        index = 0
+
+        '''
+        To change a viewclass' state as the data assigned to it changes,
+        overload the refresh_view_attrs function (inherited from 
+        RecycleDataViewBehavior)
+        '''
+        def refresh_view_attrs(self, rv, index, data):
+            self.index = index
+            if data['text'] == '0':
+                self.generated_state_text = "is zero"
+            elif int(data['text']) % 2 == 1:
+                self.generated_state_text = "is odd"
+            else:
+                self.generated_state_text = "is even"
+
+            super(StatefulLabel, self).refresh_view_attrs(rv, index, data)
+
+        '''
+        To keep state changes in the viewclass with associated data, 
+        they can be stored in the RecycleView's data object
+        '''
+        def store_checkbox_state(self):
+            rv = App.get_running_app().rv
+            rv.data[self.index]['active'] = self.active
+
+
+    class RV(RecycleView, App):
+        def __init__(self, **kwargs):
+            super(RV, self).__init__(**kwargs)
+            self.data = [{'text': str(x), 'active': False} for x in range(10)]
+            App.get_running_app().rv = self
+
+        def build(self):
+            return self
+
+
+    if __name__ == '__main__':
+        RV().run()
+
+
+#
+
 
 TODO:
     - Method to clear cached class instances.
@@ -527,3 +618,4 @@ class RecycleView(RecycleViewBehavior, ScrollView):
     sets the key viewclass for the current
     :attr:`~kivy.uix.recycleview.layout_manager`.
     """
+
