a4a92d557e52139ca62001dfd4d199d3343cffd5
Daniel Foerster <pydsigner@gmail.com>
2021-02-01 19:49:02 -0600

    Address weird issue with unittest for Widget.clear_widgets()

--- kivy/tests/test_widget.py
+++ kivy/tests/test_widget.py
@@ -31,14 +31,14 @@ class WidgetTestCase(unittest.TestCase):
             pass
 
     def test_clear_widgets(self):
-        root = self.cls()
+        root = self.root
         self.assertEqual(root.children, [])
         c1 = self.cls()
         c2 = self.cls()
         c3 = self.cls()
-        root.add_widget(c1)
-        root.add_widget(c2)
-        root.add_widget(c3)
+        root.add_widget(c1, index=0)
+        root.add_widget(c2, index=1)
+        root.add_widget(c3, index=2)
         self.assertEqual(root.children, [c1, c2, c3])
         root.clear_widgets([c2])
         self.assertEqual(root.children, [c1, c3])
