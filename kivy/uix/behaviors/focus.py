6afa992c68986eb51e920507722dca361ab2533e
xayhewalo <7256157+xayhewalo@users.noreply.github.com>
2021-02-07 04:21:41 +0000

    allow modifiers besides 'shift' to trigger get_focus_previous(), fixes #7364

--- kivy/uix/behaviors/focus.py
+++ kivy/uix/behaviors/focus.py
@@ -531,7 +531,7 @@ class FocusBehavior(object):
         key was consumed.
         '''
         if keycode[1] == 'tab':  # deal with cycle
-            if ['shift'] == modifiers:
+            if 'shift' in modifiers:
                 next = self.get_focus_previous()
             else:
                 next = self.get_focus_next()
