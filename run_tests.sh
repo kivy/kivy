cfc737d35d7a4672fc9c97a8d15545a2092257ce
zencode <zenkey.zencode@gmail.com>
2021-04-03 12:59:44 +0200

    :sparkles: Tweak wording

--- run_tests.sh
+++ run_tests.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
-# Print test output
+# =================
+# Run Kivy Tests
+# =================
+# When running tests, we have 3 useful options. Comment and uncomment as
+# desired.
+
+# 1. Print test output
 pytest --cov-report term-missing --cov=kivy kivy/tests/
-# Print test output and save to html. To view, open `htmlcov/index.html`.
+
+# 2. Save test output to html. To view, run the below and open
+#    `htmlcov/index.html`.
+# Note: This option does not currently include the missing lines analysis.
 # pytest --cov-report html --cov-report term-missing --cov=kivy kivy/tests/
+
+# 3. Print test output and save to 'coverage.txt'
+# pytest --cov-report term-missing --cov=kivy kivy/tests/ > coverage.txt
