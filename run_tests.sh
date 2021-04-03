3be72c749124f70f3cc53c1b61c0f81bb0325ff0
zencode <zenkey.zencode@gmail.com>
2021-04-03 12:51:02 +0200

    :arrow_up: Add option for html output

--- run_tests.sh
+++ run_tests.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
+# Print test output
 pytest --cov-report term-missing --cov=kivy kivy/tests/
+# Print test output and save to html. To view, open `htmlcov/index.html`.
+# pytest --cov-report html --cov-report term-missing --cov=kivy kivy/tests/
