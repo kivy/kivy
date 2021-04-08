f39d1825b713d98bc5db9383278244bf459061dc
Robert Niederreiter <office@squarewave.at>
2021-04-08 09:30:41 +0200

    Add missing import in Video widget

--- kivy/uix/video.py
+++ kivy/uix/video.py
@@ -42,7 +42,7 @@ from kivy.uix.image import Image
 from kivy.core.video import Video as CoreVideo
 from kivy.resources import resource_find
 from kivy.properties import (BooleanProperty, NumericProperty, ObjectProperty,
-                             OptionProperty)
+                             OptionProperty, StringProperty)
 from kivy.logger import Logger
 
 
