9e5426b8816fd6e9d34fbc202799e79054722d9f
pythonic64 <pythonic64@gmail.com>
2021-04-16 23:10:56 +0200

    Added test for WindowBase.to_relative_pos method.

---
+++ kivy/tests/test_window_base.py
@@ -0,0 +1,16 @@
+from itertools import product
+
+from kivy.tests import GraphicUnitTest
+
+
+class WindowBaseTest(GraphicUnitTest):
+
+    def test_to_relative_pos(self):
+        for x, y in product([0, 319, 50, 51], [0, 239, 50, 51]):
+            self.Window.system_size = (320, 240)
+            w, h = self.Window.system_size
+            expected_sx = x / (w - 1.0)
+            expected_sy = y / (h - 1.0)
+            result_sx, result_sy = self.Window.to_relative_pos(x, y)
+            assert result_sx == expected_sx
+            assert result_sy == expected_sy
