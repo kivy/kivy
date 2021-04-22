b40db493abef2a8c42ec769df14d35ea4cb3f3da
pythonic64 <pythonic64@gmail.com>
2021-04-22 15:09:13 +0200

    Restoring system_size with the same type in WindowBaseTest.test_to_normalize_pos method.

--- kivy/tests/test_window_base.py
+++ kivy/tests/test_window_base.py
@@ -6,14 +6,15 @@ from kivy.tests import GraphicUnitTest
 class WindowBaseTest(GraphicUnitTest):
 
     def test_to_normalized_pos(self):
-        old_system_size = self.Window.system_size[:]
-        self.Window.system_size = w, h = (320, 240)
+        win = self.Window
+        old_system_size = win.system_size[:]
+        win.system_size = w, h = type(old_system_size)((320, 240))
         try:
             for x, y in product([0, 319, 50, 51], [0, 239, 50, 51]):
                 expected_sx = x / (w - 1.0)
                 expected_sy = y / (h - 1.0)
-                result_sx, result_sy = self.Window.to_normalized_pos(x, y)
+                result_sx, result_sy = win.to_normalized_pos(x, y)
                 assert result_sx == expected_sx
                 assert result_sy == expected_sy
         finally:
-            self.Window.system_size = old_system_size
+            win.system_size = old_system_size
