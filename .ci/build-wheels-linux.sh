41fa25309001a2945e9d341c1b6330d1063fe1a0
Matthew Einhorn <matt@einhorn.dev>
2021-05-01 22:07:17 -0400

    Use pkg-config for harfbuzz paths [build wheel linux].

--- .ci/build-wheels-linux.sh
+++ .ci/build-wheels-linux.sh
@@ -3,7 +3,9 @@ set -e -x
 
 yum -y install  autoconf automake cmake gcc gcc-c++ git make pkgconfig zlib-devel portmidi portmidi-devel xorg-x11-server-devel mesa-libEGL-devel mtdev-devel mesa-libEGL freetype freetype-devel openjpeg openjpeg-devel libpng libpng-devel libtiff libtiff-devel libwebp libwebp-devel dbus-devel dbus ibus-devel ibus libsamplerate-devel libsamplerate systemd-devel libmodplug-devel libmodplug libvorbis-devel libvorbis flac-devel flac libjpeg-turbo-devel libjpeg-turbo wget glib2-devel cairo-devel
 mkdir ~/kivy_sources;
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$HOME/kivy_build/lib;
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$HOME/kivy_build/lib
+export PATH="$HOME/kivy_build/bin:$PATH"
+export PKG_CONFIG_PATH="$PKG_CONFIG_PATH:$HOME/kivy_build/lib/pkgconfig:$HOME/kivy_build/lib64/pkgconfig"
 
 cd ~/kivy_sources;
 wget http://www.libsdl.org/release/SDL2-2.0.14.tar.gz
@@ -18,8 +20,8 @@ cd ~/kivy_sources;
 wget http://www.libsdl.org/projects/SDL_mixer/release/SDL2_mixer-2.0.4.tar.gz;
 tar xzf SDL2_mixer-2.0.4.tar.gz;
 cd SDL2_mixer-2.0.4;
-PATH="$HOME/kivy_build/bin:$PATH" PKG_CONFIG_PATH="$HOME/kivy_build/lib/pkgconfig" ./configure --prefix="$HOME/kivy_build" --bindir="$HOME/kivy_build/bin" --enable-music-mod-modplug-shared=no --enable-music-mod-mikmod-shared=no --enable-music-midi-fluidsynth-shared=no --enable-music-ogg-shared=no --enable-music-flac-shared=no --enable-music-mp3-mpg123-shared=no;
-PATH="$HOME/kivy_build/bin:$PATH" make;
+./configure --prefix="$HOME/kivy_build" --bindir="$HOME/kivy_build/bin" --enable-music-mod-modplug-shared=no --enable-music-mod-mikmod-shared=no --enable-music-midi-fluidsynth-shared=no --enable-music-ogg-shared=no --enable-music-flac-shared=no --enable-music-mp3-mpg123-shared=no;
+make;
 make install;
 make distclean;
 
@@ -27,8 +29,8 @@ cd ~/kivy_sources;
 wget http://www.libsdl.org/projects/SDL_image/release/SDL2_image-2.0.5.tar.gz;
 tar xzf SDL2_image-2.0.5.tar.gz;
 cd SDL2_image-2.0.5;
-PATH="$HOME/kivy_build/bin:$PATH" PKG_CONFIG_PATH="$HOME/kivy_build/lib/pkgconfig" ./configure --prefix="$HOME/kivy_build" --bindir="$HOME/kivy_build/bin" --enable-png-shared=no --enable-jpg-shared=no --enable-tif-shared=no --enable-webp-shared=no;
-PATH="$HOME/kivy_build/bin:$PATH" make;
+./configure --prefix="$HOME/kivy_build" --bindir="$HOME/kivy_build/bin" --enable-png-shared=no --enable-jpg-shared=no --enable-tif-shared=no --enable-webp-shared=no;
+make;
 make install;
 make distclean;
 
@@ -45,7 +47,7 @@ harf_root="harfbuzz-2.8.0"
 wget "https://github.com/harfbuzz/harfbuzz/releases/download/2.8.0/$harf_root.tar.xz"
 tar xf "$harf_root.tar.xz"
 cd "$harf_root"
-meson setup build --wrap-mode=default --buildtype=release -Dglib=disabled -Dgobject=disabled -Dcairo=disabled -Dfreetype=enabled --prefix="$HOME/kivy_build" --bindir="$HOME/kivy_build/bin"
+meson setup build --wrap-mode=default --buildtype=release -Dglib=disabled -Dgobject=disabled -Dcairo=disabled -Dfreetype=enabled --prefix="$HOME/kivy_build" --bindir="$HOME/kivy_build/bin" --libdir="$HOME/kivy_build/lib"
 meson compile -C build
 meson install -C build
 
@@ -57,8 +59,8 @@ wget "https://github.com/libsdl-org/SDL_ttf/archive/$ttf_hash.tar.gz"
 tar xzf "$ttf_hash.tar.gz"
 cd "SDL_ttf-$ttf_hash"
 sed -si "s/\(define TTF_USE_HARFBUZZ\) 0/\1 1/" SDL_ttf.c
-env CFLAGS="$(pkg-config --cflags harfbuzz)" LDFLAGS="$(pkg-config --libs harfbuzz)" PATH="$HOME/kivy_build/bin:$PATH" PKG_CONFIG_PATH="$HOME/kivy_build/lib/pkgconfig" ./configure --prefix="$HOME/kivy_build" --bindir="$HOME/kivy_build/bin"
-PATH="$HOME/kivy_build/bin:$PATH" make
+env CFLAGS="$(pkg-config --cflags harfbuzz)" LDFLAGS="$(pkg-config --libs harfbuzz)" ./configure --prefix="$HOME/kivy_build" --bindir="$HOME/kivy_build/bin"
+make
 make install
 make distclean
 
@@ -67,7 +69,7 @@ for PYBIN in /opt/python/*3*/bin; do
     if [[ $PYBIN != *"34"* && $PYBIN != *"35"* ]]; then
         "${PYBIN}/pip" install --upgrade setuptools pip;
         "${PYBIN}/pip" install --upgrade cython nose pygments docutils;
-        USE_HARFBUZZ=1 KIVY_SPLIT_EXAMPLES=1 USE_X11=1 USE_SDL2=1 USE_PANGOFT2=0 USE_GSTREAMER=0 PKG_CONFIG_PATH="$HOME/kivy_build/lib/pkgconfig" "${PYBIN}/pip" wheel --no-deps . -w dist/;
+        USE_HARFBUZZ=1 KIVY_SPLIT_EXAMPLES=1 USE_X11=1 USE_SDL2=1 USE_PANGOFT2=0 USE_GSTREAMER=0 "${PYBIN}/pip" wheel --no-deps . -w dist/;
     fi
 done
 
