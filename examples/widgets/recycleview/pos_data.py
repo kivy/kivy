f44704bea27d60b97747d6f9b32639efbaf24d7e
Rakesh Peter <rakesh.peter@gmail.com>
2021-06-15 19:42:40 +0530

    Suggested Changes and few additions...
    1) Fixed Trailing Whitespaces / TextInputs multiline: False /  Popup size_hint removed / on_release for delete button / pop auto-dismiss removed 
    2) Sorting hopefully fixed as suggested. 
    3) Filter box added. Issues to be detailed.
    4) Very crude Select functionality. Probably better using Selectables

--- examples/widgets/recycleview/pos_data.py
+++ examples/widgets/recycleview/pos_data.py
@@ -7,7 +7,6 @@ from kivy.lang import Builder
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.popup import Popup
 from kivy.uix.label import Label
-from kivy.uix.textinput import TextInput
 
 kv = """
 #: import Clock kivy.clock.Clock
@@ -23,12 +22,11 @@ kv = """
     size_hint: None, None
     size: 400, 300
     title_size: 20
-    auto_dismiss: True
 
     BoxLayout:
         orientation: "vertical"
-        padding: dp(10)    
-        
+        padding: dp(10)
+
         GridLayout:
             id: container
             cols: 2
@@ -41,9 +39,9 @@ kv = """
             PopupLabelCell:
                 text: 'Date'
             GridLayout:
-                cols:3 
+                cols:3
                 Spinner:
-                    id: DD                    
+                    id: DD
                 Spinner:
                     id: MM
                     values: ('Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec')
@@ -55,6 +53,7 @@ kv = """
             TextInput:
                 id: customer
                 write_tab: False
+                multiline: False
                 on_focus: Clock.schedule_once(lambda dt: self.select_all()) if self.focus else None
             PopupLabelCell:
                 text: 'Length'
@@ -65,6 +64,7 @@ kv = """
                     halign: "center"
                     valign: "middle"
                     write_tab: False
+                    multiline: False
                     on_focus: Clock.schedule_once(lambda dt: self.select_all()) if self.focus else None
                 Spinner:
                     id: uom
@@ -72,10 +72,10 @@ kv = """
                     values: ('mm', 'cm', 'meter', 'inch', 'feet')
                 Button:
                     text: '-'
-                    on_press: length.text = str(int(length.text) - 1) if (int(length.text) - 1) > 0 else str(1)                
+                    on_press: length.text = str(int(length.text) - 1) if (int(length.text) - 1) > 0 else str(1)
                 Button:
                     text: '+'
-                    on_press: length.text = str(int(length.text) + 1)                 
+                    on_press: length.text = str(int(length.text) + 1)
             PopupLabelCell:
                 text: 'Quantity'
             GridLayout:
@@ -85,65 +85,74 @@ kv = """
                     halign: "center"
                     valign: "middle"
                     write_tab: False
+                    multiline: False
                     on_focus: Clock.schedule_once(lambda dt: self.select_all()) if self.focus else None
                 Label:
                     text: 'nos'
                 Button:
                     text: '-'
-                    on_press: qty.text = str(int(qty.text) - 1) if (int(qty.text) - 1) > 0 else str(1)                
+                    on_press: qty.text = str(int(qty.text) - 1) if (int(qty.text) - 1) > 0 else str(1)
                 Button:
                     text: '+'
                     on_press: qty.text = str(int(qty.text) + 1)
             PopupLabelCell:
                 text: 'Order ID'
-                opacity: 0                
+                opacity: 0
             TextInput:
                 id: orderno
                 disabled: True
                 opacity: 0
-                
         BoxLayout:
             Button:
-                size_hint: 1, 0.5
                 text: "Save Changes"
                 on_release:
                     app.root.save(orderno.text, DD.text, MM.text, YY.text, customer.text, length.text, uom.text, qty.text); root.dismiss()
             Button:
-                size_hint: 1, 0.5
                 text: "Cancel Changes"
-                on_release: app.root.remove(orderno.text); root.dismiss()
+                on_release: app.root.cancel(orderno.text); root.dismiss()
 
 <Row@RecycleKVIDsDataViewBehavior+BoxLayout>:
     canvas.before:
         Color:
-            rgba: 0.5, 0.5, 0.5, 1
+            rgba: 0.3, 0.3, 0.3, 1
         Rectangle:
             size: self.size
             pos: self.pos
-    orderno: ''    
+    orderno: ''
     Button:
-        on_press: app.root.remove(orderno.text)
         text: 'X'
         size_hint: 0.3, 1
+        on_release: app.root.remove(orderno.text)
+        background_color: (.9,.9,.9,1) if (app.root.is_selected(orderno.text)) else (.3,.3,.3,1) if  (orderno.text != "" and int(orderno.text)%2) == 0 else (.5,.5,.5,1)
     Button:
-        on_press: app.root.edit(orderno.text)
         text: 'Edit'
         size_hint: 0.3, 1
+        on_release: app.root.edit(orderno.text)
+        background_color: (.9,.9,.9,1) if (app.root.is_selected(orderno.text)) else (.3,.3,.3,1) if (orderno.text != "" and int(orderno.text)%2) == 0 else (.5,.5,.5,1)
     Button:
         id: selectitem
         text: 'Select'
         size_hint: 0.3, 1
-        on_press: print("Selected item:" + orderno.text)
-    Label:
+        on_press: app.root.select(orderno.text);
+        background_color: (.9,.9,.9,1) if  (app.root.is_selected(orderno.text)) else (.3,.3,.3,1) if (orderno.text != "" and int(orderno.text)%2) == 0 else (.5,.5,.5,1)
+    Button:
         id: orderno
-        text: root.orderno      
-    Label:
-        id: date        
-    Label:
-        id: customer        
-    Label:
-        id: qty    
-<OrderHandler>:    
+        text: root.orderno
+        on_release: app.root.edit(orderno.text)
+        background_color: (.9,.9,.9,1) if (app.root.is_selected(orderno.text)) else (.3,.3,.3,1) if (orderno.text != "" and int(orderno.text)%2) == 0 else (.5,.5,.5,1)
+    Button:
+        id: date
+        on_release: app.root.edit(orderno.text)
+        background_color: (.9,.9,.9,1) if (app.root.is_selected(orderno.text)) else (.3,.3,.3,1) if (orderno.text != "" and int(orderno.text)%2) == 0 else (.5,.5,.5,1)
+    Button:
+        id: customer
+        on_release: app.root.edit(orderno.text)
+        background_color: (.9,.9,.9,1) if (app.root.is_selected(orderno.text)) else (.3,.3,.3,1) if  (orderno.text != "" and int(orderno.text)%2) == 0 else (.5,.5,.5,1)
+    Button:
+        id: qty
+        on_release: app.root.edit(orderno.text)
+        background_color: (.9,.9,.9,1) if (app.root.is_selected(orderno.text)) else (.3,.3,.3,1) if (orderno.text != "" and int(orderno.text)%2) == 0 else (.5,.5,.5,1)
+<OrderHandler>:
     canvas:
         Color:
             rgba: 0.3, 0.3, 0.3, 1
@@ -153,10 +162,30 @@ kv = """
     rv: rv
     orientation: 'vertical'
     GridLayout:
+        rows:1
+        cols:3
+        size_hint_y:  None
+        height: dp(50)
+        padding: dp(8)
+        spacing: dp(16)
+        Label:
+            text: 'Filter Text:'
+            size_hint_x: 0.2
+        TextInput:
+            id: filter
+            on_focus: Clock.schedule_once(lambda dt: self.select_all()) if self.focus else None
+            on_text_validate: app.root.filter(self.text)
+            multiline: False
+            write_tab: False
+        Button:
+            text:'X'
+            size_hint_x: 0.2
+            on_release: filter.text=""; app.root.update_view()
+    GridLayout:
         cols: 6
         rows: 1
         size_hint_y: None
-        height: dp(54)
+        height: dp(50)
         padding: dp(8)
         spacing: dp(16)
         Button:
@@ -164,22 +193,21 @@ kv = """
             on_press: root.add()
         Button:
             text: 'Sort by Order ID'
-            on_press: root.sort('orderno')        
+            on_press: root.sort('orderno')
         Button:
             text: 'Sort by Date'
-            on_press: root.sort('date')            
+            on_press: root.sort('date')
         Button:
             text: 'Sort by Customer'
             on_press: root.sort('customer')
         Button:
             text: 'Clear'
-            size_hint: 0.7, 1            
-            on_press: root.clear()        
+            size_hint: 0.7, 1
+            on_press: root.clear()
         Button:
             text: 'Exit'
             size_hint: 0.5, 1
             on_press: app.stop()
-        
 
     RecycleView:
         id: rv
@@ -210,107 +238,143 @@ class EditStatePopup(Popup):
     def populate_content(self, order):
         months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec']
         self.ids.orderno.text = str(order['orderno'])
+
+        #TODO: Date check logic
         if(order['date.text']):
             self.ids.DD.text = str(order['date.text']).split('-')[0]
             self.ids.DD.values = [str(dd) for dd in range(1,32,1)]
             self.ids.MM.text = str(order['date.text']).split('-')[1]
             self.ids.MM.values = months
             self.ids.YY.text = str(order['date.text']).split('-')[2]
-            self.ids.YY.values = [str(yy) for yy in range(2020,2031,1)] 
+            self.ids.YY.values = [str(yy) for yy in range(2020,2031,1)]
         if(order['customer.text']):
             self.ids.customer.text = str(order['customer.text'])
         if(order['qty.text']):
             self.ids.length.text = "".join(re.findall("[0-9]+", str(order['qty.text'].split('x')[0])))
             self.ids.uom.text = "".join(re.findall("[a-zA-Z]+", str(order['qty.text'].split('x')[0])))
             self.ids.qty.text = "".join(re.findall("[0-9]+", str(order['qty.text'].split('x')[1])))
-    
+
 class OrderHandler(BoxLayout):
 
     sorted_customer = False
     sorted_orderno = False
     sorted_date = False
-    
+
     def __init__(self, **kwargs):
         super(OrderHandler, self).__init__(**kwargs)
-        self.populate_random()        
-        self.sort('orderno')
-        self.sort('orderno')
-    
+        self.itemList = []
+        self.selectList = []
+        self.newitemFlag = False
+        self.populate_random()
+
     def populate_random(self):
-        self.rv.data = [
+        self.itemList = [
             {'date.text': ''.join((datetime.date.today() - datetime.timedelta(days=randint(0,30))).strftime('%d-%b-%Y')),
              'customer.text': ''.join(sample(ascii_lowercase, 6)),
              'orderno':str(x+1),
-             'qty.text': str(randint(1,100)) + 'mm x ' + str(randint(1,50)) + 'nos' }             
+             'qty.text': str(randint(1,100)) + 'mm x ' + str(randint(1,50)) + 'nos' }
             for x in range(50)]
+        print("Populating " + str(len(self.itemList)) + " items to list... ")
+        self.update_view()
 
-    def sort(self, key):        
-        if key == 'customer':
-            self.rv.data = sorted(self.rv.data, key=lambda x: x['customer.text'], reverse = self.sorted_customer)
-            self.sorted_customer = not self.sorted_customer
-        elif key == 'orderno':
-            self.rv.data = sorted(self.rv.data, key=lambda x: int(x['orderno']), reverse = self.sorted_orderno)
-            self.sorted_orderno = not self.sorted_orderno
-        elif key == 'date':
-            self.rv.data = sorted(self.rv.data, key=lambda x: x['date.text'], reverse = self.sorted_date)
-            self.sorted_date = not self.sorted_date
+    def update_view(self):
+        if self.itemList:
+            self.rv.data = self.itemList.copy()
+            self.sorted_orderno = True
+            self.sort('orderno')
+            print("Updating view..")
 
     def clear(self):
         self.rv.data = []
 
     def add(self):
-        if self.rv.data:
-            if (self.sorted_orderno):
-                self.sort('orderno')
-            else: 
-                self.sort('orderno')
-                self.sort('orderno')
-            
-            lastorderno = self.rv.data[0].get('orderno')    
-            print("Last order number:" + lastorderno)
-        else:
-            lastorderno = 0
-        
-        self.rv.data.insert(0, 
-            {'date.text': ''.join(datetime.date.today().strftime('%d-%b-%Y')),
-             'customer.text': '',
-             'orderno':str(int(lastorderno)+1),
-             'qty.text': '10mm x 1nos' })
-        
-        self.edit(int(lastorderno)+1)
+        if self.itemList:
+            id = max(int(d['orderno']) for d in self.itemList) + 1
+            self.itemList.insert(0,
+                {'date.text': ''.join(datetime.date.today().strftime('%d-%b-%Y')),
+                'customer.text': '',
+                'orderno':str(id),
+                'qty.text': '10mm x 1nos' })
+            self.newitemFlag = True
+            self.edit(id)
 
-    def update(self, value):
-        if self.rv.data:
-            self.rv.refresh_from_data()
-            
     def edit(self, id):
-        if self.rv.data:
-            list = self.rv.data.copy()            
+        if self.itemList:
+            list = self.itemList.copy()
             list[:] = [d for d in list if d.get('orderno') == str(id)]
             print("Editing order: " + str(list[0]))
             if (list[0]):
                 popup = EditStatePopup(list[0])
-                popup.open()            
-            
-    def save(self, orderno, DD, MM, YY, customer, length, uom, qty): 
+                popup.open()
+
+    def remove(self, id):
         if self.rv.data:
-            print ("Adding order: " + orderno + " |  " + DD + "-" + MM  + "-" + YY + " | " + customer  + " | " + length +  uom  + " x " + qty + "nos")
+            if(len(self.selectList) > 0):
+                list = self.selectList.copy()
+                self.selectList = []
+                self.sel_remove(list)
+            if (self.newitemFlag):
+                self.newitemFlag = False
+                print ("Updating Order id... " + str(id))
+                list = self.itemList.copy()
+            else:
+                print ("Removing Order id... " + str(id))
+                list = self.rv.data.copy()
+            list[:] = [d for d in list if d.get('orderno') != str(id)]
+            self.rv.data = list
+            self.itemList = list.copy()
+            self.rv.refresh_from_data()
+
+    def sel_remove(self, list):
+        for item in list:
+            self.remove(item)
+
+    def cancel(self, id):
+        if (self.newitemFlag):
+            self.remove(id)
+        self.update_view()
+
+    def save(self, orderno, DD, MM, YY, customer, length, uom, qty):
+        if self.itemList:
             self.remove(orderno)
-            self.rv.data.insert(0,
+            print ("Adding order: " + orderno + " |  " + DD + "-" + MM  + "-" + YY + " | " + customer  + " | " + length +  uom  + " x " + qty + "nos")
+            self.itemList.insert(0,
                 {'date.text': DD + "-" + MM + "-" + YY,
                  'customer.text': customer.strip(),
                  'orderno': str(orderno),
-                 'qty.text': length.strip() + uom + " x " + qty.strip() + "nos"})        
-            self.rv.refresh_from_data()
-            
-    def remove(self, id):        
+                 'qty.text': length.strip() + uom + " x " + qty.strip() + "nos"})
+            self.update_view()
+
+    def select(self, id):
+        if id in self.selectList:
+            self.selectList.remove(id)
+        else: self.selectList.append(id)
+        print("Selected List:" + str(self.selectList))
+        self.rv.refresh_from_data()
+
+    def is_selected(self, id):
+        if id in self.selectList:
+            return True
+
+    def sort(self, key):
         if self.rv.data:
-            print ("Removing Order id... " + str(id))
-            list = self.rv.data.copy()            
-            list[:] = [d for d in list if d.get('orderno') != str(id)]
+            if key == 'customer':
+                self.rv.data = sorted(self.rv.data, key=lambda x: x['customer.text'], reverse = self.sorted_customer)
+                self.sorted_customer = not self.sorted_customer
+            elif key == 'orderno':
+                self.rv.data = sorted(self.rv.data, key=lambda x: int(x['orderno']), reverse = self.sorted_orderno)
+                self.sorted_orderno = not self.sorted_orderno
+            elif key == 'date':
+                self.rv.data = sorted(self.rv.data, key=lambda x: x['date.text'], reverse = self.sorted_date)
+                self.sorted_date = not self.sorted_date
+
+    def filter(self, key):
+        if self.rv.data:
+            print("Filtering: " + key)
+            list = self.itemList.copy()
+            list[:] = [d for d in list if str(key) in "".join(d.values())]
             self.rv.data = list
             self.rv.refresh_from_data()
-            
 
 class OrderHandlerApp(App):
     def build(self):
