69bff5053342473cbe369a290561409c6ebc849b
SlappedWithSilence <83423830+SlappedWithSilence@users.noreply.github.com>
2021-10-24 12:12:23 -0500

    Fixing error in comment
    Co-authored-by: Gabriel Pettier <gabriel.pettier@gmail.com>

--- kivy/uix/image.py
+++ kivy/uix/image.py
@@ -360,7 +360,7 @@ class AsyncImage(Image):
     '''
 
     extra_headers = DictProperty(None, allow_none=True)
-    '''If this property is not set to none, any remote requests made will contain the headers stored in this dict.
+    '''If this property is not set to None, any remote requests made will contain the headers stored in this dict.
 
     :attr:`extra_headers` is a :class:`~kivy.properties.DictProperty` and defaults
     to None.
