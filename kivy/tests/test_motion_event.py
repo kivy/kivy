53f3ee0a6fdbe4ffd4b802eda53622654b7a9883
pythonic64 <pythonic64@gmail.com>
2021-10-31 09:22:56 +0100

    TestMotionEvent: Fixed assert message in test_to_absolute_pos method.

--- kivy/tests/test_motion_event.py
+++ kivy/tests/test_motion_event.py
@@ -25,17 +25,17 @@ class TestMotionEvent:
             yield x / x_max, y / y_max, x_max, y_max, 270, result
 
     def test_to_absolute_pos(self):
-        event = DummyMotionEvent('fake', 'fake1', (0, 0))
+        event = DummyMotionEvent('dummy', 'dummy1', (0, 0))
         for item in self.build_to_absolute_pos_data(320, 240, 20, 21):
             args = item[:-1]
             expected_x, expected_y = item[-1]
             x, y = event.to_absolute_pos(*args)
             message = ('For args {} expected ({}, {}), got ({}, {})'
-                       .format(args, x, y, expected_x, expected_y))
+                       .format(args, expected_x, expected_y, x, y))
             correct = isclose(x, expected_x) and isclose(y, expected_y)
             assert correct, message
 
     def test_to_absolute_pos_error(self):
-        event = DummyMotionEvent('fake', 'fake1', (0, 0))
+        event = DummyMotionEvent('dummy', 'dummy1', (0, 0))
         with pytest.raises(ValueError):
             event.to_absolute_pos(0, 0, 100, 100, 10)
