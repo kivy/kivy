kivy/kivy

commit e8725996ac8cc8d533ba91f1e8325b74928d0b7d

MotionEvent: Fix scale_for_screen method (#7659)
* Fixed scale_for_screen to compute all absolute coords from normalized coords.
* Updating dz only if argument p is passed.
* Updated doc of to_absolute_pos method.
* Renamed arguments of to_absolute_pos method.
* Removed default value for rotation in to_absolute_pos method.
* Minor updated in scale_for_scren and doc update in to_absolute_pos method.
* Minor updated in scale_for_scren and doc update in to_absolute_pos method.I
* Fixed to_absolute_pos for case when rotation is 270 degrees.
* Simplified offset calculation for "scale" keyboard mode in scale_for_screen method.
* Added test_motion_event.py module.
  * Fixed assert message in test_to_absolute_pos method.
  * Added create_dummy_motion_event method.

kivy/input/motionevent.py
@@ -364,45 +364,54 @@ class MotionEvent(MotionEventBase):
             Max value for `x`, `y` and `z` is changed respectively to `w` - 1,
             `h` - 1 and `p` - 1.
         '''
-        x_max, y_max = max(0.0, w - 1.0), max(0.0, h - 1.0)
-        sx, sy = self.sx, self.sy
-        if rotation == 0:
-            self.x = sx * x_max
-            self.y = sy * y_max
-        elif rotation == 90:
-            sx, sy = sy, 1 - sx
-            self.x = sx * y_max
-            self.y = sy * x_max
-        elif rotation == 180:
-            sx, sy = 1 - sx, 1 - sy
-            self.x = sx * x_max
-            self.y = sy * y_max
-        elif rotation == 270:
-            sx, sy = 1 - sy, sx
-            self.x = sx * y_max
-            self.y = sy * x_max
-
+        x_max, y_max = max(0, w - 1), max(0, h - 1)
+        absolute = self.to_absolute_pos
+        self.x, self.y = absolute(self.sx, self.sy, x_max, y_max, rotation)
+        self.ox, self.oy = absolute(self.osx, self.osy, x_max, y_max, rotation)
+        self.px, self.py = absolute(self.psx, self.psy, x_max, y_max, rotation)
+        # Update z values
         if p is not None:
-            self.z = self.sz * max(0.0, p - 1.0)
-
+            z_max = max(0, p - 1)
+            self.z = self.sz * z_max
+            self.oz = self.osz * z_max
+            self.pz = self.psz * z_max
+            self.dz = self.z - self.pz
         if smode:
+            # Adjust y for keyboard height
             if smode == 'pan':
                 self.y -= kheight
+                self.oy -= kheight
+                self.py -= kheight
             elif smode == 'scale':
-                self.y += (kheight * (
-                    (self.y - kheight) / (h - kheight))) - kheight
-
-        if self.ox is None:
-            self.px = self.ox = self.x
-            self.py = self.oy = self.y
-            self.pz = self.oz = self.z
-
+                offset = kheight * (self.y - h) / (h - kheight)
+                self.y += offset
+                self.oy += offset
+                self.py += offset
+        # Update delta for x and y
         self.dx = self.x - self.px
         self.dy = self.y - self.py
-        self.dz = self.z - self.pz
-
-        # cache position
+        # Cache position
         self.pos = self.x, self.y
+
+    def to_absolute_pos(self, nx, ny, x_max, y_max, rotation):
+        '''Transforms normalized (0-1) coordinates `nx` and `ny` to absolute
+        coordinates using `x_max`, `y_max` and `rotation`.
+
+        :raises:
+            `ValueError`: If `rotation` is not one of: 0, 90, 180 or 270
+
+        .. versionadded:: 2.1.0
+        '''
+        if rotation == 0:
+            return nx * x_max, ny * y_max
+        elif rotation == 90:
+            return ny * y_max, (1 - nx) * x_max
+        elif rotation == 180:
+            return (1 - nx) * x_max, (1 - ny) * y_max
+        elif rotation == 270:
+            return (1 - ny) * y_max, nx * x_max
+        raise ValueError('Invalid rotation %s, '
+                         'valid values are 0, 90, 180 or 270' % rotation)
 
     def push(self, attrs=None):
         '''Push attribute values in `attrs` onto the stack

kivy/tests/test_motion_event.py
@@ -1,0 +1,44 @@
+import pytest
+
+from kivy.compat import isclose
+from kivy.input import MotionEvent
+
+
+class DummyMotionEvent(MotionEvent):
+    pass
+
+
+class TestMotionEvent:
+
+    def create_dummy_motion_event(self):
+        return DummyMotionEvent('dummy', 'dummy1', (0, 0))
+
+    def build_to_absolute_pos_data(self, x_max, y_max, x_step, y_step):
+        for x, y in zip(range(0, x_max, x_step), range(0, y_max, y_step)):
+            result = (x, y)
+            yield x / x_max, y / y_max, x_max, y_max, 0, result
+        for x, y, in zip(range(0, x_max, x_step), range(0, y_max, y_step)):
+            result = (y, x_max - x)
+            yield x / x_max, y / y_max, x_max, y_max, 90, result
+        for x, y, in zip(range(0, x_max, x_step), range(0, y_max, y_step)):
+            result = (x_max - x, y_max - y)
+            yield x / x_max, y / y_max, x_max, y_max, 180, result
+        for x, y, in zip(range(0, x_max, x_step), range(0, y_max, y_step)):
+            result = (y_max - y, x)
+            yield x / x_max, y / y_max, x_max, y_max, 270, result
+
+    def test_to_absolute_pos(self):
+        event = self.create_dummy_motion_event()
+        for item in self.build_to_absolute_pos_data(320, 240, 20, 21):
+            args = item[:-1]
+            expected_x, expected_y = item[-1]
+            x, y = event.to_absolute_pos(*args)
+            message = ('For args {} expected ({}, {}), got ({}, {})'
+                       .format(args, expected_x, expected_y, x, y))
+            correct = isclose(x, expected_x) and isclose(y, expected_y)
+            assert correct, message
+
+    def test_to_absolute_pos_error(self):
+        event = self.create_dummy_motion_event()
+        with pytest.raises(ValueError):
+            event.to_absolute_pos(0, 0, 100, 100, 10)

commit 6172c5c04b5a0f79e28268d9f281eeac51c61763

Fix Scatter rotation and scaling of child widgets when using multitouch
Instead of calculating the origin and previous position every time the "scale_for_screen"
method is called it is only calculated when a "on_touch_down" event is triggered

This bug was introduced in: e8725996ac8cc8d533ba91f1e8325b74928d0b7d

The method "transform_with_touch" in "Scatter" is relying on the "touch.ppos" to only get updated
when the screen is initially touched and not when the finger is moved

Fixes #7685

kivy/input/motionevent.py
@@ -367,26 +367,35 @@ class MotionEvent(MotionEventBase):
         x_max, y_max = max(0, w - 1), max(0, h - 1)
         absolute = self.to_absolute_pos
         self.x, self.y = absolute(self.sx, self.sy, x_max, y_max, rotation)
-        self.ox, self.oy = absolute(self.osx, self.osy, x_max, y_max, rotation)
-        self.px, self.py = absolute(self.psx, self.psy, x_max, y_max, rotation)
+        opos_ppos_updated = False
+        if self.ox is None:
+            self.ox, self.oy = absolute(self.osx, self.osy, x_max, y_max,
+                                        rotation)
+            self.px, self.py = absolute(self.psx, self.psy, x_max, y_max,
+                                        rotation)
+            opos_ppos_updated = True
         # Update z values
         if p is not None:
             z_max = max(0, p - 1)
             self.z = self.sz * z_max
-            self.oz = self.osz * z_max
-            self.pz = self.psz * z_max
+            if self.oz is None:
+                self.oz = self.osz * z_max
+            if self.pz is None:
+                self.pz = self.psz * z_max
             self.dz = self.z - self.pz
         if smode:
             # Adjust y for keyboard height
             if smode == 'pan':
                 self.y -= kheight
-                self.oy -= kheight
-                self.py -= kheight
+                if opos_ppos_updated:
+                    self.oy -= kheight
+                    self.py -= kheight
             elif smode == 'scale':
                 offset = kheight * (self.y - h) / (h - kheight)
                 self.y += offset
-                self.oy += offset
-                self.py += offset
+                if opos_ppos_updated:
+                    self.oy += offset
+                    self.py += offset
         # Update delta for x and y
         self.dx = self.x - self.px
         self.dy = self.y - self.py