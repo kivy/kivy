fbf561f73ddba9941b1b7e771f86264c6e6eef36
Gabriel Pettier <gabriel.pettier@gmail.com>
2022-01-12 18:26:20 +0100

    Add python3.10 in the ci configuration [build wheel linux] [build wheel osx] [build wheel win] (#7663)
    * Add python3.10 in the ci configuration
    
    * Update setup.cfg and setup.py for python3.10
    
    * also update pyproject.toml to allow using latest cython
    
    * update kivy-deps.sdl2 required version in setup.cfg
    
    * bump kivy-deps.sdl2 version
    
    * use develop version of pyinstaller for python3.10 support
    
    * osx try simplified build
    
    * force usage of next funcparserlib release that doesnt use 2to3 in setup.py
    
    * 3.6 reached EOL on 2021-12-23
    
    * macOS: add cp310-macosx_universal2
    
    * We don't install anymore the full version. So ffmpeg dependency is not needed anymore. I expect to have a universal2 whl for ffpyplayer when we will switch back to full,dev
    
    * manylinux2010 --> manylinux2014 (via cibuildwheel)
    
    * Remove dependencies, so we can check delocating. Installs dev version cause support for 3.10 in ffpyplayer is missing
    
    * Update manylinux sdl2 version and max cython version.
    
    * Bump win deps.
    
    Co-authored-by: Mirko Galimberti <me@mirkogalimberti.com>
    Co-authored-by: Matt Einhorn <moiein2000@gmail.com>
    Co-authored-by: Matthew Einhorn <matt@einhorn.dev>

--- setup.py
+++ setup.py
@@ -1089,6 +1089,7 @@ if not build_examples:
             'Programming Language :: Python :: 3.7',
             'Programming Language :: Python :: 3.8',
             'Programming Language :: Python :: 3.9',
+            'Programming Language :: Python :: 3.10',
             'Topic :: Artistic Software',
             'Topic :: Games/Entertainment',
             'Topic :: Multimedia :: Graphics :: 3D Rendering',
