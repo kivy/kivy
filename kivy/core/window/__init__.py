542a813f9854db6733ea982abf98063e2bcb6452
Filip Radović <pythonic64@gmail.com>
2022-02-02 22:36:09 +0100

    WindowBase: Don't return motion event in transform_motion_event_2d method. (#7778)

--- kivy/core/window/__init__.py
+++ kivy/core/window/__init__.py
@@ -1543,8 +1543,8 @@ class WindowBase(EventDispatcher):
         )
 
     def transform_motion_event_2d(self, me, widget=None):
-        '''Returns transformed motion event `me` to this window size and then
-        if `widget` is passed transforms `me` to `widget`'s local coordinates.
+        '''Transforms the motion event `me` to this window size and then if
+        `widget` is passed transforms `me` to `widget`'s local coordinates.
 
         :raises:
             `AttributeError`: If widget's ancestor is ``None``.
@@ -1564,22 +1564,20 @@ class WindowBase(EventDispatcher):
             smode=self.softinput_mode,
             kheight=self.keyboard_height
         )
-        if widget is None:
-            return me
-        parent = widget.parent
-        try:
-            if parent:
-                me.apply_transform_2d(parent.to_widget)
-            else:
-                me.apply_transform_2d(widget.to_widget)
-                me.apply_transform_2d(widget.to_parent)
-        except AttributeError:
-            # when using inner window, an app have grab the touch
-            # but app is removed. The touch can't access
-            # to one of the parent. (i.e, self.parent will be None)
-            # and BAM the bug happen.
-            raise
-        return me
+        if widget is not None:
+            parent = widget.parent
+            try:
+                if parent:
+                    me.apply_transform_2d(parent.to_widget)
+                else:
+                    me.apply_transform_2d(widget.to_widget)
+                    me.apply_transform_2d(widget.to_parent)
+            except AttributeError:
+                # when using inner window, an app have grab the touch
+                # but app is removed. The touch can't access
+                # to one of the parent. (i.e, self.parent will be None)
+                # and BAM the bug happen.
+                raise
 
     def _apply_transform(self, m):
         return m
