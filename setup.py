3b17393563ad02a1f9f614b25deb0d30885e4395
Alexander Lais <alexander.lais@gmail.com>
2022-02-16 22:46:01 +0100

    Update setup.py
    Co-authored-by: Matt Einhorn <matt@einhorn.dev>

--- setup.py
+++ setup.py
@@ -114,12 +114,8 @@ if kivy_ios_root is not None:
 if exists('/opt/vc/include/bcm_host.h'):
     used_pi_version = pi_version
     # Force detected Raspberry Pi version for cross-builds, if needed
-    if 'FORCE_RPI_VERSION' in environ:
-        try:
-            used_pi_version = int(environ.get('FORCE_RPI_VERSION'))
-        except:
-            print("Could not override Raspberry Pi version "
-                 f"to {environ.get('FORCE_RPI_VERSION')}")
+    if 'KIVY_RPI_VERSION' in environ:
+        used_pi_version = int(environ['KIVY_RPI_VERSION'])
     # The proprietary broadcom video core drivers are not available on the
     # Raspberry Pi 4
     if (used_pi_version or 4) < 4:
