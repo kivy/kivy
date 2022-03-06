bab3352fd14f33dfaf982e2e5b012482b7ef6124
Mirko Galimberti <me@mirkogalimberti.com>
2022-03-06 16:28:16 +0100

    Bump to 2.2.0.dev0

--- kivy/_version.py
+++ kivy/_version.py
@@ -1,16 +1,16 @@
 # This file is imported from __init__.py and exec'd from setup.py
 
 MAJOR = 2
-MINOR = 1
+MINOR = 2
 MICRO = 0
-RELEASE = True
+RELEASE = False
 
 __version__ = '%d.%d.%d' % (MAJOR, MINOR, MICRO)
 
 if not RELEASE:
     # if it's a rcx release, it's not proceeded by a period. If it is a
     # devx release, it must start with a period
-    __version__ += ''
+    __version__ += '.dev0'
 
 
 _kivy_git_hash = ''
