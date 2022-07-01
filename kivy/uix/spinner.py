f8f0013d20ce36dc29c4c062d9be46a493615800
AnthonyZimmermann <anthony.zimmermann@protonmail.com>
2022-07-01 08:13:02 +0200

    Implement TextInputSpinner
    Implement a TextInputSpinner class which basically is a spinner with
    a TextInput instead of a button. This allows for user input of values
    that are not in the set of predefined options.
    Furthermore, a filtering function has been implemented which filters the
    options based on the current input. The implementation is flexible and
    allows developers to implement their custom filter function.
    Implements tests for the filtering feature.

--- kivy/uix/spinner.py
+++ kivy/uix/spinner.py
@@ -7,215 +7,91 @@ Spinner
 .. image:: images/spinner.jpg
     :align: right
 
-Spinner is a widget that provides a quick way to select one value from a set.
+:class:`~kivy.uix.spinner.Spinner` is a widget that provides a quick way to
+select one value from a set.
 In the default state, a spinner shows its currently selected value.
 Touching the spinner displays a dropdown menu with all the other available
 values from which the user can select a new one.
 
+:class:`~kivy.uix.spinner.TextInputSpinner` is another variant of a spinner
+which provides a :class:`~kivy.uix.textinput.TextInput` as main visual input
+element. In the default state, the :class:`~kivy.uix.spinner.TextInputSpinner`
+appears like a usual :class:`~kivy.uix.textinput.TextInput` input element.
+Interacting with the spinner by touching or typing displays the dropdown menu
+as for a :class:`~kivy.uix.spinner.Spinner`.
+
 Example::
 
     from kivy.base import runTouchApp
+    from kivy.uix.boxlayout import BoxLayout
     from kivy.uix.spinner import Spinner
+    from kivy.uix.spinner import TextInputSpinner
 
     spinner = Spinner(
         # default value shown
         text='Home',
         # available values
         values=('Home', 'Work', 'Other', 'Custom'),
-        # just for positioning in our example
+    )
+
+    textinput_spinner = TextInputSpinner(
+        # default value shown
+        text='',
+        values=('One', 'Two', 'Three', '_THREE_'),
+    )
+
+    boxlayout = BoxLayout(
+        # just for positioning of the example
+        orientation='vertical',
         size_hint=(None, None),
-        size=(100, 44),
-        pos_hint={'center_x': .5, 'center_y': .5})
+        size=(100, 88),
+        pos_hint={'center_x': .5, 'center_y': .5},
+    )
 
     def show_selected_value(spinner, text):
-        print('The spinner', spinner, 'has text', text)
+        print('The spinner', spinner.__class__.__name__, 'has text', text)
 
     spinner.bind(text=show_selected_value)
+    textinput_spinner.bind(text=show_selected_value)
 
-    runTouchApp(spinner)
+    runTouchApp(boxlayout)
 
 
 Kv Example::
 
-    FloatLayout:
+    BoxLayout:
+        orientation='vertical'
+        size_hint: None, None
+        size: 100, 88
+        pos_hint: {'center': (.5, .5)}
+
         Spinner:
-            size_hint: None, None
-            size: 100, 44
-            pos_hint: {'center': (.5, .5)}
             text: 'Home'
             values: 'Home', 'Work', 'Other', 'Custom'
             on_text:
                 print("The spinner {} has text {}".format(self, self.text))
+
+        TextInputSpinner:
+            text: 'Home'
+            values: 'One', 'Two', 'Three', '_THREE_'
+            on_text:
+                print("The spinner {} has text {}".format(self, self.text))
 '''
 
-__all__ = ('Spinner', 'SpinnerOption')
+__all__ = ('Spinner', 'SpinnerOption', 'TextInputSpinner')
 
-from kivy.uix.button import Button
+from kivy.properties import BooleanProperty
 from kivy.uix.behaviors import SpinnerBehavior
+from kivy.uix.button import Button
+from kivy.uix.textinput import TextInput
 
 
 class SpinnerOption(Button):
-    '''Special button used in the :class:`Spinner` dropdown list. By default,
-    this is just a :class:`~kivy.uix.button.Button` with a size_hint_y of None
-    and a height of :meth:`48dp <kivy.metrics.dp>`.
+    '''Special button used in the :class:`~kivy.uix.spinner.Spinner` dropdown
+    list. By default, this is just a :class:`~kivy.uix.button.Button` with a
+    size_hint_y of None and a height of :meth:`48dp <kivy.metrics.dp>`.
     '''
     pass
-
-
-class SpinnerBehavior(object):
-    '''Spinner class, see module documentation for more information.
-    '''
-
-    values = ListProperty()
-    '''Values that can be selected by the user. It must be a list of strings.
-
-    :attr:`values` is a :class:`~kivy.properties.ListProperty` and defaults to
-    [].
-    '''
-
-    text_autoupdate = BooleanProperty(False)
-    '''Indicates if the spinner's :attr:`text` should be automatically
-    updated with the first value of the :attr:`values` property.
-    Setting it to True will cause the spinner to update its :attr:`text`
-    property every time attr:`values` are changed.
-
-    .. versionadded:: 1.10.0
-
-    :attr:`text_autoupdate` is a :class:`~kivy.properties.BooleanProperty` and
-    defaults to False.
-    '''
-
-    option_cls = ObjectProperty(SpinnerOption)
-    '''Class used to display the options within the dropdown list displayed
-    under the Spinner. The `text` property of the class will be used to
-    represent the value.
-
-    The option class requires:
-
-    - a `text` property, used to display the value.
-    - an `on_release` event, used to trigger the option when pressed/touched.
-    - a :attr:`~kivy.uix.widget.Widget.size_hint_y` of None.
-    - the :attr:`~kivy.uix.widget.Widget.height` to be set.
-
-    :attr:`option_cls` is an :class:`~kivy.properties.ObjectProperty` and
-    defaults to :class:`SpinnerOption`.
-
-    .. versionchanged:: 1.8.0
-        If you set a string, the :class:`~kivy.factory.Factory` will be used to
-        resolve the class.
-
-    '''
-
-    dropdown_cls = ObjectProperty(DropDown)
-    '''Class used to display the dropdown list when the Spinner is pressed.
-
-    :attr:`dropdown_cls` is an :class:`~kivy.properties.ObjectProperty` and
-    defaults to :class:`~kivy.uix.dropdown.DropDown`.
-
-    .. versionchanged:: 1.8.0
-        If set to a string, the :class:`~kivy.factory.Factory` will be used to
-        resolve the class name.
-
-    '''
-
-    is_open = BooleanProperty(False)
-    '''By default, the spinner is not open. Set to True to open it.
-
-    :attr:`is_open` is a :class:`~kivy.properties.BooleanProperty` and
-    defaults to False.
-
-    .. versionadded:: 1.4.0
-    '''
-
-    sync_height = BooleanProperty(False)
-    '''Each element in a dropdown list uses a default/user-supplied height.
-    Set to True to propagate the Spinner's height value to each dropdown
-    list element.
-
-    .. versionadded:: 1.10.0
-
-    :attr:`sync_height` is a :class:`~kivy.properties.BooleanProperty` and
-    defaults to False.
-    '''
-
-    def __init__(self, **kwargs):
-        self._dropdown = None
-        super().__init__()
-        fbind = self.fbind
-        build_dropdown = self._build_dropdown
-        update_dropdown = self._update_dropdown
-        fbind('dropdown_cls', build_dropdown)
-        fbind('option_cls', build_dropdown)
-        fbind('values', update_dropdown)
-        fbind('size', self._update_dropdown_size)
-        fbind('text_autoupdate', update_dropdown)
-        build_dropdown()
-
-    def _build_dropdown(self, *largs):
-        if self._dropdown:
-            self._dropdown.unbind(on_select=self._on_dropdown_select)
-            self._dropdown.unbind(on_dismiss=self._close_dropdown)
-            self._dropdown.dismiss()
-            self._dropdown = None
-        cls = self.dropdown_cls
-        if isinstance(cls, string_types):
-            cls = Factory.get(cls)
-        self._dropdown = cls()
-        self._dropdown.bind(on_select=self._on_dropdown_select)
-        self._dropdown.bind(on_dismiss=self._close_dropdown)
-        self._update_dropdown()
-
-    def _update_dropdown_size(self, *largs):
-        if not self.sync_height:
-            return
-        dp = self._dropdown
-        if not dp:
-            return
-
-        container = dp.container
-        if not container:
-            return
-        h = self.height
-        for item in container.children[:]:
-            item.height = h
-
-    def _update_dropdown(self, *largs):
-        dp = self._dropdown
-        cls = self.option_cls
-        values = self.values
-        text_autoupdate = self.text_autoupdate
-        if isinstance(cls, string_types):
-            cls = Factory.get(cls)
-        dp.clear_widgets()
-        for value in values:
-            item = cls(text=value)
-            item.height = self.height if self.sync_height else item.height
-            item.bind(on_release=lambda option: dp.select(option.text))
-            dp.add_widget(item)
-        if text_autoupdate:
-            if values:
-                if not self.text or self.text not in values:
-                    self.text = values[0]
-            else:
-                self.text = ''
-
-    def _toggle_dropdown(self, *largs):
-        if self.values:
-            self.is_open = not self.is_open
-
-    def _close_dropdown(self, *largs):
-        self.is_open = False
-
-    def _on_dropdown_select(self, instance, data, *largs):
-        self.text = data
-        self.is_open = False
-
-    def on_is_open(self, instance, value):
-        if value:
-            self._dropdown.open(self)
-        else:
-            if self._dropdown.attach_to:
-                self._dropdown.dismiss()
 
 
 class Spinner(SpinnerBehavior, Button):
@@ -229,3 +105,119 @@ class Spinner(SpinnerBehavior, Button):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.fbind('on_release', self.toggle_dropdown)
+
+
+class TextInputSpinner(SpinnerBehavior, TextInput):
+    '''TextInputSpinner class, see module documentation for more information.
+
+    .. versionadded:: 2.2.0
+    '''
+
+    live_filter = BooleanProperty(True)
+    '''Indicates if the :class:`~kivy.uix.spinner.TextInputSpinner` should be
+    live updated when the TextInput gets updated e.g., by typing.
+
+    Setting this property to True will cause the
+    :class:`~kivy.uix.spinner.TextInputSpinner` to update its visual options in
+    the dropdown selection every time
+    :attr:`~kivy.uix.spinner.TextInputSpinner.text` is changed. Filtering is
+    based on :meth:`~kivy.uix.spinner.TextInputSpinner.filter_function`.
+
+    This option will cause :attr:`~kivy.uix.spinner.TextInputSpinner.values`
+    being overwritten with the currently visual options. To get an unfiltered
+    list of all options, access the
+    :attr:`~kivy.uix.spinner.TextInputSpinner.values_unfiltered`.
+
+    .. versionadded:: 2.2.0
+
+    :attr:`~kivy.uix.spinner.TextInputSpinner.live_filter` is a
+    :class:`~kivy.properties.BooleanProperty` and defaults to True.
+    '''
+
+    filter_case_sensitive = BooleanProperty(False)
+    '''Defines case sensitivity for live_filtering.
+
+    Setting this property to False will make the
+    :meth:`~kivy.uix.spinner.TextInputSpinner.filter_function` ignore the
+    capitalization.
+
+    .. versionadded:: 2.2.0
+
+    :attr:`~kivy.uix.spinner.TextInputSpinner.live_filter` is a
+    :class:`~kivy.properties.BooleanProperty` and defaults to False.
+    '''
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.multiline = False
+
+        self.values_unfiltered = self.values
+        self._filter_values_recursion_depth = 0
+
+        fbind = self.fbind
+        fbind('values', self._filter_updated_values)
+        fbind('live_filter', self._live_filter_values)
+        fbind('text', self._on_text)
+        fbind('on_touch_down', self._on_touch_down)
+
+    def _filter_updated_values(self, *largs):
+        self._filter_values(self.values)
+
+    def _live_filter_values(self, *largs):
+        self._filter_values(self.values_unfiltered)
+
+    def filter_function(self, current_text, option):
+        '''A function which is used when
+        :attr:`~kivy.uix.spinner.TextInputSpinner.live_filter` is set to True.
+
+        The :attr:`~kivy.uix.spinner.TextInputSpinner.filter_case_sensitive`
+        defines wether this function ignores capitalization.
+
+        This function could be overwritten to implement custom a custom filter
+        behavior. To do so, the replacing function must accept two parameters
+        of which the first parameter will provide the current value of
+        :attr:`~kivy.uix.spinner.TextInputSpinner.text` and the second
+        parameter will provide an option value. The function should return True
+        if it should be listed in the dropdown list and False if the option
+        should be hidden.
+
+        .. versionadded:: 2.2.0
+        '''
+        if self.filter_case_sensitive:
+            return current_text in option
+        else:
+            return current_text.lower() in option.lower()
+
+    def _filter_values(self, values):
+        self._filter_values_recursion_depth += 1
+        if self._filter_values_recursion_depth > 1:
+            self._filter_values_recursion_depth -= 1
+            return
+
+        self.values_unfiltered = list(values)
+        current_text = self.text
+        filter_function = self.filter_function
+        if self.live_filter and len(self.text) > 0:
+            self.values = [
+                value
+                for value in values
+                if filter_function(current_text, value)
+            ]
+        else:
+            self.values = self.values_unfiltered
+
+        self._filter_values_recursion_depth -= 1
+
+    def _on_text(self, instance, value):
+        self._live_filter_values(self.values_unfiltered)
+        if not self.is_open:
+            self.open_dropdown()
+
+    def _on_touch_down(self, instance, event):
+        if self.collide_point(*event.pos):
+            self.toggle_dropdown()
+        return super().on_touch_down(event)
+
+    def close_dropdown(self, *largs):
+        super().close_dropdown(*largs)
+        self.focus = False
