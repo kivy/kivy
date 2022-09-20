3e03f587cfb40beaa5b984cde8db7020db0a2822
RobertF <34464649+RobertFlatt@users.noreply.github.com>
2022-09-20 13:44:46 -1000

    Update app.py

--- kivy/app.py
+++ kivy/app.py
@@ -284,10 +284,10 @@ The currently implemented Pause mechanism is:
        System due to the user switching to another application, a phone
        shutdown or any other reason.
     #. :meth:`App.on_pause` is called:
-    #. If False is returned or :meth:`App.on_pause` has no return statement, then
-       :meth:`App.on_stop` is called.
-    #. If True is returned or :meth:`App.on_pause` is not defined, the application
-       will sleep until the OS resumes our App.
+    #. If False is returned or :meth:`App.on_pause` has no return statement,
+       then :meth:`App.on_stop` is called.
+    #. If True is returned or :meth:`App.on_pause` is not defined, the
+       application will sleep until the OS resumes our App.
     #. When the app is resumed, :meth:`App.on_resume` is called.
     #. If our app memory has been reclaimed by the OS, then nothing will be
        called.
@@ -1001,6 +1001,8 @@ Context.html#getFilesDir()>`_ is returned.
         if platform == 'android':
             from android import mActivity
             mActivity.moveTaskToBack(True)
+        else:
+            Logger.info('App.pause() is not available on this OS.')
 
     def on_start(self):
         '''Event handler for the `on_start` event which is fired after
