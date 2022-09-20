7f3b9c506ee56441e8fc481a451ef989e0fc9662
RobertF <34464649+RobertFlatt@users.noreply.github.com>
2022-09-20 11:42:28 -1000

    Update kivy/app.py
    Co-authored-by: Mirko Galimberti <me@mirkogalimberti.com>

--- kivy/app.py
+++ kivy/app.py
@@ -284,9 +284,9 @@ The currently implemented Pause mechanism is:
        System due to the user switching to another application, a phone
        shutdown or any other reason.
     #. :meth:`App.on_pause` is called:
-    #. If False is returned or `App.on_pause` has no return statement, then
+    #. If False is returned or :meth:`App.on_pause` has no return statement, then
        :meth:`App.on_stop` is called.
-    #. If True is returned or `App.on_pause` is not defined, the application
+    #. If True is returned or :meth:`App.on_pause` is not defined, the application
        will sleep until the OS resumes our App.
     #. When the app is resumed, :meth:`App.on_resume` is called.
     #. If our app memory has been reclaimed by the OS, then nothing will be
