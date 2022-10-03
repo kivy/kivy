0a395b86903dd2d2f636e269a98317f541bd6eda
SanAndreyas <74550995+SanAndreyas@users.noreply.github.com>
2022-10-03 11:20:58 +0300

    Update config.py
    fix unicode read for Windows platform

--- kivy/config.py
+++ kivy/config.py
@@ -479,7 +479,7 @@ class ConfigParser(PythonConfigParser, object):
         #    self.readfp(f)
         old_vals = {sect: {k: v for k, v in self.items(sect)} for sect in
                     self.sections()}
-        PythonConfigParser.read(self, filename)
+        PythonConfigParser.read(self, filename, encoding="utf-8")
 
         # when reading new file, sections/keys are only increased, not removed
         f = self._do_callbacks
