0c0172ec44823a418ef84a13517b127de6c391d7
Paul van Haren <paul.vanharen@zonnet.nl>
2023-04-17 15:29:41 +0200

    Consistency between unit tests and implementation
    Some parameters names have changed, making these consistent with
    similar names in other locations of the code. Yet, the changed names
    also have to propagate to the test code

--- kivy/tests/test_gesture.py
+++ kivy/tests/test_gesture.py
@@ -6,40 +6,40 @@ import string
 class GestureTestCase(unittest.TestCase):
 
     def test_horizontal_gesture(self):
-        assert Gesture(points=13) == Gesture(points=12)
-        assert Gesture(points=13) == Gesture(points=46)
-        assert Gesture(points=13) == Gesture(points=79)
+        assert Gesture(point_list=13) == Gesture(point_list=12)
+        assert Gesture(point_list=13) == Gesture(point_list=46)
+        assert Gesture(point_list=13) == Gesture(point_list=79)
 
     def test_vertical_gesture(self):
-        assert Gesture(points=17) == Gesture(points=28)
-        assert Gesture(points=17) == Gesture(points=39)
-        assert Gesture(points=17) == Gesture(points=14)
+        assert Gesture(point_list=17) == Gesture(point_list=28)
+        assert Gesture(point_list=17) == Gesture(point_list=39)
+        assert Gesture(point_list=17) == Gesture(point_list=14)
 
     def test_diagonal_gesture(self):
-        assert Gesture(points=19) == Gesture(points=26)
-        assert Gesture(points=19) == Gesture(points=48)
-        assert Gesture(points=19) == Gesture(points=15)
-        assert Gesture(points=37) == Gesture(points=24)
-        assert Gesture(points=37) == Gesture(points=68)
-        assert Gesture(points=37) == Gesture(points=35)
+        assert Gesture(point_list=19) == Gesture(point_list=26)
+        assert Gesture(point_list=19) == Gesture(point_list=48)
+        assert Gesture(point_list=19) == Gesture(point_list=15)
+        assert Gesture(point_list=37) == Gesture(point_list=24)
+        assert Gesture(point_list=37) == Gesture(point_list=68)
+        assert Gesture(point_list=37) == Gesture(point_list=35)
 
     def test_3valued_gesture(self):
-        assert Gesture(points=13) == Gesture(points=123)
-        assert Gesture(points=17) == Gesture(points=147)
-        assert Gesture(points=19) == Gesture(points=159)
+        assert Gesture(point_list=13) == Gesture(point_list=123)
+        assert Gesture(point_list=17) == Gesture(point_list=147)
+        assert Gesture(point_list=19) == Gesture(point_list=159)
 
     def test_reversed_gesture(self):
-        assert Gesture(points=13) != Gesture(points=31)
-        assert Gesture(points=17) != Gesture(points=71)
-        assert Gesture(points=19) != Gesture(points=91)
+        assert Gesture(point_list=13) != Gesture(point_list=31)
+        assert Gesture(point_list=17) != Gesture(point_list=71)
+        assert Gesture(point_list=19) != Gesture(point_list=91)
 
     def test_scaled_gesture(self):
-        assert Gesture(points=13) == \
-            Gesture(points=[(100, 100), (0, 100)])
-        assert Gesture(points=17) == \
-            Gesture(points=[(100, 100), (100, 0)])
-        assert Gesture(points=19) == \
-            Gesture(points=[(100, 100), (0, 0)])
+        assert Gesture(point_list=13) == \
+            Gesture(point_list=[(100, 100), (0, 100)])
+        assert Gesture(point_list=17) == \
+            Gesture(point_list=[(100, 100), (100, 0)])
+        assert Gesture(point_list=19) == \
+            Gesture(point_list=[(100, 100), (0, 0)])
 
 
 if __name__ == '__main__':
