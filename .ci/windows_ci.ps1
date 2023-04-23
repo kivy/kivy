e24c1962eae4e2989ed1f498e3062b473b6dd2bf
Mirko Galimberti <me@mirkogalimberti.com>
2023-04-23 10:02:55 +0200

    `Generate-sdist` needs `packaging` as a dependency (#8217)

--- .ci/windows_ci.ps1
+++ .ci/windows_ci.ps1
@@ -24,7 +24,7 @@ function Update-version-metadata {
 }
 
 function Generate-sdist {
-    python -m pip install cython
+    python -m pip install cython packaging
     python setup.py sdist --formats=gztar
     python setup.py bdist_wheel --build_examples --universal
     python -m pip uninstall cython -y
