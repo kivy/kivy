bd041aea52d96161fb610a5f1485a7375a0531e1
Mirko Galimberti <me@mirkogalimberti.com>
2023-05-20 09:46:34 +0200

    Release 2.2.0 (#8249)

--- kivy/_version.py
+++ kivy/_version.py
@@ -3,14 +3,14 @@
 MAJOR = 2
 MINOR = 2
 MICRO = 0
-RELEASE = False
+RELEASE = True
 
 __version__ = '%d.%d.%d' % (MAJOR, MINOR, MICRO)
 
 if not RELEASE:
     # if it's a rcx release, it's not proceeded by a period. If it is a
     # devx release, it must start with a period
-    __version__ += 'rc1'
+    __version__ += ''
 
 
 _kivy_git_hash = ''
