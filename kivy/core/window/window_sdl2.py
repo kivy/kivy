5bfbd9e87ff81d03c469e337a137be83d93fc09f
Lucas Couto <lucascouto4@gmail.com>
2023-06-01 16:36:01 -0300

    removing toggle_fullscreen, _get_status, _get_filename
    Signed-off-by: Lucas Couto <lucascouto4@gmail.com>

--- kivy/core/window/window_sdl2.py
+++ kivy/core/window/window_sdl2.py
@@ -450,13 +450,6 @@ class WindowSDL(WindowBase):
         else:
             Logger.warning('Window: show() is used only on desktop OSes.')
 
-    @deprecated
-    def toggle_fullscreen(self):
-        if self.fullscreen in (True, 'auto'):
-            self.fullscreen = False
-        else:
-            self.fullscreen = 'auto'
-
     def set_title(self, title):
         self._win.set_window_title(title)
 
