c08df1c1a82dd4889b6452d5705612080c87d0df
Mak Sim <130763512+mak8kammerer@users.noreply.github.com>
2023-06-24 15:11:20 +0300

    Update window_sdl2.py
    Window.opacity feature

--- kivy/core/window/window_sdl2.py
+++ kivy/core/window/window_sdl2.py
@@ -489,6 +489,12 @@ class WindowSDL(WindowBase):
     def _set_window_pos(self, x, y):
         self._win.set_window_pos(x, y)
 
+    def _get_window_opacity(self):
+        return self._win.get_window_opacity()
+
+    def _set_window_opacity(self, opacity):
+        self._win.set_window_opacity(opacity)
+
     # Transparent Window background
     def _is_shaped(self):
         return self._win.is_window_shaped()
