0da1eabd3f922185c8a9c85eba697532062dd276
Mak Sim <130763512+mak8kammerer@users.noreply.github.com>
2023-08-17 14:28:35 +0300

    New example (#8296)

--- examples/miscellaneous/opacitywindow.py
+++ examples/miscellaneous/opacitywindow.py
@@ -1,25 +1,31 @@
 from kivy.app import App
-from kivy.uix.slider import Slider
+from kivy.uix.boxlayout import BoxLayout
 from kivy.lang import Builder
 
 Builder.load_string('''
 #:import window kivy.core.window.Window
-<OpacitySlider>:
-    min: 0.0
-    max: 1.0
-    value: window.opacity
-    on_value: window.opacity = args[1]
+<Layout>:
+    orientation: 'vertical'
+    Label:
+        text: f'Window opacity: {window.opacity}'
+        font_size: 25
+    Slider:
+        size_hint_y: 4
+        min: 0.0
+        max: 1.0
+        value: window.opacity
+        on_value: window.opacity = args[1]
 ''')
 
 
-class OpacitySlider(Slider):
+class Layout(BoxLayout):
     pass
 
 
 class OpacityWindowApp(App):
 
     def build(self):
-        return OpacitySlider()
+        return Layout()
 
 
 if __name__ == '__main__':
