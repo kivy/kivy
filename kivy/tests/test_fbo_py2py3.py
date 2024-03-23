a66fb3b1bd29a31a6b16889ae2cf5a31909de8f7
Mirko Galimberti <me@mirkogalimberti.com>
2024-03-23 16:23:48 +0100

    It still makes sense to test this method, just rename the test case

--- kivy/tests/test_fbo_py2py3.py
+++ kivy/tests/test_fbo_py2py3.py
@@ -32,7 +32,7 @@ class FboTest(Widget):
         self.fbo.draw()
 
 
-class FBOPy2Py3TestCase(GraphicUnitTest):
+class FBOGetPixelColorTest(GraphicUnitTest):
     def test_fbo_get_pixel_color(self):
         fbow = FboTest()
         render_error = 2
@@ -58,8 +58,6 @@ class FBOPy2Py3TestCase(GraphicUnitTest):
         for i, pos in enumerate(fbow.positions):
             c = fbow.fbo.get_pixel_color(pos[0], pos[1])
             # returned class
-            print(c, values[i][0])
-            print("coucou")
             self.assertTrue(isinstance(c, values[i][0]))
             # returned types in container
             for v in c:
