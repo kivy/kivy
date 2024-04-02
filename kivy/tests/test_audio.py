0a907abe15dc918476e15bad35bf70e90bee71c8
Mak Sim <mmmakkkss@proton.me>
2024-04-02 12:24:11 +0300

    Update tests

--- kivy/tests/test_audio.py
+++ kivy/tests/test_audio.py
@@ -9,6 +9,23 @@ import pytest
 if os.environ.get('KIVY_TEST_AUDIO') == '0':
     pytestmark = pytest.mark.skip("Audio is not available")
 
+IS_GSTPLAYER_AVAILABLE = True
+try:
+    import kivy.lib.gstplayer._gstplayer
+except ImportError:
+    IS_GSTPLAYER_AVAILABLE = False
+else:
+    del kivy.lib.gstplayer._gstplayer
+
+IS_FFPYPLAYER_AVAILABLE = True
+try:
+    import ffpyplayer
+except ImportError:
+    IS_FFPYPLAYER_AVAILABLE = False
+else:
+    del ffpyplayer
+
+
 SAMPLE_FILE = os.path.join(os.path.dirname(__file__), 'sample1.ogg')
 SAMPLE_LENGTH = 1.402
 DELTA = SAMPLE_LENGTH * 0.01
@@ -60,27 +77,39 @@ class OnEOSTest:
     def test_on_eos_event(self):
         eos = False
 
-        def callback(*args):
+        def callback(_):
             nonlocal eos
             eos = True
 
         import time
-        sound = self.make_sound(SAMPLE_FILE)
+        sound = self.get_sound()
         sound.bind(on_eos=callback)
         sound.play()
-        time.sleep(SAMPLE_LENGTH)
+        time.sleep(SAMPLE_LENGTH + 0.5)  # the callback is called with a delay
+        from kivy.clock import Clock
+        Clock.tick()
         self.assertTrue(eos)
 
 
-class AudioGstreamerTestCase(AudioTestCase, OnEOSTest):
+@pytest.mark.skipif(
+    not IS_GSTPLAYER_AVAILABLE,
+    reason='gstreamer is not available'
+)
+class AudioGstplayerTestCase(AudioTestCase, OnEOSTest):
 
-    def make_sound(self, source):
+    def get_sound(self):
+        assert os.path.exists(SAMPLE_FILE)
         from kivy.core.audio import audio_gstplayer
-        return audio_gstplayer.SoundGstplayer(source=source)
+        return audio_gstplayer.SoundGstplayer(source=SAMPLE_FILE)
 
 
+@pytest.mark.skipif(
+    not IS_FFPYPLAYER_AVAILABLE,
+    reason='ffpyplayer is not available'
+)
 class AudioFFPyplayerTestCase(AudioTestCase, OnEOSTest):
 
-    def make_sound(self, source):
+    def get_sound(self):
+        assert os.path.exists(SAMPLE_FILE)
         from kivy.core.audio import audio_ffpyplayer
-        return audio_ffpyplayer.SoundFFPy(source=source)
+        return audio_ffpyplayer.SoundFFPy(source=SAMPLE_FILE)
