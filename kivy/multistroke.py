65ee5e2789840a27ff18cd78c2e9f23059437c34
Ricardo Bastos Leta Vieira <47068518+ricleta@users.noreply.github.com>
2024-11-02 21:45:10 -0300

    Fixed issue with failing tests for multistroke.py
    Re added some parts of recognize that were causing tests to fail

--- kivy/multistroke.py
+++ kivy/multistroke.py
@@ -618,6 +618,61 @@ class Recognizer(EventDispatcher):
             Clock.schedule_once(result_hack)
             return result
 
+        # This callback is scheduled once per frame until completed
+        def _recognize_tick(dt):
+            start_gc = result._completed
+            stop_now = False
+
+            while not stop_now and (tasklist and not result._break_flag) and \
+                    (not GPF or (result._completed - start_gc < GPF)):
+
+                if (timeout and
+                        Clock.get_time() - result._start_time >= timeout):
+                    result.status = 'timeout'
+                    stop_now = True
+                    break
+
+                # Get the best distance and number of matching operations done
+                gesture = tasklist.popleft()
+                tpl, d, res, mos = gesture.match_candidate(
+                    cand, **kwargs)
+
+                if tpl is not None:
+                    score = result._add_result(gesture, d, tpl, res)
+                    if goodscore is not None and score >= goodscore:
+                        result.status = 'goodscore'
+                        stop_now = True
+
+                result._match_ops += mos
+                result._completed += 1
+                result.dispatch('on_progress')
+
+            # The loop has ended. Prepare to dispatch 'complete'
+            def _dispatch():
+                result.dispatch('on_complete')
+                self.dispatch('on_search_complete', result)
+                return False
+
+            # Dispatch or reschedule another run
+            if not tasklist:
+                result.status = 'complete'
+                return _dispatch()
+            elif result._break_flag:
+                result.status = 'stop'
+                return _dispatch()
+            elif stop_now:
+                return _dispatch()
+            else:
+                Clock.schedule_once(_recognize_tick, delay)
+                return True
+        # End _recognize_tick()
+
+        self.dispatch('on_search_start', result)
+        if not GPF:
+            _recognize_tick(0)
+        else:
+            Clock.schedule_once(_recognize_tick, 0)
+
         return result
 
     def _candidate(self, strokes, **kwargs):
